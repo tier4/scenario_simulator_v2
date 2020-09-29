f3d722cb9d30f160c08f350512ae904b9232f024
Masaya Kataoka <ms.kataoka@gmail.com>
2020-09-29 15:02:28 +0900

    port API into ROS2
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation_controller/include/simulation_controller/api/api.hpp
+++ simulation_controller/include/simulation_controller/api/api.hpp
@@ -13,15 +13,23 @@
 
 namespace scenario_simulator
 {
+    template<class NodeT>
     class API
     {
+    using EntityManager = simulation_controller::entity::EntityManager<NodeT>;
     public:
-        API(std::string address, int port);
-        std::shared_ptr<SimulationAPIImpl> simulation;
-        std::shared_ptr<EntityAPIImpl> entity;
-    private:
-        std::shared_ptr<XmlRpc::XmlRpcClient> client_ptr_;
-        std::shared_ptr<simulation_controller::entity::EntityManager> entity_manager_ptr_;
+        API(std::string address, int port, NodeT && node)
+        {
+            auto entity_manager_ptr = std::shared_ptr<EntityManager>(new EntityManager(node));
+            auto client_ptr = std::shared_ptr<XmlRpc::XmlRpcClient>
+                (new XmlRpc::XmlRpcClient(address.c_str(), port));
+            simulation = std::shared_ptr<SimulationAPIImpl<NodeT>>
+                (new SimulationAPIImpl<NodeT>(client_ptr, entity_manager_ptr));
+            entity = std::shared_ptr<EntityAPIImpl<NodeT>>
+                (new EntityAPIImpl<NodeT>(client_ptr, entity_manager_ptr));
+        }
+        std::shared_ptr<SimulationAPIImpl<NodeT>> simulation;
+        std::shared_ptr<EntityAPIImpl<NodeT>> entity;
     };
 }  // namespace scenario_simulator
 
--- simulation_controller/src/api/entity_api_impl.cpp
+++ simulation_controller/src/api/entity_api_impl.cpp
@@ -1,7 +1,1 @@
 #include <simulation_controller/api/entity_api_impl.hpp>
-#include <simulation_controller/entity/exception.hpp>
-
-namespace scenario_simulator
-{
-    
-}
