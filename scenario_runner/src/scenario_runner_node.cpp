d70775f781694fdd6e4adf3a1d5aa4736f553b13
yamacir-kit <httperror@404-notfound.jp>
2020-10-01 14:47:09 +0900

    Cleanup

--- scenario_runner/src/scenario_runner_node.cpp
+++ scenario_runner/src/scenario_runner_node.cpp
@@ -24,18 +24,6 @@
 #include <type_traits>
 #include <memory>
 
-// enum class Result
-// {
-//   success = EXIT_SUCCESS,  // 0
-//   failure = EXIT_FAILURE,  // any non-0 value
-//
-//   syntax_error,
-//   implementation_error,
-//   unexpected_error,
-// };
-
-static_assert(EXIT_SUCCESS < EXIT_FAILURE);
-
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
@@ -54,67 +42,4 @@ int main(int argc, char ** argv)
   rclcpp::shutdown();
 
   return EXIT_SUCCESS;
-
-  // rclcpp::NodeOptions options {};
-  //
-  // auto node {rclcpp::Node::make_shared("scenario_runner_node")};
-  //
-  // std::string scenario {""};
-  // (*node).declare_parameter<decltype(scenario)>("scenario", scenario);
-  // (*node).get_parameter("scenario", scenario);
-  //
-  // bool verbose {false};
-  // (*node).declare_parameter<decltype(verbose)>("verbose", verbose);
-  // (*node).get_parameter("verbose", verbose);
-  //
-  // int port {8080};
-  // (*node).declare_parameter<decltype(port)>("port", port);
-  // (*node).get_parameter("port", port);
-  //
-  // try {
-  //   scenario_runner::OpenSCENARIO osc {scenario, "127.0.0.1", 8080};
-  //
-  //   rclcpp::Rate rate {50};
-  //
-  //   for (osc.init(); !osc.complete(); rate.sleep()) {
-  //     const auto result {osc.evaluate()};
-  //
-  //     std::cout << "[Storyboard: " << result << "]" << std::endl;
-  //
-  //     std::cout << "[" <<
-  //     (scenario_runner::standby_state.use_count() - 1) << " standby, " <<
-  //     (scenario_runner::running_state.use_count() - 1) << " running, " <<
-  //     (scenario_runner::complete_state.use_count() - 1) << " complete, and " <<
-  //     (scenario_runner::stop_transition.use_count() - 1) << " stopping " <<
-  //       " (" <<
-  //     (scenario_runner::start_transition.use_count() +
-  //     scenario_runner::end_transition.use_count() - 2) <<
-  //       " in transition)" <<
-  //       "]\n" <<
-  //       std::endl;
-  //   }
-  // } catch (const scenario_runner::Command & command) {
-  //   switch (command) {
-  //     case scenario_runner::Command::exitSuccess:
-  //       RCLCPP_INFO((*node).get_logger(), "Simulation succeeded.");
-  //       return static_cast<std::underlying_type<Result>::type>(Result::success);
-  //
-  //     default:
-  //     case scenario_runner::Command::exitFailure:
-  //       RCLCPP_INFO((*node).get_logger(), "Simulation failed.");
-  //       return static_cast<std::underlying_type<Result>::type>(Result::failure);
-  //   }
-  // } catch (const scenario_runner::SyntaxError & error) {
-  //   RCLCPP_ERROR((*node).get_logger(), "%s.", error.what());
-  //   return static_cast<std::underlying_type<Result>::type>(Result::syntax_error);
-  // } catch (const scenario_runner::ImplementationFault & error) {
-  //   RCLCPP_ERROR((*node).get_logger(), "%s.", error.what());
-  //   return static_cast<std::underlying_type<Result>::type>(Result::implementation_error);
-  // } catch (const std::exception & error) {
-  //   RCLCPP_ERROR((*node).get_logger(), "%s.", error.what());
-  //   return static_cast<std::underlying_type<Result>::type>(Result::unexpected_error);
-  // }
-  //
-  // RCLCPP_INFO((*node).get_logger(), "Simulation succeeded.");
-  // return static_cast<std::underlying_type<Result>::type>(Result::success);
 }
