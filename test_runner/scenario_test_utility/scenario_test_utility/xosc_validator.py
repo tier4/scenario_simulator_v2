a0dab43aebe9ef9dbacdf5a03d957062ecf8764c
Masaya Kataoka <ms.kataoka@gmail.com>
2020-10-12 16:02:59 +0900

    add ament_openscenario package
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- test_runner/scenario_test_utility/scenario_test_utility/xosc_validator.py
+++ test_runner/scenario_test_utility/scenario_test_utility/xosc_validator.py
@@ -3,7 +3,7 @@
 
 """Xosc validator."""
 
-# Copyright 2020 Tier IV, Inc. All rights reserved.
+# Copyright 2020 Autoware Foundation. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
