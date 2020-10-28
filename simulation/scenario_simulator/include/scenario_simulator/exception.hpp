973c6228043e6fadae9f21ce34db0bc4f5750606
Masaya Kataoka <ms.kataoka@gmail.com>
2020-10-28 14:19:49 +0900

    enable pass ament_lint_cpp
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation/scenario_simulator/include/scenario_simulator/exception.hpp
+++ simulation/scenario_simulator/include/scenario_simulator/exception.hpp
@@ -1,3 +1,17 @@
+// Copyright 2015-2020 Autoware Foundation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef SCENARIO_SIMULATOR__EXCEPTION_HPP_
 #define SCENARIO_SIMULATOR__EXCEPTION_HPP_
 
@@ -6,13 +20,13 @@ namespace scenario_simulator
 class SimulationRuntimeError : public std::runtime_error
 {
 public:
-  SimulationRuntimeError(XmlRpc::XmlRpcValue value)
+  explicit SimulationRuntimeError(XmlRpc::XmlRpcValue value)
   : runtime_error(value["message"]) {}
-  SimulationRuntimeError(const char * message)
+  explicit SimulationRuntimeError(const char * message)
   : runtime_error(message) {}
 
 private:
 };
-}
+}  // namespace scenario_simulator
 
 #endif  // SCENARIO_SIMULATOR__EXCEPTION_HPP_
