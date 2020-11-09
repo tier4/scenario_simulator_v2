98aba7985f1d4db83cca5976b80034a2f7177387
Masaya Kataoka <ms.kataoka@gmail.com>
2020-11-09 17:40:59 +0900

    update dockerfile
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- entrypoint.bash
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-source /opt/ros/dashing/setup.bash
-source /root/scenario_simulator_ws/install/local_setup.bash
-exec "$@"
--- external/spline_interpolation/src/spline_interpolation.cpp
+++ external/spline_interpolation/src/spline_interpolation.cpp
@@ -45,9 +45,10 @@ void SplineInterpolator::generateSpline(
   } else if (method_ == Method::SOR) {
     SOR_solver_.setCoefficients(h_, a_, 1000);
     c_ = SOR_solver_.solve();
-  } else {
+  } /*else {
     std::cerr << "Unsupported method. Supported solver: Explicit, PCG, SOR" << std::endl;
   }
+  */
 
   for (size_t i = 0; i < N - 1; i++) {
     d_.push_back((c_[i + 1] - c_[i]) / (3.0 * h_[i]));
@@ -64,7 +65,7 @@ double SplineInterpolator::getValue(
   const double & query, const std::vector<double> & base_index) const
 {
   if (!initialized_) {
-    std::cerr << "[interpolate] spline is uninitialized" << std::endl;
+    // std::cerr << "[interpolate] spline is uninitialized" << std::endl;
     return 0.0;
   }
 
@@ -82,7 +83,7 @@ bool SplineInterpolator::interpolate(
   method_ = method;
 
   if (!isValidInput(base_index, base_value, return_index)) {
-    std::cerr << "[interpolate] invalid input. interpolation failed." << std::endl;
+    // std::cerr << "[interpolate] invalid input. interpolation failed." << std::endl;
     return false;
   }
 
