738c80e42e186ce9a3bfe237c8755cba91ba1811
Masaya Kataoka <ms.kataoka@gmail.com>
2020-12-21 14:14:34 +0900

    retry tick when the action is root
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation/simulation_api/src/entity/vehicle_entity.cpp
+++ simulation/simulation_api/src/entity/vehicle_entity.cpp
@@ -102,6 +102,9 @@ void VehicleEntity::onUpdate(double current_time, double step_time)
   tree_ptr_->setValueToBlackBoard("entity_type_list", entity_type_list_);
   tree_ptr_->setValueToBlackBoard("entity_status", status_.get());
   action_status_ = tree_ptr_->tick(current_time, step_time);
+  while(getCurrentAction() == "root") {
+    action_status_ = tree_ptr_->tick(current_time, step_time);
+  }
   auto status_updated = tree_ptr_->getUpdatedStatus();
   if (status_updated.lanelet_pose_valid) {
     auto following_lanelets = hdmap_utils_ptr_->getFollowingLanelets(
