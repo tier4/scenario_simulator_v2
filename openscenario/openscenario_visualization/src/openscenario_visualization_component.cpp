158bce6a5303caced58603ed93cd271a2dab9d86
Masaya Kataoka <ms.kataoka@gmail.com>
2020-12-22 23:25:16 +0900

    add marker to the other actions
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- openscenario/openscenario_visualization/src/openscenario_visualization_component.cpp
+++ openscenario/openscenario_visualization/src/openscenario_visualization_component.cpp
@@ -324,25 +324,29 @@ const visualization_msgs::msg::MarkerArray OpenscenarioVisualizationComponent::g
   waypoints_marker.header.stamp = stamp;
   waypoints_marker.ns = status.name;
   waypoints_marker.id = 4;
-  waypoints_marker.action = waypoints_marker.ADD;
-  // waypoints_marker.points = waypoints.waypoints;
-  for (const auto waypoint : waypoints.waypoints) {
-    geometry_msgs::msg::Point p;
-    p.x = -waypoint.x;
-    p.y = -waypoint.y;
-    p.z = -waypoint.z;
-    waypoints_marker.points.emplace_back(p);
+  if (waypoints.waypoints.size() == 0) {
+    waypoints_marker.action = waypoints_marker.DELETE;
+    ret.markers.push_back(waypoints_marker);
+  } else {
+    waypoints_marker.action = waypoints_marker.ADD;
+    // waypoints_marker.points = waypoints.waypoints;
+    for (const auto waypoint : waypoints.waypoints) {
+      geometry_msgs::msg::Point p;
+      p.x = -waypoint.x;
+      p.y = -waypoint.y;
+      p.z = -waypoint.z;
+      waypoints_marker.points.emplace_back(p);
+    }
+    waypoints_marker.color = color;
+    waypoints_marker.type = waypoints_marker.LINE_STRIP;
+    waypoints_marker.colors =
+      std::vector<std_msgs::msg::ColorRGBA>(waypoints.waypoints.size() - 1, color);
+    waypoints_marker.colors = std::vector<std_msgs::msg::ColorRGBA>(1, color);
+    waypoints_marker.scale.x = 0.3;
+    waypoints_marker.scale.y = 0.3;
+    waypoints_marker.scale.z = 0.3;
+    ret.markers.push_back(waypoints_marker);
   }
-  waypoints_marker.color = color;
-  waypoints_marker.type = waypoints_marker.SPHERE_LIST;
-  waypoints_marker.colors =
-    std::vector<std_msgs::msg::ColorRGBA>(waypoints.waypoints.size(), color);
-  waypoints_marker.colors = std::vector<std_msgs::msg::ColorRGBA>(1, color);
-  waypoints_marker.scale.x = 0.3;
-  waypoints_marker.scale.y = 0.3;
-  waypoints_marker.scale.z = 0.3;
-  ret.markers.push_back(waypoints_marker);
-
   return ret;
 }
 
