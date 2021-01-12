03af1289c302401124eebe3c170120100ad38174
Masaya Kataoka <ms.kataoka@gmail.com>
2021-01-12 16:54:39 +0900

    enable visualize front entity position
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation/simulation_api/include/simulation_api/behavior/vehicle/follow_lane_sequence/follow_front_entity_action.hpp
+++ simulation/simulation_api/include/simulation_api/behavior/vehicle/follow_lane_sequence/follow_front_entity_action.hpp
@@ -46,6 +46,8 @@ public:
   const openscenario_msgs::msg::WaypointsArray calculateWaypoints() override;
   const boost::optional<openscenario_msgs::msg::Obstacle> calculateObstacle(
     const openscenario_msgs::msg::WaypointsArray & waypoints) override;
+private:
+  boost::optional<double> distance_to_front_entity_;
 };
 }  // namespace follow_lane_sequence
 }  // namespace vehicle
