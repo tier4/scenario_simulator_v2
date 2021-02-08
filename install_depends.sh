6985bf49a7a7fc3a2be8ccd39a1d5283e1c9d513
yamacir-kit <httperror@404-notfound.jp>
2021-02-08 13:22:27 +0900

    Update workflows

--- install_depends.sh
+++ install_depends.sh
@@ -11,6 +11,7 @@ vcs import external < dependency.repos
 git clone -b ros2 https://github.com/tier4/pilot.auto.git
 
 mv "$repository_root/pilot.auto/awapi/autoware_api_msgs"              "$repository_root/external"
+mv "$repository_root/pilot.auto/common/msgs/autoware_control_msgs"    "$repository_root/external"
 mv "$repository_root/pilot.auto/common/msgs/autoware_perception_msgs" "$repository_root/external"
 mv "$repository_root/pilot.auto/common/msgs/autoware_planning_msgs"   "$repository_root/external"
 mv "$repository_root/pilot.auto/common/msgs/autoware_system_msgs"     "$repository_root/external"
