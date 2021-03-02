d0edf510e0615b8c85467466c3c06a359e4a1b71
Masaya Kataoka <ms.kataoka@gmail.com>
2021-03-02 18:18:41 +0900

    add launch file
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ simulation/simulation_interface/include/simulation_interface/zmq_server.hpp
@@ -0,0 +1,75 @@
+// Copyright 2015-2020 Tier IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SIMULATION_INTERFACE__ZMQ_SERVER_HPP_
+#define SIMULATION_INTERFACE__ZMQ_SERVER_HPP_
+
+#include <zmqpp/zmqpp.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+#include <thread>
+#include <functional>
+
+namespace zeromq
+{
+template<typename ReqType, typename ResType>
+class Server
+{
+public:
+  explicit Server(
+    const std::string & endpoint,
+    std::function<void(const ReqType &, ResType &)> func)
+  : endpoint_(endpoint),
+    context_(zmqpp::context()),
+    type_(zmqpp::socket_type::reply),
+    socket_(context_, type_)
+  {
+    using namespace std::chrono_literals;
+    socket_.bind(endpoint_);
+    poller_.add(socket_);
+    thread_ = std::thread(&Server::poll, this);
+  }
+
+private:
+  void poll()
+  {
+    while (rclcpp::ok()) {
+      poller_.poll(0.01);
+      if (poller_.has_input(socket_)) {
+        zmqpp::message request;
+        socket_.receive(request);
+        std::string serialized_str = request.get(0);
+        ReqType req_proto;
+        req_proto.ParseFromString(serialized_str);
+        ResType res_proto;
+        func_(req_proto, res_proto);
+        std::string res_serialized_str;
+        res_proto.SerializeToString(&res_serialized_str);
+        zmqpp::message response;
+        response << res_serialized_str;
+        socket_.send(response);
+      }
+    }
+  }
+  const std::string endpoint_;
+  const zmqpp::context context_;
+  const zmqpp::socket_type type_;
+  zmqpp::socket socket_;
+  zmqpp::poller poller_;
+  std::thread thread_;
+  std::function<void(const ReqType &, ResType &)> func_;
+};
+}  // namespace zeromq
+
+#endif  // SIMULATION_INTERFACE__ZMQ_SERVER_HPP_
--- simulation/simulation_interface/src/example.cpp
+++ simulation/simulation_interface/src/example.cpp
@@ -12,25 +12,62 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <simulation_interface/conversions.hpp>
+#include <simulation_interface/zmq_server.hpp>
+#include <simulation_interface/zmq_client.hpp>
+
+#include <rclcpp/rclcpp.hpp>
 
 #include <simulation_api_schema.pb.h>
-
 #include <string>
+#include <chrono>
 
-int main()
+void callback(
+  const simulation_api_schema::InitializeRequest & req,
+  simulation_api_schema::InitializeResponse & res)
 {
-  /*
-  simulation_api_schema::InitializeResponse res;
-  res.mutable_result()->set_success(true);
-  res.mutable_result()->set_description("test");
-  res.PrintDebugString();
-  XmlRpc::XmlRpcValue xml;
-  simulation_interface::serializeToBinValue(res);
-  simulation_interface::fromProto(res, xml);
-  std::string description = xml["description"];
-  simulation_interface::toProto(xml, res);
-  res.PrintDebugString();
-  */
+  std::cout << __LINE__ << "," << __FILE__ << std::endl;
+  req.PrintDebugString();
+  std::cout << __LINE__ << "," << __FILE__ << std::endl;
+}
+
+class ExampleNode : public rclcpp::Node
+{
+public:
+  explicit ExampleNode(const rclcpp::NodeOptions & option)
+  : Node("example", option),
+    server_("tcp://*:5555", callback),
+    client_("tcp://localhost:5555")
+  {
+    using namespace std::chrono_literals;
+    update_timer_ = this->create_wall_timer(50ms, std::bind(&ExampleNode::sendRequest, this));
+  }
+  void sendRequest()
+  {
+    simulation_api_schema::InitializeRequest request;
+    simulation_api_schema::InitializeResponse response;
+    client_.call(request, response);
+    std::cout << __LINE__ << "," << __FILE__ << std::endl;
+    request.PrintDebugString();
+    std::cout << __LINE__ << "," << __FILE__ << std::endl;
+  }
+
+private:
+  rclcpp::TimerBase::SharedPtr update_timer_;
+  zeromq::Server<
+    simulation_api_schema::InitializeRequest,
+    simulation_api_schema::InitializeResponse> server_;
+  zeromq::Client<
+    simulation_api_schema::InitializeRequest,
+    simulation_api_schema::InitializeResponse> client_;
+
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions options;
+  auto component = std::make_shared<ExampleNode>(options);
+  rclcpp::spin(component);
+  rclcpp::shutdown();
   return 0;
 }
