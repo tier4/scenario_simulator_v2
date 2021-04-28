d406d934d98861c45504aa69c101ec78ce0627a5
Masaya Kataoka <ms.kataoka@gmail.com>
2021-04-28 16:35:58 +0900

    use cp command
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- install_depends.sh
+++ install_depends.sh
@@ -8,14 +8,12 @@ mkdir -p "$repository_root/external"
 
 vcs import external < dependency.repos
 
-git clone -b ros2 https://github.com/tier4/pilot.auto.git
+cp -r "$repository_root/external/autoware/awapi/autoware_api_msgs"              "$repository_root/external"
+cp -r "$repository_root/external/autoware/common/msgs/autoware_control_msgs"    "$repository_root/external"
+cp -r "$repository_root/external/autoware/common/msgs/autoware_debug_msgs"      "$repository_root/external"
+cp -r "$repository_root/external/autoware/common/msgs/autoware_perception_msgs" "$repository_root/external"
+cp -r "$repository_root/external/autoware/common/msgs/autoware_planning_msgs"   "$repository_root/external"
+cp -r "$repository_root/external/autoware/common/msgs/autoware_system_msgs"     "$repository_root/external"
+cp -r "$repository_root/external/autoware/common/msgs/autoware_vehicle_msgs"    "$repository_root/external"
 
-mv "$repository_root/pilot.auto/awapi/autoware_api_msgs"              "$repository_root/external"
-mv "$repository_root/pilot.auto/common/msgs/autoware_control_msgs"    "$repository_root/external"
-mv "$repository_root/pilot.auto/common/msgs/autoware_debug_msgs"      "$repository_root/external"
-mv "$repository_root/pilot.auto/common/msgs/autoware_perception_msgs" "$repository_root/external"
-mv "$repository_root/pilot.auto/common/msgs/autoware_planning_msgs"   "$repository_root/external"
-mv "$repository_root/pilot.auto/common/msgs/autoware_system_msgs"     "$repository_root/external"
-mv "$repository_root/pilot.auto/common/msgs/autoware_vehicle_msgs"    "$repository_root/external"
-
-rm -rf "$repository_root/pilot.auto"
+rm -rf "$repository_root/external/autoware"
