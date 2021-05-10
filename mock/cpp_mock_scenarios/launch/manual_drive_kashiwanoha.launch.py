6c8be2cdc05c264c17a9ea00c7966f2f5617241e
Masaya Kataoka <ms.kataoka@gmail.com>
2021-05-10 12:07:12 +0900

    fix launch file
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- mock/cpp_mock_scenarios/launch/manual_drive_kashiwanoha.launch.py
+++ mock/cpp_mock_scenarios/launch/manual_drive_kashiwanoha.launch.py
@@ -25,9 +25,9 @@ from launch import LaunchDescription
 
 from launch.actions import IncludeLaunchDescription
 
-from launch_ros.actions import Node
-
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
