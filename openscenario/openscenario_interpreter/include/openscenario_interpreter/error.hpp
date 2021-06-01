5b44f81e3085719284a3ba7d52625c5e732685d0
yamacir-kit <httperror@404-notfound.jp>
2021-06-01 18:49:39 +0900

    Remove ImplementationFault from struct RelativeWorldPosition

--- openscenario/openscenario_interpreter/include/openscenario_interpreter/error.hpp
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/error.hpp
@@ -39,7 +39,11 @@ namespace openscenario_interpreter
 #define UNSUPPORTED_ELEMENT_SPECIFIED(PARENT, CHILD) \
   SyntaxError(                                       \
     "Given class ",                                  \
-    #CHILD " is valid OpenSCENARIO element of class " #PARENT ", but is is not supported yet")
+    #CHILD " is valid OpenSCENARIO element of class " #PARENT ", but is not supported yet")
+
+#define UNSUPPORTED_CONVERSION_DETECTED(FROM, TO) \
+  SyntaxError("Converting " #FROM " to " #TO      \
+              ". This is valid in OpenSCENARIO standard, but is not yet supported")
 
 /* ---- NOTE -------------------------------------------------------------------
  *
