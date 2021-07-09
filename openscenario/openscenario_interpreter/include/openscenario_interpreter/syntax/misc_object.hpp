2cf351c3f09140061d9f327e0477e44949cb7680
yamacir-kit <httperror@404-notfound.jp>
2021-07-09 17:00:27 +0900

    Update struct MiscObject

--- openscenario/openscenario_interpreter/include/openscenario_interpreter/syntax/misc_object.hpp
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/syntax/misc_object.hpp
@@ -15,9 +15,13 @@
 #ifndef OPENSCENARIO_INTERPRETER__SYNTAX__MISC_OBJECT_HPP_
 #define OPENSCENARIO_INTERPRETER__SYNTAX__MISC_OBJECT_HPP_
 
+#include <openscenario_interpreter/scope.hpp>
+#include <openscenario_interpreter/syntax/bounding_box.hpp>
 #include <openscenario_interpreter/syntax/double.hpp>
 #include <openscenario_interpreter/syntax/misc_object_category.hpp>
-#include <utility>
+#include <openscenario_interpreter/syntax/parameter_declarations.hpp>
+#include <openscenario_interpreter/syntax/properties.hpp>
+#include <openscenario_interpreter/syntax/string.hpp>
 
 namespace openscenario_interpreter
 {
@@ -38,11 +42,41 @@ inline namespace syntax
  *  </xsd:complexType>
  *
  * -------------------------------------------------------------------------- */
-struct MiscObject
+struct MiscObject : private Scope
 {
-  const Double mass;  // Mass of the miscellaneous object. Unit: kg; Range: [0..inf[.
+  const Double mass;  // Mass of the miscellaneous object. Unit: Kg; Range: [0..inf[.
 
-  const MiscObjectCategory misc_object_category;
+  const MiscObjectCategory misc_object_category;  // Categorization of the miscellaneous object.
+
+  const String name;  // Name of the miscellaneous object type.
+
+  const String
+    model3d;  // Definition of the model of the miscellaneous object as a model type or a relative or absolute file path.
+
+  const ParameterDeclarations parameter_declarations;  // Definition of additional parameters.
+
+  const BoundingBox bounding_box;  // Bounding box definition for the miscellaneous object.
+
+  Properties properties;  // Property definitions for the miscellaneous object.
+
+  template <typename Tree, typename Scope>
+  explicit MiscObject(const Tree & tree, Scope & scope)
+  // clang-format off
+  : Scope(scope),
+    mass                  (readAttribute<Double               >("mass",                  tree, localScope())),
+    misc_object_category  (readAttribute<MiscObjectCategory   >("miscObjectCategory",    tree, localScope())),
+    name                  (readAttribute<String               >("name",                  tree, localScope())),
+    model3d               (readAttribute<String               >("model3d",               tree, localScope())),
+    parameter_declarations(readElement  <ParameterDeclarations>("ParameterDeclarations", tree, localScope())),
+    bounding_box          (readElement  <BoundingBox          >("BoundingBox",           tree, localScope())),
+    properties            (readElement  <Properties           >("Properties",            tree, localScope()))
+  // clang-format on
+  {
+  }
+
+  // explicit operator openscenario_msgs::msg::MiscObjectParameters() const
+  // {
+  // }
 };
 }  // namespace syntax
 }  // namespace openscenario_interpreter
