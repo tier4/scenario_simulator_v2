afa79ba6a635bd3fbd335a6ea6e9d9f9f7f62115
yamacir-kit <httperror@404-notfound.jp>
2021-07-19 17:32:19 +0900

    Update class Configuration to assert .osm file existence

--- simulation/traffic_simulator/include/traffic_simulator/api/configuration.hpp
+++ simulation/traffic_simulator/include/traffic_simulator/api/configuration.hpp
@@ -15,9 +15,11 @@
 #ifndef TRAFFIC_SIMULATOR__API__CONFIGURATION_HPP_
 #define TRAFFIC_SIMULATOR__API__CONFIGURATION_HPP_
 
+#include <algorithm>
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/operations.hpp>
+#include <boost/range/iterator_range.hpp>
 #include <iomanip>
 #include <scenario_simulator_exception/exception.hpp>
 #include <string>
@@ -56,11 +58,9 @@ private:
   Pathname map_path = "";
 
 public:
-  Filename lanelet2_map_file =
-    "lanelet2_map.osm";  // TODO (yamacir-kit): Use `static inline` (if C++17)
+  Filename lanelet2_map_file = "lanelet2_map.osm";
 
-  Filename pointcloud_map_file =
-    "pointcloud_map.pcd";  // TODO (yamacir-kit): Use `static inline` (if C++17)
+  Filename pointcloud_map_file = "pointcloud_map.pcd";
 
   Pathname scenario_path = "";
 
@@ -83,9 +83,40 @@ public:
     } else if (not boost::filesystem::is_directory(map_path)) {
       throw common::SimulationError(
         "The map_path must be a directory (given an ", std::quoted(map_path.string()), ")");
-    } else {
+    } else if (not contains(map_path, ".osm")) {
+      throw common::SimulationError("The map_path must contain at least one *.osm file");
+    }
+    // else if (not contains(map_path, ".pcd")) {
+    //   throw common::SimulationError("The map_path must contain at least one *.pcd file");
+    // }
+    else {
       return map_path;
     }
+  }
+
+  template <typename... Ts>
+  auto contains(Ts &&... xs) const -> bool
+  {
+    return not findLexicographicallyFirstFilenameOf(std::forward<decltype(xs)>(xs)...).empty();
+  }
+
+  auto findLexicographicallyFirstFilenameOf(
+    const Pathname & pathname, const std::string & extension) const -> Filename
+  {
+    Filename result;
+
+    for (const auto & each :
+         boost::make_iterator_range(boost::filesystem::directory_iterator(pathname), {})) {
+      const auto filename = each.path().filename().string();
+      if (
+        each.path().extension() == extension and
+        std::lexicographical_compare(
+          std::cbegin(result), std::cend(result), std::cbegin(filename), std::cend(filename))) {
+        result = filename;
+      }
+    }
+
+    return result;
   }
 
   auto getMapPath() const -> const auto & { return assertMapPath(map_path); }
