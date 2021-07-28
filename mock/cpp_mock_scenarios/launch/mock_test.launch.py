1238a1378de8efaa0e14a4e60707add345c8add1
Masaya Kataoka <ms.kataoka@gmail.com>
2021-07-28 18:05:06 +0900

    apply reformat
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- mock/cpp_mock_scenarios/launch/mock_test.launch.py
+++ mock/cpp_mock_scenarios/launch/mock_test.launch.py
@@ -43,19 +43,19 @@ from typing import cast
 
 
 class Color:
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    PURPLE = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    END = '\033[0m'
-    BOLD = '\038[1m'
-    UNDERLINE = '\033[4m'
-    INVISIBLE = '\033[08m'
-    REVERCE = '\033[07m'
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    PURPLE = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    END = "\033[0m"
+    BOLD = "\038[1m"
+    UNDERLINE = "\033[4m"
+    INVISIBLE = "\033[08m"
+    REVERCE = "\033[07m"
 
 
 def on_stderr_output(event: launch.Event) -> None:
