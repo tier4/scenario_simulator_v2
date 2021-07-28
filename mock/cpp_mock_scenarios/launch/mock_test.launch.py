9179aa1a9b6dbdecf712fead6e33b2bbd988d30a
Masaya Kataoka <ms.kataoka@gmail.com>
2021-07-28 17:56:10 +0900

    enable colorlize output
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- mock/cpp_mock_scenarios/launch/mock_test.launch.py
+++ mock/cpp_mock_scenarios/launch/mock_test.launch.py
@@ -40,21 +40,36 @@ from launch.substitutions.launch_configuration import LaunchConfiguration
 from launch_ros.actions import Node
 
 from typing import cast
-from termcolor import colored
+
+
+class Color:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    PURPLE = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    END = '\033[0m'
+    BOLD = '\038[1m'
+    UNDERLINE = '\033[4m'
+    INVISIBLE = '\033[08m'
+    REVERCE = '\033[07m'
 
 
 def on_stderr_output(event: launch.Event) -> None:
     lines = event.text.decode().splitlines()
     if len(lines) == 1:
         if lines[0] == "cpp_scenario:failure":
-            print(colored("Scenario Failed", "red"))
+            print(Color.RED + "Scenario Failed" + Color.END)
 
 
 def on_stdout_output(event: launch.Event) -> None:
     lines = event.text.decode().splitlines()
     if len(lines) == 1:
         if lines[0] == "cpp_scenario:success":
-            print(colored("Scenario Succeed", "green"))
+            print(Color.GREEN + "Scenario Succeed" + Color.END)
 
 
 def generate_launch_description():
