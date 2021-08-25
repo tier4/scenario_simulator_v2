47f5082046a23cf3eac6973a54cdc3ba27a530a5
MasayaKataoka <ms.kataoka@gmail.com>
2021-08-25 14:10:41 +0900

    add EXECL FLAG
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- mock/cpp_mock_scenarios/src/follow_front_entity/accelerate_and_follow.cpp
+++ mock/cpp_mock_scenarios/src/follow_front_entity/accelerate_and_follow.cpp
@@ -44,12 +44,14 @@ private:
     double ego_twist = api_.getEntityStatus("ego").action_status.twist.linear.x;
     // double npc_accel = api_.getEntityStatus("npc").action_status.accel.linear.x;
     double npc_twist = api_.getEntityStatus("npc").action_status.twist.linear.x;
+    // LCOV_EXCL_START
     if (npc_twist > (ego_twist + 1) && ego_accel < 0) {
       stop(cpp_mock_scenarios::Result::FAILURE);
     }
     if (api_.checkCollision("ego", "npc")) {
       stop(cpp_mock_scenarios::Result::FAILURE);
     }
+    // LCOV_EXCL_STOP
     if (api_.getCurrentTime() >= 10) {
       stop(cpp_mock_scenarios::Result::SUCCESS);
     }
