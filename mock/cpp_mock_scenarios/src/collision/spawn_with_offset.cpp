19d2072a70f33c4e713cc74fb8301af31d40e726
MasayaKataoka <ms.kataoka@gmail.com>
2021-08-30 15:21:49 +0900

    fix typo
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- mock/cpp_mock_scenarios/src/collision/spawn_with_offset.cpp
+++ mock/cpp_mock_scenarios/src/collision/spawn_with_offset.cpp
@@ -31,7 +31,7 @@ class SpawnWithOffsetScenario : public cpp_mock_scenarios::CppScenarioNode
 public:
   explicit SpawnWithOffsetScenario(const rclcpp::NodeOptions & option)
   : cpp_mock_scenarios::CppScenarioNode(
-      "spanw_test",
+      "spawn_with_offset",
       ament_index_cpp::get_package_share_directory("cargo_delivery") + "/maps/kashiwa",
       "lanelet2_map_with_private_road_and_walkway_ele_fix.osm", __FILE__, false, option)
   {
