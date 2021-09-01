2f96a1da4c998764ea5d1ac24739f1dce961a0b0
MasayaKataoka <ms.kataoka@gmail.com>
2021-09-01 21:13:47 +0900

    add test case for getIDs
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- simulation/traffic_simulator/test/src/traffic_lights/test_traffic_light_manager.cpp
+++ simulation/traffic_simulator/test/src/traffic_lights/test_traffic_light_manager.cpp
@@ -14,12 +14,23 @@
 
 #include <gtest/gtest.h>
 
-#include <regex>
+#include <ament_index_cpp/get_package_share_directory.hpp>
 #include <scenario_simulator_exception/exception.hpp>
 #include <traffic_simulator/traffic_lights/traffic_light_manager.hpp>
 
-TEST(TrafficLights, makeLampState)
+TEST(TrafficLightManager, getIds)
 {
+  std::string path =
+    ament_index_cpp::get_package_share_directory("traffic_simulator") + "/map/lanelet2_map.osm";
+  geographic_msgs::msg::GeoPoint origin;
+  origin.latitude = 35.61836750154;
+  origin.longitude = 139.78066608243;
+  const auto hdmap_utils_ptr = std::make_shared<hdmap_utils::HdMapUtils>(path, origin);
+  traffic_simulator::TrafficLightManager manager(hdmap_utils_ptr, nullptr, nullptr, nullptr, "map");
+  const auto ids = manager.getIds();
+  EXPECT_EQ(std::count_if(ids.begin(), ids.end(), [](int x) { return x == 34836; }), 1);
+  EXPECT_EQ(std::count_if(ids.begin(), ids.end(), [](int x) { return x == 34802; }), 1);
+  EXPECT_EQ(ids.size(), static_cast<size_t>(2));
 }
 
 int main(int argc, char ** argv)
