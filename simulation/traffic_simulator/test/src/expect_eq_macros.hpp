d75dc708126d815d452ced21795c2aa76ba63c16
MasayaKataoka <ms.kataoka@gmail.com>
2021-09-07 13:24:52 +0900

    add test case for helper function
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- simulation/traffic_simulator/test/src/expect_eq_macros.hpp
+++ simulation/traffic_simulator/test/src/expect_eq_macros.hpp
@@ -18,10 +18,32 @@
 #include <gtest/gtest.h>
 
 #include <geometry_msgs/msg/point.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
 
-#define EXPECT_POINT_EQ(P0, P1) \
-  EXPECT_DOUBLE_EQ(P0.x, P1.x); \
-  EXPECT_DOUBLE_EQ(P0.y, P1.y); \
-  EXPECT_DOUBLE_EQ(P0.z, P1.z);
+#define EXPECT_POINT_EQ(DATA0, DATA1) \
+  EXPECT_DOUBLE_EQ(DATA0.x, DATA1.x); \
+  EXPECT_DOUBLE_EQ(DATA0.y, DATA1.y); \
+  EXPECT_DOUBLE_EQ(DATA0.z, DATA1.z);
+
+#define EXPECT_VECTOR3_EQ(DATA0, DATA1) \
+  EXPECT_DOUBLE_EQ(DATA0.x, DATA1.x);   \
+  EXPECT_DOUBLE_EQ(DATA0.y, DATA1.y);   \
+  EXPECT_DOUBLE_EQ(DATA0.z, DATA1.z);
+
+#define EXPECT_QUATERNION_EQ(DATA0, DATA1) \
+  EXPECT_DOUBLE_EQ(DATA0.x, DATA1.x);      \
+  EXPECT_DOUBLE_EQ(DATA0.y, DATA1.y);      \
+  EXPECT_DOUBLE_EQ(DATA0.z, DATA1.z);      \
+  EXPECT_DOUBLE_EQ(DATA0.w, DATA1.w);
+
+#define EXPECT_POSE_EQ(DATA0, DATA1)               \
+  EXPECT_POINT_EQ(DATA0.position, DATA1.position); \
+  EXPECT_QUATERNION_EQ(DATA0.orientation, DATA1.orientation);
+
+#define EXPECT_LANELET_POSE_EQ(DATA0, DATA1)     \
+  EXPECT_EQ(DATA0.lanelet_id, DATA1.lanelet_id); \
+  EXPECT_DOUBLE_EQ(DATA0.s, DATA1.s);            \
+  EXPECT_DOUBLE_EQ(DATA0.offset, DATA1.offset);  \
+  EXPECT_VECTOR3_EQ(DATA0.rpy, DATA1.rpy);
 
 #endif  // TRAFFIC_SIMULATOR__TEST__EXPECT_EQ_MACROS_HPP_
---
+++ simulation/traffic_simulator/test/src/helper/test_helper.cpp
@@ -0,0 +1,69 @@
+// Copyright 2015-2020 Tier IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include <regex>
+#include <scenario_simulator_exception/exception.hpp>
+#include <traffic_simulator/helper/helper.hpp>
+
+#include "../expect_eq_macros.hpp"
+
+TEST(HELPER, RPY)
+{
+  const auto rpy = traffic_simulator::helper::constructRPY(1, 2, 1);
+  geometry_msgs::msg::Vector3 vec;
+  vec.x = 1.0;
+  vec.y = 2.0;
+  vec.z = 1.0;
+  EXPECT_VECTOR3_EQ(rpy, vec);
+}
+
+TEST(HELPER, QUATERNION)
+{
+  const auto rpy =
+    traffic_simulator::helper::constructRPYfromQuaternion(geometry_msgs::msg::Quaternion());
+  EXPECT_VECTOR3_EQ(rpy, geometry_msgs::msg::Vector3());
+}
+
+TEST(HELPER, POSE)
+{
+  const auto pose = traffic_simulator::helper::constructPose(1, 1, 1, 0, 0, 0);
+  geometry_msgs::msg::Pose expect_pose;
+  expect_pose.position.x = 1;
+  expect_pose.position.y = 1;
+  expect_pose.position.z = 1;
+  EXPECT_POSE_EQ(pose, expect_pose);
+}
+
+TEST(HELPER, LANELET_POSE)
+{
+  const auto lanelet_pose = traffic_simulator::helper::constructLaneletPose(5, 10, 2, 0, 0, 0);
+  openscenario_msgs::msg::LaneletPose expected_pose;
+  expected_pose.lanelet_id = 5;
+  expected_pose.s = 10.0;
+  expected_pose.offset = 2.0;
+  expected_pose.rpy.x = 0;
+  expected_pose.rpy.y = 0;
+  expected_pose.rpy.z = 0;
+  EXPECT_LANELET_POSE_EQ(lanelet_pose, expected_pose);
+  std::stringstream ss;
+  ss << lanelet_pose;
+}
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
