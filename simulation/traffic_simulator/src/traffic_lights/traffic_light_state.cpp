e8aca5ec6d11f7945328a24247598f6e01c0ace2
MasayaKataoka <ms.kataoka@gmail.com>
2021-09-07 11:39:08 +0900

    add test case for operator <<
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- simulation/traffic_simulator/src/traffic_lights/traffic_light_state.cpp
+++ simulation/traffic_simulator/src/traffic_lights/traffic_light_state.cpp
@@ -55,7 +55,7 @@ std::ostream & operator<<(std::ostream & os, const TrafficLightColor & datum)
     case TrafficLightColor::RED:    return os << "red";
     case TrafficLightColor::GREEN:  return os << "green";
     case TrafficLightColor::YELLOW: return os << "yellow";
-    default:                        return os << "error";
+    default:                        return os << "error"; // LCOV_EXCL_LINE
   }
   // clang-format on
 }
@@ -124,7 +124,7 @@ std::ostream & operator<<(std::ostream & os, const TrafficLightArrow & datum)
     case TrafficLightArrow::STRAIGHT: return os << "straight";
     case TrafficLightArrow::LEFT:     return os << "left";
     case TrafficLightArrow::RIGHT:    return os << "right";
-    default:                          return os << "error";
+    default:                          return os << "error"; // LCOV_EXCL_LINE
   }
   // clang-format on
 }
--- simulation/traffic_simulator/test/src/traffic_lights/test_traffic_light_state.cpp
+++ simulation/traffic_simulator/test/src/traffic_lights/test_traffic_light_state.cpp
@@ -46,6 +46,27 @@ TEST(TrafficLights, makeLampState)
     std::out_of_range);
 }
 
+TEST(TraffigLights, OPERATOR_INSERT)
+{
+  std::stringstream ss;
+  ss << traffic_simulator::TrafficLightArrow::STRAIGHT;
+  EXPECT_STREQ(ss.str().c_str(), "straight");
+  ss << traffic_simulator::TrafficLightArrow::NONE;
+  EXPECT_STREQ(ss.str().c_str(), "none");
+  ss << traffic_simulator::TrafficLightArrow::LEFT;
+  EXPECT_STREQ(ss.str().c_str(), "left");
+  ss << traffic_simulator::TrafficLightArrow::RIGHT;
+  EXPECT_STREQ(ss.str().c_str(), "right");
+  ss << traffic_simulator::TrafficLightColor::NONE;
+  EXPECT_STREQ(ss.str().c_str(), "none");
+  ss << traffic_simulator::TrafficLightColor::RED;
+  EXPECT_STREQ(ss.str().c_str(), "red");
+  ss << traffic_simulator::TrafficLightColor::GREEN;
+  EXPECT_STREQ(ss.str().c_str(), "green");
+  ss << traffic_simulator::TrafficLightColor::YELLOW;
+  EXPECT_STREQ(ss.str().c_str(), "yellow");
+}
+
 int main(int argc, char ** argv)
 {
   testing::InitGoogleTest(&argc, argv);
