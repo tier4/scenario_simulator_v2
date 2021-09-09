99c761be70184727af2612e9eaa6fce3a5ef79ec
yamacir-kit <httperror@404-notfound.jp>
2021-09-09 15:07:28 +0900

    Cleanup member function `Interpreter::makeCurrentConfiguration`

--- openscenario/openscenario_interpreter/src/openscenario_interpreter.cpp
+++ openscenario/openscenario_interpreter/src/openscenario_interpreter.cpp
@@ -59,10 +59,10 @@ auto Interpreter::isSuccessIntended() const -> bool { return intended_result == 
 
 auto Interpreter::makeCurrentConfiguration() const -> traffic_simulator::Configuration
 {
+  const auto logic_file = script.as<OpenScenario>().global().logic_file;
+
   auto configuration = traffic_simulator::Configuration(
-    boost::filesystem::is_directory(script.as<OpenScenario>().global().logic_file)
-      ? script.as<OpenScenario>().global().logic_file
-      : script.as<OpenScenario>().global().logic_file.parent_path());
+    boost::filesystem::is_directory(logic_file) ? logic_file : logic_file.parent_path());
   {
     configuration.auto_sink = false;
 
@@ -72,10 +72,8 @@ auto Interpreter::makeCurrentConfiguration() const -> traffic_simulator::Configu
     configuration.scenario_path = osc_path;
 
     // XXX DIRTY HACK!!!
-    if (
-      not boost::filesystem::is_directory(script.as<OpenScenario>().global().logic_file) and
-      script.as<OpenScenario>().global().logic_file.extension() == ".osm") {
-      configuration.lanelet2_map_file = script.as<OpenScenario>().global().logic_file.filename().string();
+    if (not boost::filesystem::is_directory(logic_file) and logic_file.extension() == ".osm") {
+      configuration.lanelet2_map_file = logic_file.filename().string();
     }
   }
 
