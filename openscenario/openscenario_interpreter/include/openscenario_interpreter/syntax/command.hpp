56822fd877016d21c05b9973bf061fb6800024c3
yamacir-kit <httperror@404-notfound.jp>
2021-09-27 11:40:15 +0900

    Move class `Command` member implementation into .cpp

--- openscenario/openscenario_interpreter/include/openscenario_interpreter/syntax/command.hpp
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/syntax/command.hpp
@@ -15,8 +15,7 @@
 #ifndef OPENSCENARIO_INTERPRETER__SYNTAX__COMMAND_HPP_
 #define OPENSCENARIO_INTERPRETER__SYNTAX__COMMAND_HPP_
 
-#include <openscenario_interpreter/object.hpp>
-#include <string>
+#include <iostream>
 
 namespace openscenario_interpreter
 {
@@ -35,16 +34,8 @@ struct Command
 
   constexpr operator value_type() const noexcept { return value; }
 
-  auto & operator=(const value_type & rhs)
-  {
-    value = rhs;
-    return *this;
-  }
+  auto operator=(const value_type &) -> Command &;
 };
-
-static_assert(std::is_standard_layout<Command>::value, "");
-
-static_assert(std::is_trivial<Command>::value, "");
 
 std::istream & operator>>(std::istream &, Command &);
 
