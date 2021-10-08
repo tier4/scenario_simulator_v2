97f878ca73603e7c15cf5f3d43c320abff32936d
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-08 16:00:55 +0900

    enable pass compile in traffic_simulator
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- simulation/traffic_simulator/src/behavior/target_speed_planner.cpp
+++ simulation/traffic_simulator/src/behavior/target_speed_planner.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <behavior_tree_plugin/target_speed_planner.hpp>
 #include <iostream>
+#include <traffic_simulator/behavior/target_speed_planner.hpp>
 
 namespace traffic_simulator
 {
