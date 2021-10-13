1b039e9425f2de8f450f8de64c11588792e99a4e
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-13 14:50:58 +0900

    add debug output in mock node
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- simulation/behavior_tree_plugin/src/vehicle/behavior_tree.cpp
+++ simulation/behavior_tree_plugin/src/vehicle/behavior_tree.cpp
@@ -32,6 +32,7 @@ namespace entity_behavior
 {
 void VehicleBehaviorTree::configure()
 {
+  std::cout << __FILE__ << "," << __LINE__ << std::endl;
   std::string path = ament_index_cpp::get_package_share_directory("behavior_tree_plugin") +
                      "/config/vehicle_entity_behavior.xml";
   factory_.registerNodeType<entity_behavior::vehicle::follow_lane_sequence::FollowLaneAction>(
@@ -55,6 +56,7 @@ void VehicleBehaviorTree::configure()
   current_action_ = "root";
   setupLogger();
   setRequest("none");
+  std::cout << __FILE__ << "," << __LINE__ << std::endl;
 }
 
 void VehicleBehaviorTree::setupLogger()
@@ -79,17 +81,27 @@ void VehicleBehaviorTree::setupLogger()
 
 void VehicleBehaviorTree::update(double current_time, double step_time)
 {
+  std::cout << __FILE__ << "," << __LINE__ << std::endl;
   tickOnce(current_time, step_time);
+  std::cout << __FILE__ << "," << __LINE__ << std::endl;
   while (getCurrentAction() == "root") {
+    std::cout << __FILE__ << "," << __LINE__ << std::endl;
     tickOnce(current_time, step_time);
+    std::cout << __FILE__ << "," << __LINE__ << std::endl;
   }
+  std::cout << __FILE__ << "," << __LINE__ << std::endl;
 }
 
 BT::NodeStatus VehicleBehaviorTree::tickOnce(double current_time, double step_time)
 {
+  std::cout << __FILE__ << "," << __LINE__ << std::endl;
   setCurrentTime(current_time);
+  std::cout << __FILE__ << "," << __LINE__ << std::endl;
   setStepTime(step_time);
-  return tree_.rootNode()->executeTick();
+  std::cout << __FILE__ << "," << __LINE__ << std::endl;
+  const auto ret = tree_.rootNode()->executeTick();
+  std::cout << __FILE__ << "," << __LINE__ << std::endl;
+  return ret;
 }
 
 void VehicleBehaviorTree::callback(
