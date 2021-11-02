1d51bdee0066a39a3c108e13bdab570d6efabf51
yamacir-kit <httperror@404-notfound.jp>
2021-11-02 15:35:56 +0900

    Add new member function `teleport` to class `TeleportAction`

--- openscenario/openscenario_interpreter/src/syntax/teleport_action.cpp
+++ openscenario/openscenario_interpreter/src/syntax/teleport_action.cpp
@@ -28,9 +28,15 @@ TeleportAction::TeleportAction(const pugi::xml_node & node, Scope & scope)
 {
 }
 
-auto TeleportAction::accomplished() noexcept -> bool { return true; }
+auto TeleportAction::accomplished() noexcept -> bool  //
+{
+  return true;
+}
 
-auto TeleportAction::endsImmediately() noexcept -> bool { return true; }
+auto TeleportAction::endsImmediately() noexcept -> bool  //
+{
+  return true;
+}
 
 auto TeleportAction::evaluate() const -> Element
 {
@@ -40,31 +46,38 @@ auto TeleportAction::evaluate() const -> Element
 
 auto TeleportAction::run() const -> void
 {
-  auto teleport_action = overload(
-    [](const WorldPosition & position, const auto & actor) {
-      return applyTeleportAction(actor, static_cast<geometry_msgs::msg::Pose>(position));
-    },
-    [](const RelativeWorldPosition & position, const auto & actor) {
-      return applyTeleportAction(
-        actor,
-        position.reference,  // name
-        position,            // geometry_msgs::msg::Point
-        position.orientation);
-    },
-    [](const LanePosition & position, const auto & actor) {
-      return applyTeleportAction(
-        actor, static_cast<traffic_simulator_msgs::msg::LaneletPose>(position));
-    });
-
   for (const auto & actor : actors) {
     if (not global().entities.at(actor).as<ScenarioObject>().is_added) {
       AddEntityAction(localScope(), position)(actor);  // NOTE: Tier IV extension
     } else {
-      apply<void>(teleport_action, position, actor);
+      return teleport(actor, position);
     }
   }
 }
 
-auto TeleportAction::start() noexcept -> void {}
+auto TeleportAction::start() noexcept -> void  //
+{
+}
+
+auto TeleportAction::teleport(const EntityRef & entity_ref, const Position & position) -> void
+{
+  auto teleport = overload(
+    [&](const WorldPosition & position) {
+      return applyTeleportAction(entity_ref, static_cast<geometry_msgs::msg::Pose>(position));
+    },
+    [&](const RelativeWorldPosition & position) {
+      return applyTeleportAction(
+        entity_ref,
+        position.reference,  // name
+        position,            // geometry_msgs::msg::Point
+        position.orientation);
+    },
+    [&](const LanePosition & position) {
+      return applyTeleportAction(
+        entity_ref, static_cast<traffic_simulator_msgs::msg::LaneletPose>(position));
+    });
+
+  return apply<void>(teleport, position);
+}
 }  // namespace syntax
 }  // namespace openscenario_interpreter
