66c7490fab036a8dc0a91531ffd8e21426645793
MasayaKataoka <ms.kataoka@gmail.com>
2022-01-13 11:32:09 +0900

    remove unused line
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- mock/cpp_mock_scenarios/src/lane_change/lanechange_linear.cpp
+++ mock/cpp_mock_scenarios/src/lane_change/lanechange_linear.cpp
@@ -61,9 +61,6 @@ private:
       "ego", traffic_simulator::helper::constructLaneletPose(34462, 10, 0, 0, 0, 0),
       traffic_simulator::helper::constructActionStatus(10));
     api_.setTargetSpeed("ego", 10, true);
-    /*
-    api_.requestLaneChange("ego", traffic_simulator::lane_change::Direction::LEFT);
-    */
     api_.requestLaneChange(
       "ego",
       traffic_simulator::lane_change::RelativeTarget(
