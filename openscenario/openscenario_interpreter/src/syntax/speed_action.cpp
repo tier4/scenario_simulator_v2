ea5fceefc5e6bb9f2c6b87347d5f7d86a89e4a84
yamacir-kit <httperror@404-notfound.jp>
2022-01-14 16:39:01 +0900

    Update syntax `SpeedAction` to use new API `requestSpeedChange`

--- openscenario/openscenario_interpreter/src/syntax/speed_action.cpp
+++ openscenario/openscenario_interpreter/src/syntax/speed_action.cpp
@@ -29,9 +29,10 @@ SpeedAction::SpeedAction(const pugi::xml_node & node, Scope & scope)
 
 auto SpeedAction::accomplished() -> bool
 {
-  return std::all_of(std::begin(accomplishments), std::end(accomplishments), [](auto && each) {
-    return std::get<1>(each);
-  });
+  // return std::all_of(std::begin(accomplishments), std::end(accomplishments), [](auto && each) {
+  //   return std::get<1>(each);
+  // });
+  return true;  // NOTE: dummy
 }
 
 auto SpeedAction::endsImmediately() const -> bool
@@ -51,72 +52,72 @@ auto SpeedAction::reset() -> void
 
 auto SpeedAction::run() -> void
 {
-  for (auto && each : accomplishments) {
-    std::get<1>(each) = std::get<1>(each) or update(EntityRef(std::get<0>(each)));
-
-    // auto && actor = std::get<0>(each);
-    //
-    // if (speed_action_target.is<AbsoluteTargetSpeed>()) {
-    //   connection.requestSpeedChange(
-    //     actor,
-    //     speed_action_target.as<AbsoluteTargetSpeed>().value,
-    //     static_cast<traffic_simulator::SpeedChangeTransition>(
-    //       speed_action_dynamics.dynamics_shape),  // NOTE: implicit conversion
-    //     traffic_simulator::SpeedChangeConstraint(
-    //       static_cast<traffic_simulator::SpeedChangeConstraint::Type>(
-    //         speed_action_dynamics.dynamics_dimension),  // NOTE: implicit conversion
-    //       speed_action_dynamics.value),
-    //     true);
-    // } else {
-    //   connection.requestSpeedChange(
-    //     actor,
-    //     traffic_simulator::RelativeTargetSpeed(
-    //       speed_action_target.as<RelativeTargetSpeed>().entity_ref,
-    //       static_cast<traffic_simulator::RelativeTargetSpeed::Type>(
-    //         speed_action_target.as<RelativeTargetSpeed>().speed_target_value_type),  // NOTE: implicit conversion
-    //       speed_action_target.as<RelativeTargetSpeed>().value),
-    //     static_cast<traffic_simulator::SpeedChangeTransition>(
-    //       speed_action_dynamics.dynamics_shape),  // NOTE: implicit conversion
-    //     traffic_simulator::SpeedChangeConstraint(
-    //       static_cast<traffic_simulator::SpeedChangeConstraint::Type>(
-    //         speed_action_dynamics.dynamics_dimension),  // NOTE: implicit conversion
-    //       speed_action_dynamics.value),
-    //     speed_action_target.as<RelativeTargetSpeed>().continuous);
-    // }
-  }
+  // for (auto && each : accomplishments) {
+  //   std::get<1>(each) = std::get<1>(each) or update(EntityRef(std::get<0>(each)));
+  // }
 }
 
 auto SpeedAction::start() -> void
 {
   reset();
 
-  update = [this](const EntityRef & actor)  //
-  {
-    const auto get_current_absolute_target_speed =
-      speed_action_target.getCalculateAbsoluteTargetSpeed();
-
-    switch (speed_action_dynamics.dynamics_shape) {
-      case DynamicsShape::step: {
-        auto status = getEntityStatus(actor);
-        status.action_status.twist.linear.x = get_current_absolute_target_speed();
-        setEntityStatus(actor, status);
-        setTargetSpeed(actor, status.action_status.twist.linear.x, true);
-        break;
-      }
-      case DynamicsShape::linear:
-        setTargetSpeed(actor, get_current_absolute_target_speed(), true);
-        break;
-
-      default:
-        throw UNSUPPORTED_SETTING_DETECTED(SpeedAction, speed_action_dynamics.dynamics_shape);
+  for (auto && each : accomplishments) {
+    if (speed_action_target.is<AbsoluteTargetSpeed>()) {
+      connection.requestSpeedChange(
+        std::get<0>(each),
+        speed_action_target.as<AbsoluteTargetSpeed>().value,
+        static_cast<traffic_simulator::SpeedChangeTransition>(
+          speed_action_dynamics.dynamics_shape),  // NOTE: implicit conversion
+        traffic_simulator::SpeedChangeConstraint(
+          static_cast<traffic_simulator::SpeedChangeConstraint::Type>(
+            speed_action_dynamics.dynamics_dimension),  // NOTE: implicit conversion
+          speed_action_dynamics.value),
+        true);
+    } else {
+      connection.requestSpeedChange(
+        std::get<0>(each),
+        traffic_simulator::RelativeTargetSpeed(
+          speed_action_target.as<RelativeTargetSpeed>().entity_ref,
+          static_cast<traffic_simulator::RelativeTargetSpeed::Type>(
+            speed_action_target.as<RelativeTargetSpeed>().speed_target_value_type),  // NOTE: implicit conversion
+          speed_action_target.as<RelativeTargetSpeed>().value),
+        static_cast<traffic_simulator::SpeedChangeTransition>(
+          speed_action_dynamics.dynamics_shape),  // NOTE: implicit conversion
+        traffic_simulator::SpeedChangeConstraint(
+          static_cast<traffic_simulator::SpeedChangeConstraint::Type>(
+            speed_action_dynamics.dynamics_dimension),  // NOTE: implicit conversion
+          speed_action_dynamics.value),
+        speed_action_target.as<RelativeTargetSpeed>().continuous);
     }
+  }
 
-    if (speed_action_target.is<RelativeTargetSpeed>()) {
-      setTargetSpeed(actor, get_current_absolute_target_speed(), true);
-    }
-
-    return speed_action_target.getIsEnd()(actor);
-  };
+  // update = [this](const EntityRef & actor)  //
+  // {
+  //   const auto get_current_absolute_target_speed =
+  //     speed_action_target.getCalculateAbsoluteTargetSpeed();
+  //
+  //   switch (speed_action_dynamics.dynamics_shape) {
+  //     case DynamicsShape::step: {
+  //       auto status = getEntityStatus(actor);
+  //       status.action_status.twist.linear.x = get_current_absolute_target_speed();
+  //       setEntityStatus(actor, status);
+  //       setTargetSpeed(actor, status.action_status.twist.linear.x, true);
+  //       break;
+  //     }
+  //     case DynamicsShape::linear:
+  //       setTargetSpeed(actor, get_current_absolute_target_speed(), true);
+  //       break;
+  //
+  //     default:
+  //       throw UNSUPPORTED_SETTING_DETECTED(SpeedAction, speed_action_dynamics.dynamics_shape);
+  //   }
+  //
+  //   if (speed_action_target.is<RelativeTargetSpeed>()) {
+  //     setTargetSpeed(actor, get_current_absolute_target_speed(), true);
+  //   }
+  //
+  //   return speed_action_target.getIsEnd()(actor);
+  // };
 }
 }  // namespace syntax
 }  // namespace openscenario_interpreter
