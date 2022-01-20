f51e02b80b0f9fed09614eaf710557e9c0de0593
kyabe2718 <kyabetsu.3141@gmail.com>
2022-01-20 12:17:42 +0900

    fix missing #include

--- openscenario/openscenario_interpreter/include/openscenario_interpreter/openscenario_interpreter.hpp
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/openscenario_interpreter.hpp
@@ -21,6 +21,7 @@
 #include <lifecycle_msgs/msg/transition.hpp>
 #include <memory>
 #include <openscenario_interpreter/console/escape_sequence.hpp>
+#include <openscenario_interpreter/procedure.hpp>
 #include <openscenario_interpreter/syntax/custom_command_action.hpp>
 #include <openscenario_interpreter/syntax/openscenario.hpp>
 #include <openscenario_interpreter/utility/execution_timer.hpp>
