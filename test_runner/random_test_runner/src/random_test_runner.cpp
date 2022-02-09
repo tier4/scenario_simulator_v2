c010ac878c6aaf4b9a011b7090f04ae9f3806f8c
danielm1405 <daniel.marczak@robotec.ai>
2022-02-09 14:58:57 +0100

    refactor

--- test_runner/random_test_runner/src/random_test_runner.cpp
+++ test_runner/random_test_runner/src/random_test_runner.cpp
@@ -94,7 +94,9 @@ RandomTestRunner::RandomTestRunner(const rclcpp::NodeOptions & option)
         get_logger(), validated_params, test_case_parameters_vector[test_id], lanelet_utils)
         .generate(),
       error_reporter_.spawnTestCase(validated_params.name, std::to_string(test_id)),
-      test_control_parameters.simulator_type, get_logger());
+      test_control_parameters.simulator_type,
+      test_control_parameters.architecture_type,
+      get_logger());
     yaml_test_params_saver.addTestCase(test_case_parameters_vector[test_id], validated_params.name);
   }
 
@@ -139,6 +141,8 @@ TestControlParameters RandomTestRunner::collectAndValidateTestControlParameters(
   tp.test_count = this->declare_parameter<int>("test_count", 5);
   tp.simulator_type = simulatorTypeFromString(
     this->declare_parameter<std::string>("simulator_type", "simple_sensor_simulator"));
+  tp.architecture_type = architectureTypeFromString(
+    this->declare_parameter<std::string>("architecture_type", ""));
 
   if (!tp.input_dir.empty() && !boost::filesystem::is_directory(tp.input_dir)) {
     throw std::runtime_error(
@@ -151,17 +155,6 @@ TestControlParameters RandomTestRunner::collectAndValidateTestControlParameters(
   }
 
   return tp;
-}
-
-SimulatorType RandomTestRunner::simulatorTypeFromString(const std::string & simulator_type_str)
-{
-  if (simulator_type_str == "simple_sensor_simulator") {
-    return SimulatorType::SIMPLE_SENSOR_SIMULATOR;
-  } else if (simulator_type_str == "unity") {
-    return SimulatorType::UNITY;
-  }
-  throw std::runtime_error(
-    fmt::format("Failed to convert {} to simulation type", simulator_type_str));
 }
 
 TestSuiteParameters RandomTestRunner::validateParameters(
