3a752e3e774fcf55dd98d800ea4d59e3635bd3a6
MasayaKataoka <ms.kataoka@gmail.com>
2022-03-09 20:57:26 +0900

    remove old method
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- simulation/traffic_simulator/include/traffic_simulator/api/api.hpp
+++ simulation/traffic_simulator/include/traffic_simulator/api/api.hpp
@@ -240,19 +240,6 @@ private:
   bool updateEntityStatusInSim();
   bool updateTrafficLightsInSim();
 
-  /*
-  template <typename Parameters>
-  bool spawn(
-    const bool is_ego, const Parameters & parameters,
-    const traffic_simulator_msgs::msg::EntityStatus & status,
-    const traffic_simulator_msgs::msg::EntitySemantics & semantics =
-      PedestrianSemantics::defaultSemantics(),
-    const std::string & behavior = PedestrianBehavior::defaultBehavior())
-  {
-    return spawn(is_ego, parameters.toXml(), status, behavior);
-  }
-*/
-
   const Configuration configuration;
 
   const std::shared_ptr<traffic_simulator::entity::EntityManager> entity_manager_ptr_;
