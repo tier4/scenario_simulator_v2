d5ea107a15bc5929756f3e6382c62f04c357c259
Masaya Kataoka <ms.kataoka@gmail.com>
2022-05-17 14:51:52 +0900

    add Grid class
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ simulation/simple_sensor_simulator/include/simple_sensor_simulator/sensor_simulation/occupancy_grid/grid.hpp
@@ -0,0 +1,32 @@
+// Copyright 2015-2020 Tier IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SIMPLE_SENSOR_SIMULATOR__SENSOR_SIMULATION__OCCUPANCY_GRID__GRID_HPP_
+#define SIMPLE_SENSOR_SIMULATOR__SENSOR_SIMULATION__OCCUPANCY_GRID__GRID_HPP_
+
+#include <geometry_msgs/msg/point.hpp>
+
+namespace simple_sensor_simulator
+{
+class Grid
+{
+public:
+  Grid(double resolution, double height, double width);
+  const double resolution;
+  const double height;
+  const double width;
+};
+}  // namespace simple_sensor_simulator
+
+#endif  // SIMPLE_SENSOR_SIMULATOR__SENSOR_SIMULATION__OCCUPANCY_GRID__GRID_HPP_
