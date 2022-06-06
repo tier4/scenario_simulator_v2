bed9f9eb2c8b076e19aded8645f864a65bc4fbde
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-06-06 18:00:50 +0900

    Fix misses

--- simulation/traffic_simulator/include/traffic_simulator/metrics/metric_base.hpp
+++ simulation/traffic_simulator/include/traffic_simulator/metrics/metric_base.hpp
@@ -1,4 +1,4 @@
-// Copyright 20152021 TIER IV.inc. All rights reserved.
+// Copyright 2015 TIER IV.inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- simulation/traffic_simulator/include/traffic_simulator/vehicle_model/sim_model_util.hpp
+++ simulation/traffic_simulator/include/traffic_simulator/vehicle_model/sim_model_util.hpp
@@ -1,4 +1,4 @@
-// Copyright 20152020 Autoware Foundation. All rights reserved.
+// Copyright 2015 Autoware Foundation. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
