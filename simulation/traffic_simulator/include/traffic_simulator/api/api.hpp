f030cefe65cfc5a3a92d9439dc73c21e0abd2368
MasayaKataoka <ms.kataoka@gmail.com>
2022-06-09 18:43:49 +0900

    add definition in API class
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- simulation/traffic_simulator/include/traffic_simulator/api/api.hpp
+++ simulation/traffic_simulator/include/traffic_simulator/api/api.hpp
@@ -190,6 +190,8 @@ public:
   FORWARD_TO_ENTITY_MANAGER(entityExists);
   FORWARD_TO_ENTITY_MANAGER(getBoundingBoxDistance);
   FORWARD_TO_ENTITY_MANAGER(getCurrentAction);
+  FORWARD_TO_ENTITY_MANAGER(getDistanceToLeftBound);
+  FORWARD_TO_ENTITY_MANAGER(getDistanceToRightBound);
   FORWARD_TO_ENTITY_MANAGER(getDriverModel);
   FORWARD_TO_ENTITY_MANAGER(getEgoName);
   FORWARD_TO_ENTITY_MANAGER(getEmergencyStateName);
