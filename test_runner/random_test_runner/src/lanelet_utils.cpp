29ceff322e6d726ea6030ec628c82ff85d49be55
Masaya Kataoka <ms.kataoka@gmail.com>
2022-06-22 12:54:17 +0900

    apply reformat
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- test_runner/random_test_runner/src/lanelet_utils.cpp
+++ test_runner/random_test_runner/src/lanelet_utils.cpp
@@ -24,8 +24,8 @@
 #include <lanelet2_extension_psim/projection/mgrs_projector.hpp>
 
 #include "geographic_msgs/msg/geo_point.hpp"
-#include "traffic_simulator/hdmap_utils/hdmap_utils.hpp"
 #include "geometry_math/linear_algebra.hpp"
+#include "traffic_simulator/hdmap_utils/hdmap_utils.hpp"
 
 LaneletUtils::LaneletUtils(const boost::filesystem::path & filename)
 {
