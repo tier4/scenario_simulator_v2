fd46ac5648f89f8fe918ffdc5eed8668e9d0699a
Piotr Zyskowski <piotr.zyskowski@robotec.ai>
2022-09-01 00:03:30 +0200

    license information added

--- external/concealer/include/concealer/utility/service_with_validation.h
+++ external/concealer/include/concealer/utility/service_with_validation.h
@@ -1,5 +1,19 @@
-#ifndef SRC_SERVICE_INTERFACE_HPP
-#define SRC_SERVICE_INTERFACE_HPP
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CONCEALER__SERVICE_WITH_VALIDATION_HPP_
+#define CONCEALER__SERVICE_WITH_VALIDATION_HPP_
 
 #include <chrono>
 #include <string>
@@ -80,4 +94,4 @@ private:
 };
 }  // namespace concealer
 
-#endif  //SRC_SERVICE_INTERFACE_HPP
+#endif  //CONCEALER__SERVICE_WITH_VALIDATION_HPP_
