a8c6825fc14cf6490a023c9a514b0e061e925a54
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-09-07 18:08:26 +0900

    Add Deterministic

---
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/syntax/deterministic.hpp
@@ -0,0 +1,42 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef OPENSCENARIO_INTERPRETER__DETERMINISTIC_HPP_
+#define OPENSCENARIO_INTERPRETER__DETERMINISTIC_HPP_
+
+#include <openscenario_interpreter/scope.hpp>
+#include <pugixml.hpp>
+
+namespace openscenario_interpreter
+{
+inline namespace syntax
+{
+/* ---- Deterministic ---------------------------------------------------
+ *
+ * <xsd:complexType name="Deterministic">
+ *   <xsd:sequence>
+ *     <xsd:group ref="DeterministicParameterDistribution" minOccurs="0" maxOccurs="unbounded"/>
+ *   </xsd:sequence>
+ * </xsd:complexType>
+ *
+ * -------------------------------------------------------------------------- */
+struct Deterministic : public ComplexType
+{
+  const DeterministicParameterDistribution deterministic_parameter_distribution;
+
+  explicit Deterministic(const pugi::xml_node &, Scope & scope) {}
+};
+}  // namespace syntax
+}  // namespace openscenario_interpreter
+#endif  // OPENSCENARIO_INTERPRETER__DETERMINISTIC_HPP_
