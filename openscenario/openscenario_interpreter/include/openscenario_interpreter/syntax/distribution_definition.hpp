805056352dc51815be4a44c932be13387b0d6ab1
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-09-07 16:47:25 +0900

    Add DistributionDefinition

---
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/syntax/distribution_definition.hpp
@@ -0,0 +1,41 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef OPENSCENARIO_INTERPRETER__DISTRIBUTION_DEFINITION_HPP_
+#define OPENSCENARIO_INTERPRETER__DISTRIBUTION_DEFINITION_HPP_
+
+#include <openscenario_interpreter/scope.hpp>
+#include <pugixml.hpp>
+
+namespace openscenario_interpreter
+{
+inline namespace syntax
+{
+/* ---- DistributionDefinition ---------------------------------------------------
+ *
+ * <xsd:group name="DistributionDefinition">
+ *   <xsd:choice>
+ *     <xsd:element name="Deterministic" type="Deterministic"/>
+ *     <xsd:element name="Stochastic" type="Stochastic"/>
+ *   </xsd:choice>
+ * </xsd:group>
+ *
+ * -------------------------------------------------------------------------- */
+struct DistributionDefinition : public Group
+{
+  explicit DistributionDefinition(const pugi::xml_node &, Scope & scope);
+};
+}  // namespace syntax
+}  // namespace openscenario_interpreter
+#endif  // OPENSCENARIO_INTERPRETER__DISTRIBUTION_DEFINITION_HPP_
---
+++ openscenario/openscenario_interpreter/src/syntax/distribution_definition.cpp
@@ -0,0 +1,33 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <openscenario_interpreter/syntax/distribution_definition.hpp>
+#include <openscenario_interpreter/syntax/deterministic.hpp>
+#include <openscenario_interpreter/syntax/stochastic.hpp>
+
+namespace openscenario_interpreter
+{
+inline namespace syntax
+{
+
+DistributionDefinition::DistributionDefinition(const pugi::xml_node &tree, Scope & scope)
+: Group(
+    choice(tree,
+           std::make_pair("Deterministic", [&tree, &scope](auto && node){return make<Deterministic>(tree,scope);}),
+           std::make_pair("Stochastic",    [&tree, &scope](auto && node){return make<Stochastic>(tree,scope);})))
+{
+
+}
+}  // namespace syntax
+}  // namespace openscenario_interpreter
