7f3785b5b5924b9001c67c76265ddb3cdc4d7a24
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-09-13 19:08:33 +0900

    Add DeterministicMultiParameterDistributionType

---
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/syntax/deterministic_multi_parameter_distribution_type.hpp
@@ -0,0 +1,46 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef OPENSCENARIO_INTERPRETER__DETERMINISTIC_MULTI_PARAMETER_DISTRIBUTION_TYPE_HPP_
+#define OPENSCENARIO_INTERPRETER__DETERMINISTIC_MULTI_PARAMETER_DISTRIBUTION_TYPE_HPP_
+
+#include <openscenario_interpreter/scope.hpp>
+#include <openscenario_interpreter/syntax/value_set_distribution.hpp>
+#include <pugixml.hpp>
+
+namespace openscenario_interpreter
+{
+inline namespace syntax
+{
+/* ---- DeterministicMultiParameterDistribution ---------------------------------------------------
+ *
+ * <xsd:group name="DeterministicMultiParameterDistributionType">
+ *   <xsd:sequence>
+ *     <xsd:element name="ValueSetDistribution" type="ValueSetDistribution"/>
+ *   </xsd:sequence>
+ * </xsd:group>
+ *
+ * -------------------------------------------------------------------------- */
+struct DeterministicMultiParameterDistributionType : public Group
+{
+  explicit DeterministicMultiParameterDistributionType(const pugi::xml_node &, Scope & scope);
+};
+
+DEFINE_LAZY_VISITOR(
+  DeterministicMultiParameterDistributionType,
+  CASE(ValueSetDistribution),     //
+);
+}  // namespace syntax
+}  // namespace openscenario_interpreter
+#endif  // OPENSCENARIO_INTERPRETER__DETERMINISTIC_MULTI_PARAMETER_DISTRIBUTION_TYPE_HPP_
