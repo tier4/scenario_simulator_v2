0b1fd4ac21c23030e7a082e6782c0203909ae3e6
Masaya Kataoka <ms.kataoka@gmail.com>
2022-10-27 19:27:19 +0900

    use functions in base class
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation/behavior_tree_plugin/src/vehicle/vehicle_action_node.cpp
+++ simulation/behavior_tree_plugin/src/vehicle/vehicle_action_node.cpp
@@ -46,84 +46,8 @@ void VehicleActionNode::getBlackBoardValues()
 traffic_simulator_msgs::msg::EntityStatus VehicleActionNode::calculateEntityStatusUpdated(
   double target_speed)
 {
-  geometry_msgs::msg::Accel accel_new;
-  accel_new = entity_status.action_status.accel;
-  double target_accel = (target_speed - entity_status.action_status.twist.linear.x) / step_time;
-  if (entity_status.action_status.twist.linear.x > target_speed) {
-    target_accel = boost::algorithm::clamp(
-      target_accel, behavior_parameter.dynamic_constraints.max_deceleration * -1, 0);
-  } else {
-    target_accel = boost::algorithm::clamp(
-      target_accel, 0, behavior_parameter.dynamic_constraints.max_acceleration);
-  }
-  accel_new.linear.x = target_accel;
-  geometry_msgs::msg::Twist twist_new;
-  twist_new.linear.x = boost::algorithm::clamp(
-    entity_status.action_status.twist.linear.x + accel_new.linear.x * step_time, -10,
-    vehicle_parameters.performance.max_speed);
-  twist_new.linear.y = 0.0;
-  twist_new.linear.z = 0.0;
-  twist_new.angular.x = 0.0;
-  twist_new.angular.y = 0.0;
-  twist_new.angular.z = 0.0;
-  std::int64_t new_lanelet_id = entity_status.lanelet_pose.lanelet_id;
-  double new_s =
-    entity_status.lanelet_pose.s +
-    (twist_new.linear.x + entity_status.action_status.twist.linear.x) / 2.0 * step_time;
-  if (new_s < 0) {
-    auto previous_lanelet_ids =
-      hdmap_utils->getPreviousLaneletIds(entity_status.lanelet_pose.lanelet_id);
-    new_lanelet_id = previous_lanelet_ids[0];
-    new_s = new_s + hdmap_utils->getLaneletLength(new_lanelet_id) - 0.01;
-    traffic_simulator_msgs::msg::EntityStatus entity_status_updated;
-    entity_status_updated.time = current_time + step_time;
-    entity_status_updated.lanelet_pose.lanelet_id = new_lanelet_id;
-    entity_status_updated.lanelet_pose.s = new_s;
-    entity_status_updated.lanelet_pose.offset = entity_status.lanelet_pose.offset;
-    entity_status_updated.lanelet_pose.rpy = entity_status.lanelet_pose.rpy;
-    entity_status_updated.action_status.twist = twist_new;
-    entity_status_updated.action_status.accel = accel_new;
-    entity_status_updated.pose = hdmap_utils->toMapPose(entity_status_updated.lanelet_pose).pose;
-    return entity_status_updated;
-  } else {
-    bool calculation_success = false;
-    for (size_t i = 0; i < route_lanelets.size(); i++) {
-      if (route_lanelets[i] == entity_status.lanelet_pose.lanelet_id) {
-        double length = hdmap_utils->getLaneletLength(entity_status.lanelet_pose.lanelet_id);
-        calculation_success = true;
-        if (length < new_s) {
-          if (i != (route_lanelets.size() - 1)) {
-            new_s = new_s - length;
-            new_lanelet_id = route_lanelets[i + 1];
-            break;
-          } else {
-            new_s = new_s - length;
-            auto next_ids = hdmap_utils->getNextLaneletIds(route_lanelets[i]);
-            if (next_ids.empty()) {
-              return stopAtEndOfRoad();
-            }
-            new_lanelet_id = next_ids[0];
-            break;
-          }
-        }
-      }
-    }
-    if (!calculation_success) {
-      THROW_SIMULATION_ERROR(
-        "failed to calculate next status calculateEntityStatusUpdated function");
-    }
-    traffic_simulator_msgs::msg::EntityStatus entity_status_updated;
-    entity_status_updated.time = current_time + step_time;
-    entity_status_updated.lanelet_pose.lanelet_id = new_lanelet_id;
-    entity_status_updated.lanelet_pose.s = new_s;
-    entity_status_updated.lanelet_pose.offset = entity_status.lanelet_pose.offset;
-    entity_status_updated.lanelet_pose.rpy = entity_status.lanelet_pose.rpy;
-    entity_status_updated.pose = hdmap_utils->toMapPose(entity_status_updated.lanelet_pose).pose;
-    entity_status_updated.action_status.twist = twist_new;
-    entity_status_updated.action_status.accel = accel_new;
-    return entity_status_updated;
-  }
-  THROW_SIMULATION_ERROR("failed to calculate next status calculateEntityStatusUpdated function");
+  return ActionNode::calculateEntityStatusUpdated(
+    target_speed, vehicle_parameters.performance.max_speed, behavior_parameter.dynamic_constraints);
 }
 
 traffic_simulator_msgs::msg::EntityStatus
