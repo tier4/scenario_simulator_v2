d4f438cce03751e1cbb7f2d37958401d83d0f01b
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-11-11 20:20:40 +0900

    fix(heartbeat): fix deadlock
    Signed-off-by: Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>

--- openscenario/openscenario_interpreter/include/openscenario_interpreter/heartbeat_server.hpp
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/heartbeat_server.hpp
@@ -27,20 +27,23 @@ private:
 
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr service;
 
+  rclcpp::CallbackGroup::SharedPtr service_callback_group;
+
 public:
   explicit HeartbeatServer(rclcpp_lifecycle::LifecycleNode & node)
+  : service_callback_group(node.create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive))
   {
     using namespace std::placeholders;
     service = node.create_service<std_srvs::srv::Empty>(
       "heartbeat", std::bind(&HeartbeatServer::handleHeartbeat, this, _1, _2),
-      rmw_qos_profile_services_default);
+      rmw_qos_profile_services_default, service_callback_group);
   }
 
   auto handleHeartbeat(
     const std::shared_ptr<std_srvs::srv::Empty::Request> request,
     std::shared_ptr<std_srvs::srv::Empty::Response> response) -> void
   {
-    if (not update_notifier) {
+    if (update_notifier == nullptr) {
       update_notifier = std::make_shared<std::promise<void>>();
       auto future = update_notifier->get_future();
       future.get();
