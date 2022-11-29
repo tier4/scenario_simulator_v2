00d7d4b322b56fe294d6def352e73b9fcf4f281a
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-11-29 16:03:28 +0900

    feat(interpreter): add ParameterValueDistributionDefinition

--- openscenario/openscenario_interpreter/src/syntax/parameter_value_distribution_definition.cpp
+++ openscenario/openscenario_interpreter/src/syntax/parameter_value_distribution_definition.cpp
@@ -12,23 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <openscenario_interpreter/syntax/catalog_definition.hpp>
-#include <openscenario_interpreter/syntax/open_scenario_category.hpp>
+#include <openscenario_interpreter/reader/element.hpp>
 #include <openscenario_interpreter/syntax/parameter_value_distribution_definition.hpp>
-#include <openscenario_interpreter/syntax/scenario_definition.hpp>
 
 namespace openscenario_interpreter
 {
 inline namespace syntax
 {
-OpenScenarioCategory::OpenScenarioCategory(const pugi::xml_node & tree, Scope & scope)
-: Group(
-    // clang-format off
-    choice(tree,
-            std::make_pair("Storyboard",                [&](auto && node) { return make<ScenarioDefinition                  >(tree, scope);}),  // DIRTY HACK!!!
-            std::make_pair("Catalog",                   [&](auto && node) { return make<CatalogDefinition                   >(tree, scope);}),
-            std::make_pair("ParameterValueDistribution",[&](auto && node) { return make<ParameterValueDistributionDefinition>(node, scope);})))
-// clang-format on
+ParameterValueDistributionDefinition::ParameterValueDistributionDefinition(
+  const pugi::xml_node & node, Scope & scope)
+: ParameterValueDistribution(
+    readElement<ParameterValueDistribution>("parameterValueDistribution", node, scope))
 {
 }
 }  // namespace syntax
