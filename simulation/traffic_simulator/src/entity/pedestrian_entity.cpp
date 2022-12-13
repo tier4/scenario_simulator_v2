5c50255740e6a4914211dbe2287b9b9f8f183f34
hrjp <r115159@g.kobe-kosen.ac.jp>
2022-12-13 09:11:20 +0000

    requestAssignRoute support setGoalPoses

--- simulation/traffic_simulator/src/entity/pedestrian_entity.cpp
+++ simulation/traffic_simulator/src/entity/pedestrian_entity.cpp
@@ -50,9 +50,23 @@ void PedestrianEntity::requestAssignRoute(
   const std::vector<traffic_simulator_msgs::msg::LaneletPose> & waypoints)
 {
   behavior_plugin_ptr_->setRequest(behavior::Request::FOLLOW_LANE);
+  lanelet::Ids route_ids;
   if (status_.lanelet_pose_valid) {
-    route_planner_ptr_->getRouteLanelets(status_.lanelet_pose, waypoints);
+    route_ids=route_planner_ptr_->getRouteLanelets(status_.lanelet_pose, waypoints);
   }
+  const auto route_points=hdmap_utils_ptr_->getCenterPoints(route_ids);
+  goal_poses_.clear();
+  for(const auto & point : route_points){
+    geometry_msgs::msg::Pose pose;
+    pose.position=point;
+    if(const auto lanelet_pose =
+        hdmap_utils_ptr_->toLaneletPose(pose, getStatus().bounding_box, true)){
+      const auto map_pose_stamped =
+      hdmap_utils_ptr_->toMapPose(lanelet_pose.get().lanelet_id, lanelet_pose.get().s, lanelet_pose.get().offset);
+      goal_poses_.emplace_back(map_pose_stamped.pose);
+    }
+  }
+  behavior_plugin_ptr_->setGoalPoses(goal_poses_);
 }
 
 void PedestrianEntity::requestAssignRoute(const std::vector<geometry_msgs::msg::Pose> & waypoints)
@@ -127,7 +141,7 @@ void PedestrianEntity::requestAcquirePosition(
   }
   const auto map_pose_stamped =
     hdmap_utils_ptr_->toMapPose(lanelet_pose.lanelet_id, lanelet_pose.s, lanelet_pose.offset);
-  goal_poses_.emplace_back(map_pose_stamped.pose);
+  goal_poses_={map_pose_stamped.pose};
   behavior_plugin_ptr_->setGoalPoses(goal_poses_);
 }
 
