0a011e5b0a10ee8b34ecb403f8f93990f1a71c49
Masaya Kataoka <ms.kataoka@gmail.com>
2023-02-14 21:44:31 +0900

    Update simulation/traffic_simulator/src/hdmap_utils/hdmap_utils.cpp
    Co-authored-by: Tatsuya Yamasaki <httperror@404-notfound.jp>

--- simulation/traffic_simulator/src/hdmap_utils/hdmap_utils.cpp
+++ simulation/traffic_simulator/src/hdmap_utils/hdmap_utils.cpp
@@ -860,10 +860,9 @@ std::vector<std::int64_t> HdMapUtils::getPreviousLaneletIds(
 {
   std::vector<std::int64_t> ret;
   for (const auto & id : lanelet_ids) {
-    ret = concat(ret, getNextLaneletIds(id));
+    ret += getNextLaneletIds(id);
   }
-  sortAndUnique(ret);
-  return ret;
+  return sortAndUnique(ret);
 }
 
 std::vector<std::int64_t> HdMapUtils::getPreviousLaneletIds(
