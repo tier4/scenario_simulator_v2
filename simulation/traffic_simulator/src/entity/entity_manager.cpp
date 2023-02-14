0d32c32be53a1b825a28d407a268d2c82a8e2243
Masaya Kataoka <ms.kataoka@gmail.com>
2023-02-14 21:39:49 +0900

    add description for hard coded parameter
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation/traffic_simulator/src/entity/entity_manager.cpp
+++ simulation/traffic_simulator/src/entity/entity_manager.cpp
@@ -293,6 +293,10 @@ auto EntityManager::getLongitudinalDistance(
       hdmap_utils_ptr_->toMapPose(from).pose, from_pose.lanelet_id, 5.0,
       include_opposite_direction);
     from_poses.emplace_back(from);
+    /**
+    * @brief hard coded parameter!! 5.0 is a matching distance of the toLaneletPoses function. 
+    * A matching distance of about 1.5 lane widths is given as the matching distance to match the Entity present on the adjacent Lanelet.
+    */
     auto to_poses = hdmap_utils_ptr_->toLaneletPoses(
       hdmap_utils_ptr_->toMapPose(to).pose, to_pose.lanelet_id, 5.0, include_opposite_direction);
     to_poses.emplace_back(to);
