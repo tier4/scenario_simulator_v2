9484346312e1b799233fb2f412863c7c0c187305
Masaya Kataoka <ms.kataoka@gmail.com>
2023-02-22 10:45:51 +0900

    add lanelet pose data type
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ simulation/traffic_simulator/include/traffic_simulator/data_type/lanelet_pose.hpp
@@ -0,0 +1,45 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TRAFFIC_SIMULATOR__DATA_TYPE__LANELET_POSE_HPP_
+#define TRAFFIC_SIMULATOR__DATA_TYPE__LANELET_POSE_HPP_
+
+#include <traffic_simulator/hdmap_utils/hdmap_utils.hpp>
+
+namespace traffic_simulator
+{
+namespace lanelet_pose
+{
+class CanonicalizedLanePosition
+{
+public:
+  explicit CanonicalizedLanePosition(
+    const traffic_simulator_msgs::msg::LaneletPose & maybe_non_canonicalized_lanelet_pose,
+    const std::shared_ptr<hdmap_utils::HdMapUtils> & hdmap_utils);
+
+private:
+  auto canonicalize(
+    const traffic_simulator_msgs::msg::LaneletPose & may_non_canonicalized_lanelet_pose,
+    const std::shared_ptr<hdmap_utils::HdMapUtils> & hdmap_utils)
+    -> traffic_simulator_msgs::msg::LaneletPose;
+  const traffic_simulator_msgs::msg::LaneletPose lanelet_pose_;
+  explicit operator traffic_simulator_msgs::msg::LaneletPose() const noexcept
+  {
+    return lanelet_pose_;
+  }
+};
+}  // namespace lanelet_pose
+}  // namespace traffic_simulator
+
+#endif  // TRAFFIC_SIMULATOR__DATA_TYPE__LANELET_POSE_HPP_
---
+++ simulation/traffic_simulator/src/data_type/lanelet_pose.cpp
@@ -0,0 +1,51 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <scenario_simulator_exception/exception.hpp>
+#include <traffic_simulator/data_type/lanelet_pose.hpp>
+
+namespace traffic_simulator
+{
+namespace lanelet_pose
+{
+
+CanonicalizedLanePosition::CanonicalizedLanePosition(
+  const traffic_simulator_msgs::msg::LaneletPose & maybe_non_canonicalized_lanelet_pose,
+  const std::shared_ptr<hdmap_utils::HdMapUtils> & hdmap_utils)
+: lanelet_pose_(canonicalize(maybe_non_canonicalized_lanelet_pose, hdmap_utils))
+{
+}
+
+auto CanonicalizedLanePosition::canonicalize(
+  const traffic_simulator_msgs::msg::LaneletPose & may_non_canonicalized_lanelet_pose,
+  const std::shared_ptr<hdmap_utils::HdMapUtils> & hdmap_utils)
+  -> traffic_simulator_msgs::msg::LaneletPose
+{
+  if (
+    const auto canonicalized =
+      hdmap_utils->canonicalizeLaneletPose(may_non_canonicalized_lanelet_pose)) {
+    return canonicalized.get();
+  } else {
+    THROW_SEMANTIC_ERROR(
+      "Lanelet pose (id=", may_non_canonicalized_lanelet_pose.lanelet_id,
+      ",s=", may_non_canonicalized_lanelet_pose.s,
+      ",offset=", may_non_canonicalized_lanelet_pose.offset,
+      ",rpy.x=", may_non_canonicalized_lanelet_pose.rpy.x,
+      ",rpy.y=", may_non_canonicalized_lanelet_pose.rpy.y,
+      ",rpy.z=", may_non_canonicalized_lanelet_pose.rpy.z,
+      ") is invalid, please check lanelet length and connection.");
+  }
+}
+}  // namespace lanelet_pose
+}  // namespace traffic_simulator
