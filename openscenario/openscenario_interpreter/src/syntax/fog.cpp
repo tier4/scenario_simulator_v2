b64a37debb2ccce2dd8bca00fc2c32ba658dccb4
f0reachARR <f0reach@f0reach.me>
2023-03-22 15:55:10 +0900

    Add Fog tag syntax

---
+++ openscenario/openscenario_interpreter/src/syntax/fog.cpp
@@ -0,0 +1,34 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "openscenario_interpreter/reader/attribute.hpp"
+#include "openscenario_interpreter/reader/element.hpp"
+#include "openscenario_interpreter/syntax/bounding_box.hpp"
+#include "openscenario_interpreter/syntax/double.hpp"
+
+#include <openscenario_interpreter/syntax/fog.hpp>
+
+namespace openscenario_interpreter
+{
+inline namespace syntax
+{
+
+Fog::Fog(const pugi::xml_node & node, Scope & scope)
+: visualRange(readAttribute<Double>("visualRange", node, scope)),
+  boundingBox(readElement<BoundingBox>("BoundingBox", node, scope))
+{
+}
+
+}  // namespace syntax
+}  // namespace openscenario_interpreter
