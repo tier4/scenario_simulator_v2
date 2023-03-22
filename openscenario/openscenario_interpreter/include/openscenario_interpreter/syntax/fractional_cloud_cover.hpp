08ae57099354a1494e9ee9ba55d3d7f3f0df9bd7
f0reachARR <f0reach@f0reach.me>
2023-03-22 13:33:19 +0900

    Add FractionalCloudCover attribute type

---
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/syntax/fractional_cloud_cover.hpp
@@ -0,0 +1,77 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef OPENSCENARIO_INTERPRETER__SYNTAX__FRACTIONAL_CLOUD_COVER_HPP_
+#define OPENSCENARIO_INTERPRETER__SYNTAX__FRACTIONAL_CLOUD_COVER_HPP_
+
+#include <iostream>
+
+namespace openscenario_interpreter
+{
+inline namespace syntax
+{
+/* ---- FractionalCloudCover (OpenSCENARIO 1.2) --------------------------------
+ *
+ * <xsd:simpleType name="FractionalCloudCover">
+ *   <xsd:union>
+ *     <xsd:simpleType>
+ *       <xsd:restriction base="xsd:string">
+ *         <xsd:enumeration value="zeroOktas"/>
+ *         <xsd:enumeration value="oneOktas"/>
+ *         <xsd:enumeration value="twoOktas"/>
+ *         <xsd:enumeration value="threeOktas"/>
+ *         <xsd:enumeration value="fourOktas"/>
+ *         <xsd:enumeration value="fiveOktas"/>
+ *         <xsd:enumeration value="sixOktas"/>
+ *         <xsd:enumeration value="sevenOktas"/>
+ *         <xsd:enumeration value="eightOktas"/>
+ *         <xsd:enumeration value="nineOktas"/>
+ *       </xsd:restriction>
+ *     </xsd:simpleType>
+ *     <xsd:simpleType>
+ *       <xsd:restriction base="parameter"/>
+ *     </xsd:simpleType>
+ *   </xsd:union>
+ * </xsd:simpleType>
+ *
+ * -------------------------------------------------------------------------- */
+struct FractionalCloudCover
+{
+  enum value_type {
+    zeroOktas,
+    oneOktas,
+    twoOktas,
+    threeOktas,
+    fourOktas,
+    fiveOktas,
+    sixOktas,
+    sevenOktas,
+    eightOktas,
+    nineOktas
+  } value;
+
+  explicit FractionalCloudCover() = default;
+
+  constexpr FractionalCloudCover(value_type value) : value(value) {}
+
+  constexpr operator value_type() const noexcept { return value; }
+};
+
+auto operator>>(std::istream &, FractionalCloudCover &) -> std::istream &;
+
+auto operator<<(std::ostream &, const FractionalCloudCover &) -> std::ostream &;
+}  // namespace syntax
+}  // namespace openscenario_interpreter
+
+#endif  // OPENSCENARIO_INTERPRETER__SYNTAX__FRACTIONAL_CLOUD_COVER_HPP_
---
+++ openscenario/openscenario_interpreter/src/syntax/fractional_cloud_cover.cpp
@@ -0,0 +1,87 @@
+
+
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <openscenario_interpreter/error.hpp>
+#include <openscenario_interpreter/syntax/fractional_cloud_cover.hpp>
+
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+namespace openscenario_interpreter
+{
+inline namespace syntax
+{
+static_assert(std::is_standard_layout<FractionalCloudCover>::value, "");
+
+static_assert(std::is_trivial<FractionalCloudCover>::value, "");
+
+auto operator>>(std::istream & is, FractionalCloudCover & datum) -> std::istream &
+{
+  std::string buffer;
+
+  is >> buffer;
+
+#define BOILERPLATE(IDENTIFIER)                     \
+  if (buffer == #IDENTIFIER) {                      \
+    datum.value = FractionalCloudCover::IDENTIFIER; \
+    return is;                                      \
+  }                                                 \
+  static_assert(true, "")
+
+  // 不毛
+  BOILERPLATE(zeroOktas);
+  BOILERPLATE(oneOktas);
+  BOILERPLATE(twoOktas);
+  BOILERPLATE(threeOktas);
+  BOILERPLATE(fourOktas);
+  BOILERPLATE(fiveOktas);
+  BOILERPLATE(sixOktas);
+  BOILERPLATE(sevenOktas);
+  BOILERPLATE(eightOktas);
+  BOILERPLATE(nineOktas);
+
+#undef BOILERPLATE
+
+  throw UNEXPECTED_ENUMERATION_VALUE_SPECIFIED(FractionalCloudCover, buffer);
+}
+
+auto operator<<(std::ostream & os, const FractionalCloudCover & datum) -> std::ostream &
+{
+  switch (datum) {
+#define BOILERPLATE(ID)          \
+  case FractionalCloudCover::ID: \
+    return os << #ID;
+
+    BOILERPLATE(zeroOktas);
+    BOILERPLATE(oneOktas);
+    BOILERPLATE(twoOktas);
+    BOILERPLATE(threeOktas);
+    BOILERPLATE(fourOktas);
+    BOILERPLATE(fiveOktas);
+    BOILERPLATE(sixOktas);
+    BOILERPLATE(sevenOktas);
+    BOILERPLATE(eightOktas);
+    BOILERPLATE(nineOktas);
+
+#undef BOILERPLATE
+
+    default:
+      throw UNEXPECTED_ENUMERATION_VALUE_ASSIGNED(FractionalCloudCover, datum);
+  }
+}
+}  // namespace syntax
+}  // namespace openscenario_interpreter
