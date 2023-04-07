a91e8576b7996f2081b1f25976614b91d8722238
Masaya Kataoka <ms.kataoka@gmail.com>
2023-04-07 23:19:34 +0900

    fix typo
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation/behavior_tree_plugin/src/vehicle/lane_change_action.cpp
+++ simulation/behavior_tree_plugin/src/vehicle/lane_change_action.cpp
@@ -202,7 +202,7 @@ BT::NodeStatus LaneChangeAction::tick()
         accel_new.linear.x = target_accel;
         geometry_msgs::msg::Twist twist_new;
         /**
-         * @note Hard coded parameter, -10.0 is a minimin linear velocity of the entity.
+         * @note Hard coded parameter, -10.0 is a minimum linear velocity of the entity.
         */
         twist_new.linear.x = std::clamp(
           entity_status->getTwist().linear.x + accel_new.linear.x * step_time, -10.0,
