d2377bd7f9aaaf74f13a693b704764ad452392a4
Masaya Kataoka <ms.kataoka@gmail.com>
2023-04-28 13:13:18 +0900

    add do_nothing_plugin package
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ simulation/do_nothing_plugin/include/do_nothing_plugin/plugin.hpp
@@ -0,0 +1,74 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DO_NOTHING_PLUGIN__PEDESTRIAN__PLUGIN_HPP_
+#define DO_NOTHING_PLUGIN__PEDESTRIAN__PLUGIN_HPP_
+
+#include <traffic_simulator/behavior/behavior_plugin_base.hpp>
+
+namespace entity_behavior
+{
+class DoNothingBehavior : public BehaviorPluginBase
+{
+public:
+  void update(double current_time, double step_time) override;
+  void configure(const rclcpp::Logger & logger) override;
+  const std::string & getCurrentAction() const override;
+
+  auto getBehaviorParameter() -> traffic_simulator_msgs::msg::BehaviorParameter override;
+
+  auto setBehaviorParameter(const traffic_simulator_msgs::msg::BehaviorParameter &)
+    -> void override;
+
+#define DEFINE_GETTER_SETTER(NAME, TYPE)        \
+public:                                         \
+  TYPE get##NAME() override { return TYPE(); }; \
+  void set##NAME(const TYPE &) override{};
+  // clang-format off
+  DEFINE_GETTER_SETTER(DebugMarker,          std::vector<visualization_msgs::msg::Marker>)
+  DEFINE_GETTER_SETTER(EntityTypeList,       EntityTypeDict)
+  DEFINE_GETTER_SETTER(GoalPoses,            std::vector<geometry_msgs::msg::Pose>)
+  DEFINE_GETTER_SETTER(HdMapUtils,           std::shared_ptr<hdmap_utils::HdMapUtils>)
+  DEFINE_GETTER_SETTER(LaneChangeParameters, traffic_simulator::lane_change::Parameter)
+  DEFINE_GETTER_SETTER(Obstacle,             std::optional<traffic_simulator_msgs::msg::Obstacle>)
+  DEFINE_GETTER_SETTER(OtherEntityStatus,    EntityStatusDict)
+  DEFINE_GETTER_SETTER(PedestrianParameters, traffic_simulator_msgs::msg::PedestrianParameters)
+  DEFINE_GETTER_SETTER(ReferenceTrajectory,  std::shared_ptr<math::geometry::CatmullRomSpline>)
+  DEFINE_GETTER_SETTER(Request,              traffic_simulator::behavior::Request)
+  DEFINE_GETTER_SETTER(RouteLanelets,        std::vector<std::int64_t>)
+  DEFINE_GETTER_SETTER(TargetSpeed,          std::optional<double>)
+  DEFINE_GETTER_SETTER(TrafficLightManager,  std::shared_ptr<traffic_simulator::TrafficLightManagerBase>)
+  DEFINE_GETTER_SETTER(UpdatedStatus,        traffic_simulator_msgs::msg::EntityStatus)
+  DEFINE_GETTER_SETTER(VehicleParameters,    traffic_simulator_msgs::msg::VehicleParameters)
+  DEFINE_GETTER_SETTER(Waypoints,            traffic_simulator_msgs::msg::WaypointsArray)
+  // clang-format on
+#undef DEFINE_GETTER_SETTER
+
+#define DEFINE_GETTER_SETTER(NAME, TYPE, FIELD_NAME)                   \
+public:                                                                \
+  TYPE get##NAME() override { return FIELD_NAME; };                    \
+  void set##NAME(const TYPE & value) override { FIELD_NAME = value; }; \
+                                                                       \
+private:                                                               \
+  TYPE FIELD_NAME;
+  // clang-format off
+  DEFINE_GETTER_SETTER(CurrentTime,  double,                                    current_time_)
+  DEFINE_GETTER_SETTER(StepTime,     double,                                    step_time_)
+  DEFINE_GETTER_SETTER(EntityStatus, traffic_simulator_msgs::msg::EntityStatus, entity_status_)
+  // clang-format on
+#undef DEFINE_GETTER_SETTER
+};
+}  // namespace entity_behavior
+
+#endif  // DO_NOTHING_PLUGIN__PEDESTRIAN__PLUGIN_HPP_
---
+++ simulation/do_nothing_plugin/src/plugin.cpp
@@ -0,0 +1,31 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <do_nothing_plugin/plugin.hpp>
+
+namespace entity_behavior
+{
+void DoNothingBehavior::configure(const rclcpp::Logger &) {}
+
+void DoNothingBehavior::update(double current_time, double step_time)
+{
+  entity_status_.time = current_time;
+}
+
+const std::string & DoNothingBehavior::getCurrentAction() const
+{
+  static std::string behavior = "do_nothing";
+  return behavior;
+}
+}  // namespace entity_behavior
