bc0788e77e7eacd59796d77735a5d86f5013fcf2
Masaya Kataoka <ms.kataoka@gmail.com>
2023-05-08 19:15:34 +0900

    add note comments for test cases
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- common/math/geometry/test/test_polynomial_solver.cpp
+++ common/math/geometry/test/test_polynomial_solver.cpp
@@ -30,7 +30,7 @@ bool checkValueWithTolerance(double value, double expected, double tolerance)
 }
 
 /**
- * @note Testcase for ax+b = 0
+ * @note Testcase for ax+b
  */
 TEST(PolynomialSolverTest, LinearFunction)
 {
@@ -48,11 +48,19 @@ TEST(PolynomialSolverTest, SolveLinearEquation)
   math::geometry::PolynomialSolver solver;
   for (double a = -20; a < 20; a = a + 0.1) {
     for (double b = -20; b < 20; b = b + 0.1) {
+      /**
+       * @note If the ax+b=0 (a=0,b=0), any x value are solution, 
+       * so throwing common::SimulationError errors are expected.
+       */
       if (
         std::abs(a) <= math::geometry::PolynomialSolver::tolerance &&
         std::abs(b) <= math::geometry::PolynomialSolver::tolerance) {
         EXPECT_THROW(solver.solveLinearEquation(a, b, 0, 1), common::SimulationError);
-      } else {
+      }
+      /**
+       * @note Other cases, solver must be able to obtain an exact solution.
+       */
+      else {
         auto ret = solver.solveLinearEquation(a, b, 0, 1);
         for (const auto & solution : ret) {
           EXPECT_TRUE(checkValueWithTolerance(solver.linearFunction(a, b, solution), 0.0, 1e-10));
@@ -63,7 +71,7 @@ TEST(PolynomialSolverTest, SolveLinearEquation)
 }
 
 /**
- * @note Testcase for ax^2+bx+c = 0
+ * @note Testcase for ax^2+bx+c
  */
 TEST(PolynomialSolverTest, QuadraticFunction)
 {
@@ -82,12 +90,20 @@ TEST(PolynomialSolverTest, SolveQuadraticEquation)
   for (double a = -20; a < 20; a = a + 1) {
     for (double b = -20; b < 20; b = b + 1) {
       for (double c = -20; c < 20; c = c + 1) {
+        /**
+         * @note If the ax^2+bx+c=0 (a=0,b=0,c=0), any x value are solution, 
+         * so throwing common::SimulationError errors are expected.
+         */
         if (
           std::abs(a) <= math::geometry::PolynomialSolver::tolerance &&
           std::abs(b) <= math::geometry::PolynomialSolver::tolerance &&
           std::abs(c) <= math::geometry::PolynomialSolver::tolerance) {
           EXPECT_THROW(solver.solveQuadraticEquation(a, b, c, 0, 1), common::SimulationError);
-        } else {
+        }
+        /**
+         * @note Other cases, solver must be able to obtain an exact solution.
+         */
+        else {
           auto ret = solver.solveQuadraticEquation(a, b, c, 0, 1);
           for (const auto & solution : ret) {
             EXPECT_TRUE(
@@ -109,13 +125,21 @@ TEST(PolynomialSolverTest, SolveCubicEquation)
     for (double b = -10; b < 10; b = b + 1) {
       for (double c = -10; c < 10; c = c + 1) {
         for (double d = -10; d < 10; d = d + 1) {
+          /**
+           * @note If the ax^3+bx^2+cx+d=0 (a=0,b=0,c=0,d=0), any x value are solution, 
+           * so throwing common::SimulationError errors are expected.
+           */
           if (
             std::abs(a) <= math::geometry::PolynomialSolver::tolerance &&
             std::abs(b) <= math::geometry::PolynomialSolver::tolerance &&
             std::abs(c) <= math::geometry::PolynomialSolver::tolerance &&
             std::abs(d) <= math::geometry::PolynomialSolver::tolerance) {
             EXPECT_THROW(solver.solveQuadraticEquation(a, b, c, 0, 1), common::SimulationError);
-          } else {
+          }
+          /**
+           * @note Other cases, solver must be able to obtain an exact solution.
+           */
+          else {
             auto ret = solver.solveCubicEquation(a, b, c, d, 0, 1);
             for (const auto & solution : ret) {
               EXPECT_TRUE(
