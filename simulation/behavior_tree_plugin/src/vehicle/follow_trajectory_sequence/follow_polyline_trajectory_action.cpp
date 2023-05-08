9ffff9bc66fd3fe16de9f4075d10a155f045b893
yamacir-kit <httperror@404-notfound.jp>
2023-05-08 19:24:58 +0900

    Simplify outermost condition of `FollowPolylineTrajectoryAction::tick`
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

--- simulation/behavior_tree_plugin/src/vehicle/follow_trajectory_sequence/follow_polyline_trajectory_action.cpp
+++ simulation/behavior_tree_plugin/src/vehicle/follow_trajectory_sequence/follow_polyline_trajectory_action.cpp
@@ -175,174 +175,167 @@ auto truncate(T & v, const U & max)
 
 auto FollowPolylineTrajectoryAction::tick() -> BT::NodeStatus
 {
-  getBlackBoardValues();
+  if (getBlackBoardValues();
+      request == traffic_simulator::behavior::Request::FOLLOW_POLYLINE_TRAJECTORY and
+      getInput<decltype(parameter)>("polyline_trajectory_parameter", parameter) and
+      getInput<decltype(target_speed)>("target_speed", target_speed) and parameter and
+      not parameter->shape.vertices.empty()) {
+    // PRINT(parameter->initial_distance_offset);
+    // PRINT(parameter->dynamic_constraints_ignorable);
+    // PRINT(parameter->closed);
 
-  switch (request) {
-    case traffic_simulator::behavior::Request::FOLLOW_POLYLINE_TRAJECTORY:
-      if (
-        getInput<decltype(parameter)>("polyline_trajectory_parameter", parameter) and
-        getInput<decltype(target_speed)>("target_speed", target_speed) and
-        not parameter->shape.vertices.empty()) {
-        // PRINT(parameter->initial_distance_offset);
-        // PRINT(parameter->dynamic_constraints_ignorable);
-        // PRINT(parameter->closed);
-
-        auto waypoints_exhausted = [&]() { return parameter->shape.vertices.empty(); };
-
-        auto pop_current_waypoint = [&]() {
-          if (std::rotate(
-                std::begin(parameter->shape.vertices), std::begin(parameter->shape.vertices) + 1,
-                std::end(parameter->shape.vertices));
-              not parameter->closed) {
-            parameter->shape.vertices.pop_back();
-          }
-        };
-
-        auto current_waypoint = [&]() { return parameter->shape.vertices.front(); };
-
-        auto remain_time = [&]() {
-          // TODO std::find_first_of => iter != std::end => return *iter, else infinity
-          if (parameter->shape.vertices.front().time) {
-            return *parameter->shape.vertices.front().time - entity_status.time;
-          } else {
-            return std::numeric_limits<double>::infinity();
-          }
-        };
-
-        auto maximum_acceleration_currently_available = [&]() {
-          return std::clamp(
-            entity_status.action_status.accel.linear.x +
-              behavior_parameter.dynamic_constraints.max_acceleration_rate * step_time,
-            -behavior_parameter.dynamic_constraints.max_deceleration,
-            +behavior_parameter.dynamic_constraints.max_acceleration);
-        };
-
-        auto minimum_acceleration_currently_available = [&]() {
-          return std::clamp(
-            entity_status.action_status.accel.linear.x -
-              behavior_parameter.dynamic_constraints.max_deceleration_rate * step_time,
-            -behavior_parameter.dynamic_constraints.max_deceleration,
-            +behavior_parameter.dynamic_constraints.max_acceleration);
-        };
-
-        auto max_speed = [&]() {
-          return target_speed ? *target_speed : behavior_parameter.dynamic_constraints.max_speed;
-        };
-
-        auto adjust = [&](auto suggested_speed) {
-          std::cout << std::string(80, '-') << std::endl;
-
-          std::cout << "waypoint 1/" << parameter->shape.vertices.size() << std::endl;
-
-          if (current_waypoint().time) {
-            const auto distance_to_next_waypoint =
-              distance(entity_status.pose.position, current_waypoint().position.position);
-
-            const auto estimated_time_of_arrival = distance_to_next_waypoint / suggested_speed;
-
-            const auto desired_speed = distance_to_next_waypoint / remain_time();
-
-            PRINT(remain_time());
-            // PRINT(suggested_speed);
-            PRINT(estimated_time_of_arrival);
-            // PRINT(desired_speed);
-
-            if (remain_time() < estimated_time_of_arrival) {
-              std::cout << "TIME SHORTAGE => ACCELERATE" << std::endl;
-              auto accelerated_speed = std::min(
-                suggested_speed + maximum_acceleration_currently_available() * step_time,
-                desired_speed);
-              // PRINT(suggested_speed);
-              // PRINT(maximum_acceleration_currently_available());
-              // PRINT(maximum_acceleration_currently_available() * step_time);
-              // PRINT(suggested_speed + maximum_acceleration_currently_available() * step_time);
-              // PRINT(accelerated_speed);
-              return accelerated_speed;
-            } else if (estimated_time_of_arrival < remain_time()) {
-              std::cout << "TOO FAST => DECELERATE" << std::endl;
-              auto decelerated_speed = std::max(
-                suggested_speed + minimum_acceleration_currently_available() * step_time,
-                desired_speed);
-              // PRINT(suggested_speed);
-              // PRINT(minimum_acceleration_currently_available());
-              // PRINT(minimum_acceleration_currently_available() * step_time);
-              // PRINT(suggested_speed + minimum_acceleration_currently_available() * step_time);
-              // PRINT(desired_speed);
-              // PRINT(decelerated_speed);
-              return decelerated_speed;
-            } else {
-              std::cout << "OK" << std::endl;
-              return suggested_speed;
-            }
-          } else {
-            return suggested_speed;
-          }
-        };
-
-        auto steering =
-          [&](auto && current_position, auto && current_target, auto && current_velocity) {
-            // TODO: truncate by maximum_acceleration_currently_available() * step_time
-            return normalize(current_target - current_position) *
-                     behavior_parameter.dynamic_constraints.max_speed -
-                   current_velocity;
-          };  // vector [m/s]
-
-        if (remain_time() <= step_time) {
-          std::cout << "TIME OVER!!!" << std::endl;
-          if (parameter->dynamic_constraints_ignorable) {
-            std::cout << "TELEPORT!" << std::endl;
-            throw std::runtime_error("TELEPORT!");
-          } else {
-            pop_current_waypoint();
-          }
-        }
-
-        velocity +=
-          steering(entity_status.pose.position, current_waypoint().position.position, velocity);
-
-        truncate(velocity, adjust(entity_status.action_status.twist.linear.x));
-
-        auto previous_direction = direction;
-
-        direction.x = 0;
-        direction.y = 0;
-        direction.z = std::atan2(velocity.y, velocity.x);
-
-        auto updated_status = entity_status;
-
-        // clang-format off
-        updated_status.action_status.twist.linear.x = norm(velocity);
-        updated_status.action_status.twist.linear.y = 0;
-        updated_status.action_status.twist.linear.z = 0;
-        updated_status.action_status.twist.angular.x = 0;
-        updated_status.action_status.twist.angular.y = 0;
-        updated_status.action_status.twist.angular.z = (direction.z - previous_direction.z) / step_time;
-        updated_status.action_status.accel.linear = (updated_status.action_status.twist.linear - entity_status.action_status.twist.linear) / step_time;
-        updated_status.action_status.accel.angular = (updated_status.action_status.twist.angular - entity_status.action_status.twist.angular) / step_time;
-        updated_status.pose.position += velocity * step_time;
-        updated_status.pose.orientation = quaternion_operation::convertEulerAngleToQuaternion(direction);
-        updated_status.time = entity_status.time + step_time;
-        updated_status.lanelet_pose_valid = false;
-        // clang-format on
-
-        setOutput("updated_status", updated_status);
-        setOutput("waypoints", calculateWaypoints());
-        setOutput("obstacle", calculateObstacle(calculateWaypoints()));
-
-        if (auto d = distance(updated_status.pose.position, current_waypoint().position.position);
-            std::abs(d) <= std::numeric_limits<double>::epsilon()) {
-          std::cout << "REACHED!!!" << std::endl;
-          pop_current_waypoint();
-        } else {
-          PRINT(d);
-        }
-
-        return waypoints_exhausted() ? BT::NodeStatus::SUCCESS : BT::NodeStatus::RUNNING;
+    auto pop_current_waypoint = [&]() {
+      if (std::rotate(
+            std::begin(parameter->shape.vertices), std::begin(parameter->shape.vertices) + 1,
+            std::end(parameter->shape.vertices));
+          not parameter->closed) {
+        parameter->shape.vertices.pop_back();
       }
-      [[fallthrough]];
+    };
 
-    default:
-      return BT::NodeStatus::FAILURE;
+    auto current_waypoint = [&]() { return parameter->shape.vertices.front(); };
+
+    auto remain_time = [&]() {
+      // TODO std::find_first_of => iter != std::end => return *iter, else infinity
+      if (parameter->shape.vertices.front().time) {
+        return *parameter->shape.vertices.front().time - entity_status.time;
+      } else {
+        return std::numeric_limits<double>::infinity();
+      }
+    };
+
+    auto maximum_acceleration_currently_available = [&]() {
+      return std::clamp(
+        entity_status.action_status.accel.linear.x +
+          behavior_parameter.dynamic_constraints.max_acceleration_rate * step_time,
+        -behavior_parameter.dynamic_constraints.max_deceleration,
+        +behavior_parameter.dynamic_constraints.max_acceleration);
+    };
+
+    auto minimum_acceleration_currently_available = [&]() {
+      return std::clamp(
+        entity_status.action_status.accel.linear.x -
+          behavior_parameter.dynamic_constraints.max_deceleration_rate * step_time,
+        -behavior_parameter.dynamic_constraints.max_deceleration,
+        +behavior_parameter.dynamic_constraints.max_acceleration);
+    };
+
+    auto max_speed = [&]() {
+      return target_speed ? *target_speed : behavior_parameter.dynamic_constraints.max_speed;
+    };
+
+    auto adjust = [&](auto suggested_speed) {
+      std::cout << std::string(80, '-') << std::endl;
+
+      std::cout << "waypoint 1/" << parameter->shape.vertices.size() << std::endl;
+
+      if (current_waypoint().time) {
+        const auto distance_to_next_waypoint =
+          distance(entity_status.pose.position, current_waypoint().position.position);
+
+        const auto estimated_time_of_arrival = distance_to_next_waypoint / suggested_speed;
+
+        const auto desired_speed = distance_to_next_waypoint / remain_time();
+
+        PRINT(remain_time());
+        // PRINT(suggested_speed);
+        PRINT(estimated_time_of_arrival);
+        // PRINT(desired_speed);
+
+        if (remain_time() < estimated_time_of_arrival) {
+          std::cout << "TIME SHORTAGE => ACCELERATE" << std::endl;
+          auto accelerated_speed = std::min(
+            suggested_speed + maximum_acceleration_currently_available() * step_time,
+            desired_speed);
+          // PRINT(suggested_speed);
+          // PRINT(maximum_acceleration_currently_available());
+          // PRINT(maximum_acceleration_currently_available() * step_time);
+          // PRINT(suggested_speed + maximum_acceleration_currently_available() * step_time);
+          // PRINT(accelerated_speed);
+          return accelerated_speed;
+        } else if (estimated_time_of_arrival < remain_time()) {
+          std::cout << "TOO FAST => DECELERATE" << std::endl;
+          auto decelerated_speed = std::max(
+            suggested_speed + minimum_acceleration_currently_available() * step_time,
+            desired_speed);
+          // PRINT(suggested_speed);
+          // PRINT(minimum_acceleration_currently_available());
+          // PRINT(minimum_acceleration_currently_available() * step_time);
+          // PRINT(suggested_speed + minimum_acceleration_currently_available() * step_time);
+          // PRINT(desired_speed);
+          // PRINT(decelerated_speed);
+          return decelerated_speed;
+        } else {
+          std::cout << "OK" << std::endl;
+          return suggested_speed;
+        }
+      } else {
+        return suggested_speed;
+      }
+    };
+
+    auto steering = [&](
+                      auto && current_position, auto && current_target, auto && current_velocity) {
+      // TODO: truncate by maximum_acceleration_currently_available() *
+      // step_time
+      return normalize(current_target - current_position) *
+               behavior_parameter.dynamic_constraints.max_speed -
+             current_velocity;
+    };  // vector [m/s]
+
+    if (remain_time() <= step_time) {
+      std::cout << "TIME OVER!!!" << std::endl;
+      if (parameter->dynamic_constraints_ignorable) {
+        std::cout << "TELEPORT!" << std::endl;
+        throw std::runtime_error("TELEPORT!");
+      } else {
+        pop_current_waypoint();
+      }
+    }
+
+    velocity +=
+      steering(entity_status.pose.position, current_waypoint().position.position, velocity);
+
+    truncate(velocity, adjust(entity_status.action_status.twist.linear.x));
+
+    auto previous_direction = direction;
+
+    direction.x = 0;
+    direction.y = 0;
+    direction.z = std::atan2(velocity.y, velocity.x);
+
+    auto updated_status = entity_status;
+
+    // clang-format off
+    updated_status.action_status.twist.linear.x = norm(velocity);
+    updated_status.action_status.twist.linear.y = 0;
+    updated_status.action_status.twist.linear.z = 0;
+    updated_status.action_status.twist.angular.x = 0;
+    updated_status.action_status.twist.angular.y = 0;
+    updated_status.action_status.twist.angular.z = (direction.z - previous_direction.z) / step_time;
+    updated_status.action_status.accel.linear = (updated_status.action_status.twist.linear - entity_status.action_status.twist.linear) / step_time;
+    updated_status.action_status.accel.angular = (updated_status.action_status.twist.angular - entity_status.action_status.twist.angular) / step_time;
+    updated_status.pose.position += velocity * step_time;
+    updated_status.pose.orientation = quaternion_operation::convertEulerAngleToQuaternion(direction);
+    updated_status.time = entity_status.time + step_time;
+    updated_status.lanelet_pose_valid = false;
+    // clang-format on
+
+    setOutput("updated_status", updated_status);
+    setOutput("waypoints", calculateWaypoints());
+    setOutput("obstacle", calculateObstacle(calculateWaypoints()));
+
+    if (auto d = distance(updated_status.pose.position, current_waypoint().position.position);
+        std::abs(d) <= std::numeric_limits<double>::epsilon()) {
+      std::cout << "REACHED!!!" << std::endl;
+      pop_current_waypoint();
+    } else {
+      PRINT(d);
+    }
+
+    return parameter->shape.vertices.empty() ? BT::NodeStatus::SUCCESS : BT::NodeStatus::RUNNING;
+  } else {
+    return BT::NodeStatus::FAILURE;
   }
 }
 }  // namespace vehicle
