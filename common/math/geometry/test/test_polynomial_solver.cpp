fab4719ec1f51e3ca3f46707e750ada57e7cb2af
Masaya Kataoka <ms.kataoka@gmail.com>
2023-05-10 15:20:26 +0900

    update comment
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- common/math/geometry/test/test_polynomial_solver.cpp
+++ common/math/geometry/test/test_polynomial_solver.cpp
@@ -37,7 +37,7 @@ TEST(PolynomialSolverTest, LinearFunction)
   math::geometry::PolynomialSolver solver;
   EXPECT_DOUBLE_EQ(solver.linearFunction(1, 1, 2), 3);  // x + 1 = 3 (x=2)
   EXPECT_DOUBLE_EQ(solver.linearFunction(1, 0, 2), 2);  // x + 0 = 2 (x=2)
-  EXPECT_DOUBLE_EQ(solver.linearFunction(0, 0, 2), 0);  // 0 (x=2)
+  EXPECT_DOUBLE_EQ(solver.linearFunction(0, 0, 2), 0);  //     0 = 0 (x=2)
 }
 
 /**
@@ -78,7 +78,7 @@ TEST(PolynomialSolverTest, QuadraticFunction)
   math::geometry::PolynomialSolver solver;
   EXPECT_DOUBLE_EQ(solver.quadraticFunction(1, 1, 1, 2), 7);  // x^2 + x + 1 = 7 (x=2)
   EXPECT_DOUBLE_EQ(solver.quadraticFunction(1, 1, 0, 2), 6);  // x^2 + x + 0 = 6 (x=2)
-  EXPECT_DOUBLE_EQ(solver.quadraticFunction(0, 0, 0, 2), 0);  // 0 (x=2)
+  EXPECT_DOUBLE_EQ(solver.quadraticFunction(0, 0, 0, 2), 0);  //           0 = 0 (x=2)
 }
 
 /**
