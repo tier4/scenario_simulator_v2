de1c751b318756610121b224e5bd5ea929efa5fc
Masaya Kataoka <ms.kataoka@gmail.com>
2023-05-10 14:55:40 +0900

    reduce line
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- common/math/geometry/src/solver/polynomial_solver.cpp
+++ common/math/geometry/src/solver/polynomial_solver.cpp
@@ -51,9 +51,7 @@ auto PolynomialSolver::solveLinearEquation(
     if (std::abs(b) <= tolerance) {
       THROW_SIMULATION_ERROR(
         "Not computable because a=0 in the linear equation ", a, " x + ", b,
-        "=0, "
-        "so any value of x = ",
-        min_value, "~", max_value, " will be the solution.",
+        "=0, so any value of x = ", min_value, "~", max_value, " will be the solution.",
         "There are no expected cases where this exception is thrown.",
         "Please contact the scenario_simulator_v2 developers, ",
         "especially Masaya Kataoka (@hakuturu583).");
