9d2666b9fcbd6df4611e5c7fc2ca31c21de8bd4e
Shota Minami <shouth373@gmail.com>
2023-05-30 15:28:27 +0900

    Add entity enumeration

--- openscenario/openscenario_interpreter/src/syntax/selected_entities.cpp
+++ openscenario/openscenario_interpreter/src/syntax/selected_entities.cpp
@@ -17,6 +17,8 @@
 #include <openscenario_interpreter/scope.hpp>
 #include <openscenario_interpreter/syntax/by_type.hpp>
 #include <openscenario_interpreter/syntax/entity_ref.hpp>
+#include <openscenario_interpreter/syntax/entity_selection.hpp>
+#include <openscenario_interpreter/syntax/scenario_object.hpp>
 #include <openscenario_interpreter/syntax/selected_entities.hpp>
 #include <utility>
 
@@ -29,9 +31,49 @@ SelectedEntityRefs::SelectedEntityRefs(const pugi::xml_node & tree, Scope & scop
 {
 }
 
+auto SelectedEntityRefs::enumerate(const Entities & entities) -> std::list<String>
+{
+  auto selected_entities = std::list<String>();
+  for (const auto & entityRef : entityRefs) {
+    auto object = entities.ref(entityRef);
+    if (object.is<ScenarioObject>()) {
+      selected_entities.emplace_back(entityRef);
+    } else if (object.is<EntitySelection>()) {
+      selected_entities.merge(object.as<EntitySelection>().enumerate(entities));
+    } else {
+      THROW_SYNTAX_ERROR(
+        "Unexpected entity ", std::quoted(entityRef), " of type ",
+        std::quoted(object->type().name()), " is detected; this is a bug");
+    }
+  }
+  return selected_entities;
+}
+
 SelectedByTypes::SelectedByTypes(const pugi::xml_node & tree, Scope & scope)
 : byTypes(readElements<ByType, 0>("ByType", tree, scope))
 {
+}
+
+auto SelectedByTypes::enumerate(const Entities & entities) -> std::list<String>
+{
+  auto selected_entities = std::list<String>();
+  for (const auto & [name, object] : entities) {
+    for (const auto & byType : byTypes) {
+      if (
+        (object.is<Vehicle>() and byType.objectType == ObjectType::vehicle) or
+        (object.is<Pedestrian>() and byType.objectType == ObjectType::pedestrian) or
+        (object.is<MiscObject>() and byType.objectType == ObjectType::miscellaneous)) {
+        selected_entities.emplace_back(name);
+      } else if (byType.objectType == ObjectType::external) {
+        THROW_SYNTAX_ERROR("Selecting external object is not supported yet");
+      } else {
+        THROW_SYNTAX_ERROR(
+          "Unexpected entity ", std::quoted(name), " of type ",
+          std::quoted(object->type().name()), " is detected; this is a bug");
+      }
+    }
+  }
+  return selected_entities;
 }
 
 SelectedEntities::SelectedEntities(const pugi::xml_node & tree, Scope & scope)
@@ -42,6 +84,11 @@ SelectedEntities::SelectedEntities(const pugi::xml_node & tree, Scope & scope)
       std::make_pair(   "ByType", [&](const auto & tree) { return make<   SelectedByTypes>(tree.parent(), scope); })))
 // clang-format on
 {
+}
+
+auto SelectedEntities::enumerate(const Entities & entities) -> std::list<String>
+{
+  return apply<std::list<String>>([&](auto & e) { return e.enumerate(entities); }, *this);
 }
 }  // namespace syntax
 }  // namespace openscenario_interpreter
