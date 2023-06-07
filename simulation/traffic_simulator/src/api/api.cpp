67b70f6ca2f3fe17a952048e620e8c0276e10ff4
Piotr Zyskowski <piotr.zyskowski@robotec.ai>
2023-06-07 16:11:39 +0200

    reintroduced entity publishing

--- simulation/traffic_simulator/src/api/api.cpp
+++ simulation/traffic_simulator/src/api/api.cpp
@@ -334,13 +334,13 @@ bool API::updateFrame()
     if (!res.result().success()) {
       return false;
     }
-//    entity_manager_ptr_->broadcastEntityTransform();
+    entity_manager_ptr_->broadcastEntityTransform();
     clock_.update();
 //    clock_pub_->publish(clock_.getCurrentRosTimeAsMsg());
     debug_marker_pub_->publish(entity_manager_ptr_->makeDebugMarker());
     return true;
   } else {
-//    entity_manager_ptr_->broadcastEntityTransform();
+    entity_manager_ptr_->broadcastEntityTransform();
     clock_.update();
 //    clock_pub_->publish(clock_.getCurrentRosTimeAsMsg());
     debug_marker_pub_->publish(entity_manager_ptr_->makeDebugMarker());
