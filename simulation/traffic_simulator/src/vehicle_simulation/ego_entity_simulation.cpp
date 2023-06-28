21c66fcee6984144d124f1ea06689dbdad585abe
Lukasz Chojnacki <lukasz.chojnacki@robotec.ai>
2023-06-28 14:32:38 +0200

    Fix formatting
    Signed-off-by: Lukasz Chojnacki <lukasz.chojnacki@robotec.ai>

--- simulation/traffic_simulator/src/vehicle_simulation/ego_entity_simulation.cpp
+++ simulation/traffic_simulator/src/vehicle_simulation/ego_entity_simulation.cpp
@@ -88,15 +88,15 @@ auto EgoEntitySimulation::makeSimulationModel(
   // clang-format off
   const auto acc_time_constant   = getParameter<double>("acc_time_constant",     0.1);
   const auto acc_time_delay      = getParameter<double>("acc_time_delay",        0.1);
-  const auto steer_lim           = getParameter<double>("steer_lim",            parameters.axles.front_axle.max_steering);  // 1.0
+  const auto steer_lim           = getParameter<double>("steer_lim",             parameters.axles.front_axle.max_steering);  // 1.0
   const auto steer_rate_lim      = getParameter<double>("steer_rate_lim",        5.0);
   const auto steer_time_constant = getParameter<double>("steer_time_constant",   0.27);
   const auto steer_time_delay    = getParameter<double>("steer_time_delay",      0.24);
-  const auto vel_lim             = getParameter<double>("vel_lim",              parameters.performance.max_speed);  // 50.0
-  const auto vel_rate_lim        = getParameter<double>("vel_rate_lim",         parameters.performance.max_acceleration);  // 7.0
+  const auto vel_lim             = getParameter<double>("vel_lim",               parameters.performance.max_speed);  // 50.0
+  const auto vel_rate_lim        = getParameter<double>("vel_rate_lim",          parameters.performance.max_acceleration);  // 7.0
   const auto vel_time_constant   = getParameter<double>("vel_time_constant",     0.1);
   const auto vel_time_delay      = getParameter<double>("vel_time_delay",        0.1);
-  const auto wheel_base          = getParameter<double>("wheel_base",           parameters.axles.front_axle.position_x - parameters.axles.rear_axle.position_x);
+  const auto wheel_base          = getParameter<double>("wheel_base",            parameters.axles.front_axle.position_x - parameters.axles.rear_axle.position_x);
   // clang-format on
 
   switch (vehicle_model_type) {
