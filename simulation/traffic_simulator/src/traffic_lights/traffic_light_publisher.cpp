372abf0e5570a6b3a446dfedd2a7e9e1f8ad6492
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-07-31 23:42:39 +0900

    feat(traffic_simulator): implement conversion from way_id to relation_id

--- simulation/traffic_simulator/src/traffic_lights/traffic_light_publisher.cpp
+++ simulation/traffic_simulator/src/traffic_lights/traffic_light_publisher.cpp
@@ -48,13 +48,24 @@ auto TrafficLightPublisher<autoware_perception_msgs::msg::TrafficSignalArray>::p
   const simulation_api_schema::UpdateTrafficLightsRequest & request) -> void
 {
   assert(hdmap_utils_ != nullptr);
+  // cache store for conversion from way_id to relation_id
+  static std::unordered_map<std::int64_t, std::int64_t> id_conversion_cache_map;
+
   autoware_perception_msgs::msg::TrafficSignalArray msg;
   msg.stamp = current_ros_time;
   for (const auto & traffic_light : request.states()) {
     using TrafficLightType = autoware_perception_msgs::msg::TrafficSignal;
     TrafficLightType traffic_light_msg;
-    //    traffic_signal. = traffic_light.id();
-    // TODO
+
+    if(auto relation_id = id_conversion_cache_map.find(traffic_light.id()); relation_id!= id_conversion_cache_map.end()) {
+      // use cached relation_id if the conversion is already cached
+      traffic_light_msg.traffic_signal_id = relation_id->second;
+    }else {
+      traffic_light_msg.traffic_signal_id = hdmap_utils_->getTrafficLightRelationIDFromWayID(traffic_light.id());
+      // cache the conversion
+      id_conversion_cache_map[traffic_light.id()] = traffic_light_msg.traffic_signal_id;
+    }
+
     for (auto bulb_status : traffic_light.traffic_light_status()) {
       using TrafficLightBulbType =
         autoware_perception_msgs::msg::TrafficSignal::_elements_type::value_type;
