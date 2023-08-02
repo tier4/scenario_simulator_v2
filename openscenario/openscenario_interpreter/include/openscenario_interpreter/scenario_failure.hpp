7dd2ea7b79df06fd2575f3cf899bfe717d1e5d24
Dawid Moszyński <dawid.moszynski@robotec.ai>
2023-08-02 12:13:58 +0200

    ref(openscenario_interpreter): improve ScenarioFailure

--- openscenario/openscenario_interpreter/include/openscenario_interpreter/scenario_failure.hpp
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/scenario_failure.hpp
@@ -25,121 +25,128 @@
 namespace openscenario_interpreter
 {
 
-struct Core
-{
-  Core(bool init_action_source) : _init_action{init_action_source} {}
-
-  Core(std::vector<std::pair<std::string, std::string>> conditions_list)
-  : _single{conditions_list.size() == 1}
-  {
-    _anonymous = std::any_of(conditions_list.begin(), conditions_list.end(), [](const auto & pair) {
-      return pair.first.empty();
-    });
-
-    if (_single) {
-      _name = "\"" + conditions_list.at(0).first + "\"";
-      _description = conditions_list.at(0).second;
-    } else {
-      size_t index{0};
-      for (const auto & condition : conditions_list) {
-        _name += "\"" + condition.first + "\", ";
-        if (!_anonymous) {
-          _description += "{\"" + condition.first + "\": " + condition.second + "}, ";
-        } else {
-          _description += "{\"" + std::to_string(index++) + "\": " + condition.second + "}, ";
-        }
-      }
-      _name.erase(_name.length() - 2);
-      _description.erase(_description.length() - 2);
-    }
-  }
-
-  auto log() const -> const std::string & { return _log; }
-
-  auto logUpdate(std::string path) -> std::string
-  {
-    std::string prolog = "CustomCommandAction typed \"exitFailure\" was triggered by the ";
-    if (_init_action) {
-      _log = prolog + "anonymous init action (" + path + ")";
-    } else if (_anonymous && _single) {
-      _log = prolog + "anonymous condition (" + path + ".Condition[0]): " + _description;
-    } else if (_single) {
-      _log = prolog + "Condition named " + _name + ": " + _description;
-    } else if (_anonymous && !_single) {
-      _log = prolog + "anonymous conditions (" + path + ".Condition[...]): " + _description;
-    } else {
-      _log = prolog + "Conditions named {" + _name + "}: " + _description;
-    }
-    return _log;
-  }
-
-private:
-  bool _anonymous{false}, _single{true}, _init_action{false};
-  std::string _name;
-  std::string _description;
-  std::string _log;
-};
-
 struct ScenarioFailure : public std::runtime_error
 {
+  struct CoreSource
+  {
+    CoreSource(bool init_action_source) : init_action_{init_action_source} {}
+
+    CoreSource(std::vector<std::pair<std::string, std::string>> conditions_list)
+    : single_{conditions_list.size() == 1}
+    {
+      anonymous_ = std::any_of(
+        conditions_list.begin(), conditions_list.end(),
+        [](const auto & pair) { return pair.first.empty(); });
+
+      std::stringstream name, description;
+      if (single_) {
+        name << std::quoted(conditions_list.front().first);
+        description << conditions_list.front().second;
+      } else {
+        for (size_t index = 0; const auto & condition : conditions_list) {
+          name << std::quoted(condition.first) << ", ";
+          description << "{" << std::quoted(anonymous_ ? std::to_string(index++) : condition.first)
+                      << ": " << condition.second << "}, ";
+        }
+        name.seekp(-2, std::ios_base::end);
+        description.seekp(-2, std::ios_base::end);
+      }
+      name_ = name.str();
+      description_ = description.str();
+    }
+
+    auto log() const -> const std::string & { return log_; }
+
+    auto logUpdate(std::string path) -> std::string
+    {
+      std::stringstream log;
+      log << "CustomCommandAction typed " << std::quoted("exitFailure") << " was triggered by the ";
+      if (init_action_) {
+        log << "anonymous init action (" << path << ")";
+      } else if (anonymous_ && single_) {
+        log << "anonymous condition (" << path << ".Condition[0]): " << description_;
+      } else if (single_) {
+        log << "Condition named " << name_ << ": " << description_;
+      } else if (anonymous_) {
+        log << "anonymous conditions (" << path << ".Condition[...]): " << description_;
+      } else {
+        log << "Conditions named {" << name_ << "}: " << description_;
+      }
+      log_ = log.str();
+      return log_;
+    }
+
+  private:
+    bool anonymous_{false}, single_{true}, init_action_{false};
+    std::string name_;
+    std::string description_;
+    std::string log_;
+  };
+
   ScenarioFailure(ScenarioFailure const &) = default;
 
+  // Constructor with no inner object -- first object
   explicit ScenarioFailure(std::string source_name, int index, std::string name)
-  : _source_name{source_name},
-    _index{index},
-    _inner{nullptr},
-    _core{nullptr},
+  : source_name_{source_name},
+    index_{index},
+    inner_{nullptr},
+    core_{nullptr},
     std::runtime_error{ScenarioFailure::formatName("", index, name)}
   {
   }
 
-  explicit ScenarioFailure(std::string name, ScenarioFailure const & inner)
-  : _inner{std::make_shared<ScenarioFailure>(inner)},
-    _core{inner._core},
-    std::runtime_error{ScenarioFailure::formatName(inner._source_name, name)}
-  {
-  }
-
+  // Constructor with inner object and current source (to be forwarded) -- intermediate object
   explicit ScenarioFailure(
     std::string source_name, int index, std::string name, ScenarioFailure const & inner)
-  : _source_name{source_name},
-    _index{index},
-    _inner{std::make_shared<ScenarioFailure>(inner)},
-    _core{inner._core},
-    std::runtime_error{ScenarioFailure::formatName(inner._source_name, index, name)}
+  : source_name_{source_name},
+    index_{index},
+    inner_{std::make_shared<ScenarioFailure>(inner)},
+    core_{inner.core_},
+    std::runtime_error{ScenarioFailure::formatName(inner.source_name_, index, name)}
   {
   }
 
-  static auto formatName(std::string inner_source_name, std::string name) -> std::string
+  // Constructor with inner but without current source -- last object
+  explicit ScenarioFailure(std::string name, ScenarioFailure const & inner)
+  : source_name_{""},
+    index_{0},
+    inner_{std::make_shared<ScenarioFailure>(inner)},
+    core_{inner.core_},
+    std::runtime_error{ScenarioFailure::formatName(inner.source_name_, name)}
   {
-    if (!inner_source_name.empty())
-      return name + "." + inner_source_name;
-    else
-      return name;
   }
 
-  static auto formatName(std::string inner_source_name, int index, std::string name) -> std::string
+  static auto formatName(const std::string & inner_source_name, const std::string & name)
+    -> std::string
   {
+    return inner_source_name.empty() ? name : name + "." + inner_source_name;
+  }
+
+  static auto formatName(const std::string & inner_source_name, int index, const std::string & name)
+    -> std::string
+  {
+    std::stringstream ss;
     if (!inner_source_name.empty())
-      return "." + name + "[\"" + inner_source_name + "\"]";
+      ss << '.' << name << "[" << std::quoted(inner_source_name) << "]";
     else
-      return "." + name + "[" + std::to_string(index) + "]";
+      ss << '.' << name << '[' << index << ']';
+    return ss.str();
   }
 
   auto setCoreSource(std::vector<std::pair<std::string, std::string>> conditions_list) -> void
   {
-    if (!_core) _core = std::make_shared<Core>(conditions_list);
+    if (!core_) core_ = std::make_shared<CoreSource>(conditions_list);
   }
 
   auto setInitActionAsSource() -> void
   {
-    if (!_core) _core = std::make_shared<Core>(true);
+    if (!core_) core_ = std::make_shared<CoreSource>(true);
   }
 
   auto recursiveWhat() const -> std::string
   {
-    if (_inner) {
-      return std::runtime_error::what() + _inner->recursiveWhat();
+    if (inner_) {
+      return std::runtime_error::what() + inner_->recursiveWhat();
     } else {
       return std::runtime_error::what();
     }
@@ -147,20 +154,20 @@ struct ScenarioFailure : public std::runtime_error
 
   const char * what() const noexcept override
   {
-    if (_core) {
-      _core->logUpdate(recursiveWhat());
-      return _core->log().c_str();
+    if (core_) {
+      core_->logUpdate(recursiveWhat());
+      return core_->log().c_str();
     } else
       return "No error core defined";
   }
 
-  std::string _source_name{""};
-  int _index{-1};
+  std::string source_name_;
+  int index_;
 
 private:
-  std::shared_ptr<Core> _core;
-  std::shared_ptr<ScenarioFailure> const _inner;
-  std::string _description;
+  std::shared_ptr<CoreSource> core_;
+  const std::shared_ptr<const ScenarioFailure> inner_;
+  std::string description_;
 };
 
 }  // namespace openscenario_interpreter
