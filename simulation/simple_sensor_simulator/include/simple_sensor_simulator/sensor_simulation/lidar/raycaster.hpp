92407f830941509edc3ae114a522079201f2cfc6
Kyoichi Sugahara <kyoichi.sugahara@tier4.jp>
2023-08-03 20:01:09 +0900

    Update simulation/simple_sensor_simulator/include/simple_sensor_simulator/sensor_simulation/lidar/raycaster.hpp
    Co-authored-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation/simple_sensor_simulator/include/simple_sensor_simulator/sensor_simulation/lidar/raycaster.hpp
+++ simulation/simple_sensor_simulator/include/simple_sensor_simulator/sensor_simulation/lidar/raycaster.hpp
@@ -49,7 +49,7 @@ public:
     primitive_ptrs_.emplace(name, std::move(primitive_ptr));
   }
   const sensor_msgs::msg::PointCloud2 raycast(
-    std::string frame_id, const rclcpp::Time & stamp, geometry_msgs::msg::Pose origin,
+    const std::string & frame_id, const rclcpp::Time & stamp, const geometry_msgs::msg::Pose & origin,
     double max_distance = 300, double min_distance = 0);
   const std::vector<std::string> & getDetectedObject() const;
   void setDirection(
