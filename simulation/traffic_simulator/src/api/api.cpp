f3b0abc14875dc48fd7f12f9b1dbc64c44f6d703
Dawid Moszyński <dawid.moszynski@robotec.ai>
2023-08-26 21:01:32 +0200

    fix(traffic_sim): fix nonEgo update - ll2 issue

--- simulation/traffic_simulator/src/api/api.cpp
+++ simulation/traffic_simulator/src/api/api.cpp
@@ -260,17 +260,16 @@ bool API::updateEntitiesStatusInSim()
       simulation_interface::toMsg(res_status.pose(), entity_status.pose);
       simulation_interface::toMsg(res_status.action_status(), entity_status.action_status);
 
-      // temporarily deinitialize lanelet pose as it should be correctly filled from here
-      entity_status.lanelet_pose_valid = false;
-      entity_status.lanelet_pose = traffic_simulator_msgs::msg::LaneletPose();
-
-      auto canonicalized = canonicalize(entity_status);
-      /// @note apply additional status data (from ll2) and then set status
-      entity_manager_ptr_->fillLaneletPose(name, canonicalized);
       if (entity_manager_ptr_->isEgo(name)) {
+        // temporarily deinitialize lanelet pose as it should be correctly filled from here
+        entity_status.lanelet_pose_valid = false;
+        entity_status.lanelet_pose = traffic_simulator_msgs::msg::LaneletPose();
+        auto canonicalized = canonicalize(entity_status);
+        /// @note apply additional status data (from ll2) and then set status
+        entity_manager_ptr_->fillLaneletPose(name, canonicalized);
         entity_manager_ptr_->setEntityStatusExternally(name, canonicalized);
       } else {
-        entity_manager_ptr_->setEntityStatus(name, canonicalized);
+        entity_manager_ptr_->setEntityStatus(name, canonicalize(entity_status));
       }
     }
     return true;
