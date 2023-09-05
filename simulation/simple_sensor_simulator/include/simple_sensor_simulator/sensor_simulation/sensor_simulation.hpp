9552c971313cf6b30f5050f045c7f5cfd4c2458f
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-09-05 17:23:45 +0900

    refactor(simple_sensor_simulator): TrafficLightDetectorEmulator => PseudoTrafficLightDetector
    Co-authored-by: Tatsuya Yamasaki <httperror@404-notfound.jp>

--- simulation/simple_sensor_simulator/include/simple_sensor_simulator/sensor_simulation/sensor_simulation.hpp
+++ simulation/simple_sensor_simulator/include/simple_sensor_simulator/sensor_simulation/sensor_simulation.hpp
@@ -91,7 +91,7 @@ public:
     }
   }
 
-  auto attachTrafficLightsDetectorEmulator(
+  auto attachPseudoTrafficLightsDetector(
     const double current_simulation_time,
     const simulation_api_schema::PseudoTrafficLightDetectorConfiguration & configuration,
     rclcpp::Node & node, std::shared_ptr<hdmap_utils::HdMapUtils> hdmap_utils) -> void
