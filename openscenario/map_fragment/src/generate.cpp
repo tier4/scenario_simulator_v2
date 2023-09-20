8084ef0320f49ffe6b5fc39cbc643b62654f9459
yamacir-kit <httperror@404-notfound.jp>
2023-09-20 17:06:45 +0900

    Move some functions into header `map_fragment.hpp`
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

--- openscenario/map_fragment/src/generate.cpp
+++ openscenario/map_fragment/src/generate.cpp
@@ -19,82 +19,7 @@
 #include <map_fragment/map_fragment.hpp>
 #include <rclcpp/rclcpp.hpp>
 
-#define PRINT(...) std::cout << #__VA_ARGS__ " = " << std::boolalpha << (__VA_ARGS__) << std::endl
-
-auto makePoint3d(double x, double y, double z, double elevation = 0.0)
-{
-  static lanelet::Id id = 0;
-  auto point = lanelet::Point3d(++id, x, y, z);
-  point.attributes()["ele"] = elevation;
-  return point;
-}
-
-auto makeLineString3d(
-  const lanelet::Point3d & origin, double length, double radius, std::size_t resolution)
-{
-  static lanelet::Id id = 0;
-
-  if (std::isinf(radius)) {
-    return lanelet::LineString3d(
-      ++id, {origin, makePoint3d(origin.x() + length, origin.y(), origin.z())});
-  } else {
-    auto line = lanelet::LineString3d(++id);
-
-    if (M_PI * 2 < length / radius) {
-      PRINT(length / radius);
-      std::exit(EXIT_FAILURE);
-    }
-
-    const auto radian_step = length / radius / resolution;
-
-    auto total_length = 0.0;
-
-    for (auto radian = 0.0; 0 < resolution; radian += radian_step, --resolution) {
-      auto x = origin.x() + radius * std::sin(radian);
-      auto y = origin.y() + radius * std::cos(radian) - radius;
-      auto z = origin.z();
-      line.push_back(makePoint3d(x, y, z));
-      total_length += radius * radian_step;
-    }
-
-    auto x = origin.x() + radius * std::sin(length / radius);
-    auto y = origin.y() + radius * std::cos(length / radius) - radius;
-    auto z = origin.z();
-    line.push_back(makePoint3d(x, y, z));
-
-    return line;
-  }
-}
-
-auto makeLanelet(double length, double width, double curvature, double resolution)
-{
-  const auto x = 0.0;
-  const auto y = 0.0;
-  const auto z = 0.0;
-
-  const auto p1 = makePoint3d(x, y - width / 2, z);
-  const auto p2 = makePoint3d(x, y + width / 2, z);
-
-  const auto radius = curvature == 0 ? std::numeric_limits<double>::infinity() : 1 / curvature;
-
-  const auto p1_radius = radius - width / 2;
-  const auto p2_radius = radius + width / 2;
-
-  const auto p1_offset = std::isinf(radius) or std::isinf(p1_radius)
-                           ? 0.0
-                           : p1_radius * (length / radius - length / p1_radius);
-
-  const auto p2_offset = std::isinf(radius) or std::isinf(p2_radius)
-                           ? 0.0
-                           : p2_radius * (length / radius - length / p2_radius);
-
-  static lanelet::Id id = 0;
-  auto lane = lanelet::Lanelet(
-    ++id, makeLineString3d(p1, length + p1_offset, p1_radius, resolution),
-    makeLineString3d(p2, length + p2_offset, p2_radius, resolution));
-  lane.attributes()["subtype"] = "road";
-  return lane;
-}
+#define DEBUG(...) std::cerr << #__VA_ARGS__ " = " << std::boolalpha << (__VA_ARGS__) << std::endl
 
 auto main(const int argc, char const * const * const argv) -> int
 try {
@@ -134,7 +59,7 @@ try {
 
   auto map = lanelet::LaneletMap();
 
-  map.add(makeLanelet(length, width, curvature, resolution));
+  map.add(map_fragment::makeLanelet(length, width, curvature, resolution));
 
   try {
     if (std::filesystem::remove_all(output_directory);
