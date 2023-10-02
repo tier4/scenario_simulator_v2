6653208a154f5604a024284010164b519c76a81a
Mateusz Palczuk <mateusz.palczuk@robotec.ai>
2023-10-02 14:53:55 +0200

    Add missing Distance tests
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk@robotec.ai>

--- common/math/geometry/test/test_distance.cpp
+++ common/math/geometry/test/test_distance.cpp
@@ -60,6 +60,63 @@ TEST(Distance, PoseToPoint)
   EXPECT_DOUBLE_EQ(math::geometry::getDistance(p0, p1), std::sqrt(2));
 }
 
+TEST(Distance, Distance2DDisjoint)
+{
+  std::vector<geometry_msgs::msg::Point> polygon0, polygon1;
+  auto makePoint = [](double x, double y) {
+    geometry_msgs::msg::Point p;
+    p.x = x;
+    p.y = y;
+    return p;
+  };
+  polygon0.emplace_back(makePoint(0, 0));
+  polygon0.emplace_back(makePoint(0, 1));
+  polygon0.emplace_back(makePoint(1, 1));
+  polygon0.emplace_back(makePoint(1, 0));
+  polygon1.emplace_back(makePoint(2, 2));
+  polygon1.emplace_back(makePoint(2, 3));
+  polygon1.emplace_back(makePoint(3, 3));
+  polygon1.emplace_back(makePoint(3, 2));
+  EXPECT_DOUBLE_EQ(math::geometry::getDistance2D(polygon0, polygon1), std::sqrt(2));
+}
+
+TEST(Distance, Distance2DIntersect)
+{
+  std::vector<geometry_msgs::msg::Point> polygon0, polygon1;
+  auto makePoint = [](double x, double y) {
+    geometry_msgs::msg::Point p;
+    p.x = x;
+    p.y = y;
+    return p;
+  };
+  polygon0.emplace_back(makePoint(0, 0));
+  polygon0.emplace_back(makePoint(0, 2));
+  polygon0.emplace_back(makePoint(2, 2));
+  polygon0.emplace_back(makePoint(2, 0));
+  polygon1.emplace_back(makePoint(1, 1));
+  polygon1.emplace_back(makePoint(1, 3));
+  polygon1.emplace_back(makePoint(3, 3));
+  polygon1.emplace_back(makePoint(3, 1));
+  EXPECT_DOUBLE_EQ(math::geometry::getDistance2D(polygon0, polygon1), 0);
+}
+
+TEST(Distance, Distance2DZeroElements)
+{
+  std::vector<geometry_msgs::msg::Point> polygon0, polygon1;
+  auto makePoint = [](double x, double y) {
+    geometry_msgs::msg::Point p;
+    p.x = x;
+    p.y = y;
+    return p;
+  };
+  polygon0.emplace_back(makePoint(0, 0));
+  polygon0.emplace_back(makePoint(0, 1));
+  polygon0.emplace_back(makePoint(1, 1));
+  polygon0.emplace_back(makePoint(1, 0));
+  EXPECT_THROW(
+    math::geometry::getDistance2D(polygon0, polygon1), boost::geometry::empty_input_exception);
+}
+
 int main(int argc, char ** argv)
 {
   testing::InitGoogleTest(&argc, argv);
