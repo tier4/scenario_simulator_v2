8e1497e5052540b40f1ee3f8c6dac94b2785afac
Adam Gotlib <adam.gotlib@robotec.ai>
2023-10-13 13:32:01 +0200

    Fix code formatting for road_segment.hpp

--- openscenario/map_fragment/include/map_fragment/road_segment.hpp
+++ openscenario/map_fragment/include/map_fragment/road_segment.hpp
@@ -20,100 +20,100 @@
 
 namespace map_fragment
 {
-  struct RoadCrossSectionDescription
-  {
-    long int number_of_lanes;
-    double lane_width;
-  }; // struct RoadCrossSectionDescription
+struct RoadCrossSectionDescription
+{
+  long int number_of_lanes;
+  double lane_width;
+}; // struct RoadCrossSectionDescription
 
-  class RoadCrossSection
-  {
-    lanelet::Points3d points_;
+class RoadCrossSection
+{
+  lanelet::Points3d points_;
 
-  public:
-    RoadCrossSection(RoadCrossSectionDescription description, // TODO: name for the origin coordinate system
-                     double origin_x,
-                     double origin_y,
-                     double h)
+public:
+  RoadCrossSection(RoadCrossSectionDescription description, // TODO: name for the origin coordinate system
+                   double origin_x,
+                   double origin_y,
+                   double h)
+  {
+
+    auto cross_section_width = description.number_of_lanes * description.lane_width;
+
+    auto x = origin_x + cross_section_width / 2 * std::sin(h);
+    auto y = origin_y - cross_section_width / 2 * std::cos(h);
+
+    for (auto i = 0; i < description.number_of_lanes + 1; i++)
     {
-
-      auto cross_section_width = description.number_of_lanes * description.lane_width;
-
-      auto x = origin_x + cross_section_width / 2 * std::sin(h);
-      auto y = origin_y - cross_section_width / 2 * std::cos(h);
-
-      for (auto i = 0; i < description.number_of_lanes + 1; i++)
-      {
-        points_.push_back(makePoint3d(x, y, 0.));
-        x -= description.lane_width * std::sin(h);
-        y += description.lane_width * std::cos(h);
-      }
+      points_.push_back(makePoint3d(x, y, 0.));
+      x -= description.lane_width * std::sin(h);
+      y += description.lane_width * std::cos(h);
     }
+  }
 
-    const lanelet::Points3d getPoints()
-    { // TODO: return as smart pointer?
-      return points_;
-    }
-  }; // class RoadCrossSection
+  const lanelet::Points3d getPoints()
+  { // TODO: return as smart pointer?
+    return points_;
+  }
+}; // class RoadCrossSection
 
-  class RoadSegment
+class RoadSegment
+{
+  lanelet::Lanelets lanelets_;
+
+public:
+  RoadSegment(double length,
+              double curvature,
+              int resolution,
+              RoadCrossSectionDescription cross_section_description)
   {
-    lanelet::Lanelets lanelets_;
 
-  public:
-    RoadSegment(double length,
-          double curvature,
-          int resolution,
-          RoadCrossSectionDescription cross_section_description)
+    //  TODO Make sure resolution >= 2
+    //  TODO Make sure length > 0
+
+    auto step = length / (resolution - 1);
+
+    // TODO Allow to initialize origin with custom values
+    auto x = 0.0;
+    auto y = 0.0;
+    auto h = 0.0;
+
+    auto n = cross_section_description.number_of_lanes + 1;
+
+    // TODO Find a cleaner way to do this
+    std::vector<lanelet::LineString3d> lane_boundaries(n);
+    for (auto i = 0; i < n; i++)
     {
-
-      //  TODO Make sure resolution >= 2
-      //  TODO Make sure length > 0
-
-      auto step = length / (resolution - 1);
-
-      // TODO Allow to initialize origin with custom values
-      auto x = 0.0;
-      auto y = 0.0;
-      auto h = 0.0;
-
-      auto n = cross_section_description.number_of_lanes + 1;
-
-      // TODO Find a cleaner way to do this
-      std::vector<lanelet::LineString3d> lane_boundaries(n);
-      for (auto i = 0; i < n; i++)
-      {
-        lane_boundaries[i].setId(i);
-      }
-
-      for (auto i = 0; i < resolution; i++)
-      {
-        RoadCrossSection cross_section(cross_section_description, x, y, h);
-        auto cross_section_points = cross_section.getPoints();
-
-        for (auto j = 0; j < cross_section_description.number_of_lanes + 1; j++)
-        {
-          lane_boundaries[j].push_back(cross_section_points[j]);
-        }
-
-        x += step * std::cos(h);
-        y += step * std::sin(h);
-        h += step * curvature;
-      }
-
-      for (auto i = 0; i < cross_section_description.number_of_lanes; i++)
-      {
-        lanelets_.push_back(makeLanelet(
-          lane_boundaries[i],
-          lane_boundaries[i + 1]));
-      }
+      lane_boundaries[i].setId(i);
     }
 
-    const lanelet::Lanelets getLanelets()
-    { // TODO: return as smart pointer?
-      return lanelets_;
+    for (auto i = 0; i < resolution; i++)
+    {
+      RoadCrossSection cross_section(cross_section_description, x, y, h);
+      auto cross_section_points = cross_section.getPoints();
+
+      for (auto j = 0; j < cross_section_description.number_of_lanes + 1; j++)
+      {
+        lane_boundaries[j].push_back(cross_section_points[j]);
+      }
+
+      x += step * std::cos(h);
+      y += step * std::sin(h);
+      h += step * curvature;
     }
-  }; // class RoadSegment
+
+    for (auto i = 0; i < cross_section_description.number_of_lanes; i++)
+    {
+      lanelets_.push_back(makeLanelet(
+        lane_boundaries[i],
+        lane_boundaries[i + 1]));
+    }
+  }
+
+  const lanelet::Lanelets getLanelets()
+  { // TODO: return as smart pointer?
+    return lanelets_;
+  }
+}; // class RoadSegment
 
 } // namespace map_fragment
 
