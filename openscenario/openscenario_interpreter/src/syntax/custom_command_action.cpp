92fc727960e993238af25e414eca2278d1b9db73
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-10-23 17:33:53 +0900

    chore: apply linter

--- openscenario/openscenario_interpreter/src/syntax/custom_command_action.cpp
+++ openscenario/openscenario_interpreter/src/syntax/custom_command_action.cpp
@@ -103,7 +103,8 @@ struct ApplyPseudoTrafficSignalDetectorConfidenceSetAction
   {
     static_assert(0 < Version and Version <= 1);
     if (parameters.size() == 2) {
-      setConventionalTrafficLightConfidence(std::stoi(parameters.at(0)), std::stod(parameters.at(1)));
+      setConventionalTrafficLightConfidence(
+        std::stoi(parameters.at(0)), std::stod(parameters.at(1)));
     } else {
       throw Error(
         "An unexpected number of arguments were passed to "
