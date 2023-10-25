2c4d5c8275b700db018144ac7ad17233274ffb72
yamacir-kit <httperror@404-notfound.jp>
2023-10-25 18:54:09 +0900

    Move constraints definition into header `constraint.hpp` from `find.cpp`
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

---
+++ openscenario/map_fragment/include/map_fragment/constraint.hpp
@@ -0,0 +1,144 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MAP_FRAGMENT__CONSTRAINTS_HPP__
+#define MAP_FRAGMENT__CONSTRAINTS_HPP__
+
+#include <lanelet2_core/geometry/LaneletMap.h>
+#include <lanelet2_routing/RoutingGraph.h>
+
+namespace map_fragment
+{
+inline namespace constraint
+{
+auto isLeftmost(const lanelet::routing::RoutingGraph & graph, const lanelet::Lanelet & lanelet)
+{
+  return graph.leftRelations(lanelet).empty();
+}
+
+auto isRightmost(
+  const lanelet::LaneletMap & map, const lanelet::routing::RoutingGraph & graph,
+  const lanelet::Lanelet & lanelet)
+{
+  auto relations = graph.rightRelations(lanelet);
+
+  return relations.empty() or
+         std::any_of(relations.begin(), relations.end(), [&](const auto & relation) {
+           /*
+              NOTE: The Lanelet returned by rightRelations is not useful
+              because the directions are aligned regardless of the original
+              orientation.
+           */
+           return lanelet.rightBound() ==
+                  map.laneletLayer.find(relation.lanelet.id())->rightBound().invert();
+         });
+}
+
+template <typename Node>
+auto loadBasicConstraints(const Node & node)
+{
+  std::unordered_map<
+    std::string, std::function<bool(
+                   const lanelet::Lanelet &, const lanelet::LaneletMap &,
+                   const lanelet::routing::RoutingGraph &)>>
+    constraints;
+
+  if (node.has_parameter("type")) {
+    const auto type = node.get_parameter("type").as_string();
+    constraints.emplace("type", [type](auto && lanelet, auto &&...) {  //
+      return lanelet.attribute("type") == type;
+    });
+  }
+
+  if (node.has_parameter("subtype")) {
+    const auto subtype = node.get_parameter("subtype").as_string();
+    constraints.emplace("subtype", [subtype](auto && lanelet, auto &&...) {
+      return lanelet.attribute("subtype") == subtype;
+    });
+  }
+
+  if (node.has_parameter("id_greater_than")) {
+    const auto lower_bound = node.get_parameter("id_greater_than").as_int();
+    constraints.emplace("id_greater_than", [lower_bound](auto && lanelet, auto &&...) {
+      return lower_bound < lanelet.id();
+    });
+  }
+
+  if (node.has_parameter("id_less_than")) {
+    const auto upper_bound = node.get_parameter("id_less_than").as_int();
+    constraints.emplace("is_less_than", [upper_bound](auto && lanelet, auto &&...) {
+      return lanelet.id() < upper_bound;
+    });
+  }
+
+  if (node.has_parameter("left_of")) {
+    const auto right_id = node.get_parameter("left_of").as_int();
+    constraints.emplace("left_of", [right_id](auto && lanelet, auto && map, auto &&) {
+      auto is_left_of = [&](auto right_id) {
+        auto right = map.laneletLayer.find(right_id);
+        return right != map.laneletLayer.end() and lanelet::geometry::leftOf(lanelet, *right);
+      };
+      return not right_id or is_left_of(right_id);
+    });
+  }
+
+  if (node.has_parameter("right_of")) {
+    const auto left_id = node.get_parameter("right_of").as_int();
+    constraints.emplace("right_of", [left_id](auto && lanelet, auto && map, auto &&) {
+      auto is_right_of = [&](auto left_id) {
+        auto left = map.laneletLayer.find(left_id);
+        return left != map.laneletLayer.end() and lanelet::geometry::rightOf(lanelet, *left);
+      };
+      return not left_id or is_right_of(left_id);
+    });
+  }
+
+  if (node.has_parameter("leftmost") and node.get_parameter("leftmost").as_bool()) {
+    constraints.emplace("leftmost", [](auto && lanelet, auto &&, auto && graph) {
+      return isLeftmost(graph, lanelet);
+    });
+  }
+
+  if (node.has_parameter("not_leftmost") and node.get_parameter("not_leftmost").as_bool()) {
+    constraints.emplace("not_leftmost", [](auto && lanelet, auto &&, auto && graph) {
+      return not isLeftmost(graph, lanelet);
+    });
+  }
+
+  if (node.has_parameter("rightmost") and node.get_parameter("rightmost").as_bool()) {
+    constraints.emplace("rightmost", [](auto && lanelet, auto && map, auto && graph) {
+      return isRightmost(map, graph, lanelet);
+    });
+  }
+
+  if (node.has_parameter("not_rightmost") and node.get_parameter("not_rightmost").as_bool()) {
+    constraints.emplace("not_rightmost", [](auto && lanelet, auto && map, auto && graph) {
+      return not isRightmost(map, graph, lanelet);
+    });
+  }
+
+  if (node.has_parameter("route_length_greater_than")) {
+    const auto lower_bound = node.get_parameter("route_length_greater_than").as_double();
+    constraints.emplace(
+      "route_length_greater_than", [lower_bound](auto && lanelet, auto &&, auto && graph) {
+        return 0 < graph.possiblePaths(lanelet, lower_bound).size();
+      });
+  }
+
+  return constraints;
+}
+}  // namespace constraint
+}  // namespace map_fragment
+
+#endif  // MAP_FRAGMENT__CONSTRAINTS_HPP__
--- openscenario/map_fragment/src/find.cpp
+++ openscenario/map_fragment/src/find.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <map_fragment/constraint.hpp>
 #include <map_fragment/filter.hpp>
 #include <map_fragment/map_fragment.hpp>
 #include <random>
@@ -39,185 +40,24 @@ try {
 
   const auto routing_graph = lanelet::routing::RoutingGraph::build(*map, vehicleTrafficRules());
 
-  auto is_leftmost = [&](const auto & lanelet) {
-    return routing_graph->leftRelations(lanelet).empty();
-  };
+  node.declare_parameter("type", "lanelet");
+  node.declare_parameter("subtype", "road");
+  node.declare_parameter("id_greater_than", lanelet::Id());
+  node.declare_parameter("id_less_than", std::numeric_limits<lanelet::Id>::max());
+  node.declare_parameter("left_of", lanelet::Id());
+  node.declare_parameter("right_of", lanelet::Id());
+  node.declare_parameter("leftmost", false);
+  node.declare_parameter("not_leftmost", false);
+  node.declare_parameter("rightmost", false);
+  node.declare_parameter("not_rightmost", false);
+  node.declare_parameter("route_length_greater_than", 0.0);
 
-  auto is_rightmost = [&](const auto & lanelet) {
-    auto right_relations = routing_graph->rightRelations(lanelet);
-    return right_relations.empty() or
-           std::any_of(
-             right_relations.begin(), right_relations.end(), [&](const auto & right_relation) {
-               /*
-                  NOTE: The Lanelet returned by rightRelations is not
-                  useful because the directions are aligned regardless of
-                  the original orientation.
-               */
-               return lanelet.rightBound() ==
-                      map->laneletLayer.find(right_relation.lanelet.id())->rightBound().invert();
-             });
-  };
-
-  const std::list<std::function<bool(const lanelet::Lanelet &)>> constraints{
-    [&]() {
-      node.declare_parameter("type", "lanelet");
-      const auto type = node.get_parameter("type").as_string();
-      return [type](const auto & lanelet) {
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id() << "] discarded: The type is not " << type << "."
-                    << std::endl;
-          return false;
-        };
-        return lanelet.attribute("type") == type or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("subtype", "road");
-      const auto subtype = node.get_parameter("subtype").as_string();
-      return [subtype](const auto & lanelet) {
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id() << "] discarded: The subtype is not " << subtype << "."
-                    << std::endl;
-          return false;
-        };
-        return lanelet.attribute("subtype") == subtype or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("id_greater_than", lanelet::Id());
-      const auto lower_bound = node.get_parameter("id_greater_than").as_int();
-      return [lower_bound](const auto & lanelet) {
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id() << "] discarded: The ID is not greater than "
-                    << lower_bound << "." << std::endl;
-          return false;
-        };
-        return lower_bound < lanelet.id() or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("id_less_than", std::numeric_limits<lanelet::Id>::max());
-      const auto upper_bound = node.get_parameter("id_less_than").as_int();
-      return [upper_bound](const auto & lanelet) {
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id() << "] discarded: The ID is not less than " << upper_bound
-                    << "." << std::endl;
-          return false;
-        };
-        return lanelet.id() < upper_bound or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("left_of", lanelet::Id());
-      const auto right_id = node.get_parameter("left_of").as_int();
-      return [&, right_id](const auto & lanelet) {
-        auto is_left_of = [&](auto right_id) {
-          auto right = map->laneletLayer.find(right_id);
-          return right != map->laneletLayer.end() and lanelet::geometry::leftOf(lanelet, *right);
-        };
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id()
-                    << "] discarded: The lanelet is not to the left of the lanelet with ID "
-                    << right_id << "." << std::endl;
-          return false;
-        };
-        return not right_id or is_left_of(right_id) or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("right_of", lanelet::Id());
-      const auto left_id = node.get_parameter("right_of").as_int();
-      return [&, left_id](const auto & lanelet) {
-        auto is_right_of = [&](auto left_id) {
-          auto left = map->laneletLayer.find(left_id);
-          return left != map->laneletLayer.end() and lanelet::geometry::rightOf(lanelet, *left);
-        };
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id()
-                    << "] discarded: The lanelet is not to the right of the lanelet with ID "
-                    << left_id << "." << std::endl;
-          return false;
-        };
-        return not left_id or is_right_of(left_id) or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("leftmost", false);
-      const auto constrained = node.get_parameter("leftmost").as_bool();
-      return [&, constrained](const auto & lanelet) {
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id()
-                    << "] discarded: There are lanelets to the left of the lanelet." << std::endl;
-          return false;
-        };
-        return not constrained or is_leftmost(lanelet) or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("not_leftmost", false);
-      const auto constrained = node.get_parameter("not_leftmost").as_bool();
-      return [&, constrained](const auto & lanelet) {
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id()
-                    << "] discarded: There are lanelets to the left of the lanelet." << std::endl;
-          return false;
-        };
-        return not constrained or not is_leftmost(lanelet) or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("rightmost", false);
-      const auto constrained = node.get_parameter("rightmost").as_bool();
-      return [&, constrained](const auto & lanelet) {
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id()
-                    << "] discarded: There are lanelets to the right of the lanelet." << std::endl;
-          return false;
-        };
-        return not constrained or is_rightmost(lanelet) or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("not_rightmost", false);
-      const auto constrained = node.get_parameter("not_rightmost").as_bool();
-      return [&, constrained](const auto & lanelet) {
-        auto discard = [&]() {
-          std::cerr << "[" << lanelet.id()
-                    << "] discarded: There are lanelets to the right of the lanelet." << std::endl;
-          return false;
-        };
-        return not constrained or not is_rightmost(lanelet) or discard();
-      };
-    }(),
-
-    [&]() {
-      node.declare_parameter("route_length_greater_than", 0.0);
-      const auto lower_bound = node.get_parameter("route_length_greater_than").as_double();
-      return [&, lower_bound](const auto & lanelet) {
-        auto discard = [&]() {
-          std::cerr
-            << "[" << lanelet.id()
-            << "] discarded: The length of the path routable from the lanelet is not greater than "
-            << lower_bound << std::endl;
-          return false;
-        };
-        return 0 < routing_graph->possiblePaths(lanelet, lower_bound).size() or discard();
-      };
-    }(),
-  };
+  const auto constraints = loadBasicConstraints(node);
 
   auto satisfy = [&](const auto & lanelet) {
-    return std::all_of(
-      constraints.begin(), constraints.end(), [&](const auto satisfy) { return satisfy(lanelet); });
+    return std::all_of(constraints.begin(), constraints.end(), [&](const auto & constraint) {
+      return constraint.second(lanelet, *map, *routing_graph);
+    });
   };
 
   auto receive = [&](auto && results) -> decltype(auto) {
