1b019c29cab968a3f1b31e00ec9b1eaca1638c2f
Masaya Kataoka <ms.kataoka@gmail.com>
2023-10-26 13:14:04 +0900

    update namespace
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- mock/cpp_mock_scenarios/src/behavior_plugin/load_do_nothing_plugin.cpp
+++ mock/cpp_mock_scenarios/src/behavior_plugin/load_do_nothing_plugin.cpp
@@ -70,7 +70,7 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions options;
-  auto component = std::make_shared<LoadDoNothingPluginScenario>(options);
+  auto component = std::make_shared<cpp_mock_scenarios::LoadDoNothingPluginScenario>(options);
   rclcpp::spin(component);
   rclcpp::shutdown();
   return 0;
