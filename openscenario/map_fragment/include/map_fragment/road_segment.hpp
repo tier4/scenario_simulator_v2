8c533c1172354245e1a7e33ea6d1fba543f1c58a
Adam Gotlib <adam.gotlib@robotec.ai>
2023-12-11 17:33:39 +0100

    Fix rounding error when calculating points for a cross-section

--- openscenario/map_fragment/include/map_fragment/road_segment.hpp
+++ openscenario/map_fragment/include/map_fragment/road_segment.hpp
@@ -61,7 +61,7 @@ public:
     const auto normal_vector = rotateInLocalZAxisAssumingZeroRoll(tangent_vector, M_PI_2);
 
     for (auto i = 0; i < description.number_of_lanes + 1; i++) {
-      const auto lateral_position = (i - description.number_of_lanes / 2) * description.lane_width;
+      const auto lateral_position = (i - description.number_of_lanes / 2.) * description.lane_width;
       const auto p = origin + normal_vector * lateral_position;
 
       points_.push_back(makePoint3d(p));
