b11b8febfecfa945c0e24925264b778361b9f6f9
Mateusz Palczuk <mateusz.palczuk@robotec.ai>
2023-12-13 15:17:08 +0100

    Fix and add randomizer tests
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk@robotec.ai>

--- test_runner/random_test_runner/test/test_randomizers.cpp
+++ test_runner/random_test_runner/test/test_randomizers.cpp
@@ -20,10 +20,10 @@
 
 #include "expect_eq_macros.hpp"
 
-TEST(Randomizers, LaneletIdRandomizer_generate255)
+TEST(UniformRandomizer, int_generate255)
 {
   RandomizationEnginePtr engine = std::make_shared<RandomizationEngine>();
-  LaneletIdRandomizer rand(engine, 0, 255);
+  UniformRandomizer<int> rand(engine, 0, 255);
   int sum = 0;
   for (int i = 0; i < 1e+6; ++i) {
     const auto generated = rand.generate();
@@ -33,10 +33,10 @@ TEST(Randomizers, LaneletIdRandomizer_generate255)
   EXPECT_NEAR(static_cast<double>(sum) / 1e+6, 127.5, 1.0);
 }
 
-TEST(Randomizers, LaneletIdRandomizer_generate10)
+TEST(UniformRandomizer, int_generate10)
 {
   RandomizationEnginePtr engine = std::make_shared<RandomizationEngine>();
-  LaneletIdRandomizer rand(engine, 0, 10);
+  UniformRandomizer<int> rand(engine, 0, 10);
   int sum = 0;
   for (int i = 0; i < 1e+6; ++i) {
     const auto generated = rand.generate();
@@ -46,10 +46,21 @@ TEST(Randomizers, LaneletIdRandomizer_generate10)
   EXPECT_NEAR(static_cast<double>(sum) / 1e+6, 5.0, 0.1);
 }
 
-TEST(Randomizers, LaneletIdRandomizer_setRange10)
+TEST(UniformRandomizer, int_generate10seed)
+{
+  RandomizationEnginePtr engine = std::make_shared<RandomizationEngine>(1);
+  UniformRandomizer<int> rand(engine, 0, 10);
+  EXPECT_EQ(rand.generate(), 4);
+  EXPECT_EQ(rand.generate(), 10);
+  EXPECT_EQ(rand.generate(), 7);
+  EXPECT_EQ(rand.generate(), 10);
+  EXPECT_EQ(rand.generate(), 0);
+}
+
+TEST(UniformRandomizer, int_setRange10)
 {
   RandomizationEnginePtr engine = std::make_shared<RandomizationEngine>();
-  LaneletIdRandomizer rand(engine, 0, 255);
+  UniformRandomizer<int> rand(engine, 0, 255);
   rand.setRange(10, 20);
   int sum = 0;
   for (int i = 0; i < 1e+6; ++i) {
@@ -60,10 +71,10 @@ TEST(Randomizers, LaneletIdRandomizer_setRange10)
   EXPECT_NEAR(static_cast<double>(sum) / 1e+6, 15.0, 0.1);
 }
 
-TEST(Randomizers, SValueRandomizer_generate100)
+TEST(UniformRandomizer, double_generate100)
 {
   RandomizationEnginePtr engine = std::make_shared<RandomizationEngine>();
-  SValueRandomizer rand(engine, 0.0, 100.0);
+  UniformRandomizer<double> rand(engine, 0.0, 100.0);
   double sum = 0.0;
   for (int i = 0; i < 1e+6; ++i) {
     const auto generated = rand.generate();
@@ -73,10 +84,10 @@ TEST(Randomizers, SValueRandomizer_generate100)
   EXPECT_NEAR(sum / 1e+6, 50.0, 1.0);
 }
 
-TEST(Randomizers, SValueRandomizer_generate1)
+TEST(UniformRandomizer, double_generate1)
 {
   RandomizationEnginePtr engine = std::make_shared<RandomizationEngine>();
-  SValueRandomizer rand(engine, 0.0, 1.0);
+  UniformRandomizer<double> rand(engine, 0.0, 1.0);
   double sum = 0.0;
   for (int i = 0; i < 1e+6; ++i) {
     const auto generated = rand.generate();
@@ -86,10 +97,21 @@ TEST(Randomizers, SValueRandomizer_generate1)
   EXPECT_NEAR(sum / 1e+6, 0.5, 0.1);
 }
 
-TEST(Randomizers, SValueRandomizer_setRange1)
+TEST(UniformRandomizer, double_generate1seed)
+{
+  RandomizationEnginePtr engine = std::make_shared<RandomizationEngine>(1);
+  UniformRandomizer<double> rand(engine, 0.0, 1.0);
+  EXPECT_NEAR(rand.generate(), 0.997185, 1e-6);
+  EXPECT_NEAR(rand.generate(), 0.932557, 1e-6);
+  EXPECT_NEAR(rand.generate(), 0.128124, 1e-6);
+  EXPECT_NEAR(rand.generate(), 0.999041, 1e-6);
+  EXPECT_NEAR(rand.generate(), 0.236089, 1e-6);
+}
+
+TEST(UniformRandomizer, double_setRange1)
 {
   RandomizationEnginePtr engine = std::make_shared<RandomizationEngine>();
-  SValueRandomizer rand(engine, 0.0, 100.0);
+  UniformRandomizer<double> rand(engine, 0.0, 100.0);
   rand.setRange(1.0, 2.0);
   double sum = 0.0;
   for (int i = 0; i < 1e+6; ++i) {
