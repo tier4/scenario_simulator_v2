52fdc0f6eafa1aef9888f708b85c81e0fa106236
Mateusz Palczuk <mateusz.palczuk@robotec.ai>
2023-12-14 14:35:21 +0100

    Add remaining JunitXmlReporter tests
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk@robotec.ai>

--- test_runner/random_test_runner/test/test_junit_xml_reporter.cpp
+++ test_runner/random_test_runner/test/test_junit_xml_reporter.cpp
@@ -28,22 +28,58 @@ std::string readFile(const std::string & path = "/tmp/result.junit.xml")
   return buffer.str();
 }
 
+TEST(JunitXmlReporter, initialize)
+{
+  EXPECT_NO_THROW(JunitXmlReporter reporter(rclcpp::get_logger("test_junit_xml_reporter")));
+  JunitXmlReporter reporter(rclcpp::get_logger("test_junit_xml_reporter"));
+  EXPECT_NO_THROW(reporter.init("/tmp"));
+  EXPECT_NO_THROW(reporter.spawnTestCase("testsuite", "testcase"));
+}
+
 TEST(JunitXmlReporter, reportCollision)
 {
   JunitXmlReporter reporter(rclcpp::get_logger("test_junit_xml_reporter"));
   reporter.init("/tmp");
   JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
 
-  testcase.reportCollision(makeNPCDescription("npc", 1.0, makeLaneletPose(123, 5.0)), 10.0);
+  EXPECT_NO_THROW(
+    testcase.reportCollision(makeNPCDescription("npc", 1.0, makeLaneletPose(123, 5.0)), 10.0));
 
-  reporter.write();
+  EXPECT_NO_THROW(reporter.write());
 
-  std::string report = readFile();
-  std::string ans = R"(<?xml version="1.0"?>
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
 <testsuites failures="0" errors="1" tests="1">
   <testsuite name="testsuite" failures="0" errors="1" tests="1">
     <testcase name="testcase">
       <error type="collision" message="npc and ego collided at 10s" />
+    </testcase>
+  </testsuite>
+</testsuites>
+)";
+  EXPECT_STREQ(report.c_str(), ans.c_str());
+}
+
+TEST(JunitXmlReporter, reportCollision_double)
+{
+  JunitXmlReporter reporter(rclcpp::get_logger("test_junit_xml_reporter"));
+  reporter.init("/tmp");
+  JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
+
+  EXPECT_NO_THROW(
+    testcase.reportCollision(makeNPCDescription("npc1", 1.0, makeLaneletPose(123, 5.0)), 10.0));
+  EXPECT_NO_THROW(
+    testcase.reportCollision(makeNPCDescription("npc2", 2.0, makeLaneletPose(321, 7.0)), 17.0));
+
+  EXPECT_NO_THROW(reporter.write());
+
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
+<testsuites failures="0" errors="2" tests="2">
+  <testsuite name="testsuite" failures="0" errors="2" tests="2">
+    <testcase name="testcase">
+      <error type="collision" message="npc1 and ego collided at 10s" />
+      <error type="collision" message="npc2 and ego collided at 17s" />
     </testcase>
   </testsuite>
 </testsuites>
@@ -57,15 +93,40 @@ TEST(JunitXmlReporter, reportStandStill)
   reporter.init("/tmp");
   JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
 
-  testcase.reportStandStill();
+  EXPECT_NO_THROW(testcase.reportStandStill());
 
-  reporter.write();
+  EXPECT_NO_THROW(reporter.write());
 
-  std::string report = readFile();
-  std::string ans = R"(<?xml version="1.0"?>
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
 <testsuites failures="0" errors="1" tests="1">
   <testsuite name="testsuite" failures="0" errors="1" tests="1">
     <testcase name="testcase">
+      <error type="stand still" message="Ego seems to be stuck" />
+    </testcase>
+  </testsuite>
+</testsuites>
+)";
+  EXPECT_STREQ(report.c_str(), ans.c_str());
+}
+
+TEST(JunitXmlReporter, reportStandStill_double)
+{
+  JunitXmlReporter reporter(rclcpp::get_logger("test_junit_xml_reporter"));
+  reporter.init("/tmp");
+  JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
+
+  EXPECT_NO_THROW(testcase.reportStandStill());
+  EXPECT_NO_THROW(testcase.reportStandStill());
+
+  EXPECT_NO_THROW(reporter.write());
+
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
+<testsuites failures="0" errors="2" tests="2">
+  <testsuite name="testsuite" failures="0" errors="2" tests="2">
+    <testcase name="testcase">
+      <error type="stand still" message="Ego seems to be stuck" />
       <error type="stand still" message="Ego seems to be stuck" />
     </testcase>
   </testsuite>
@@ -80,15 +141,40 @@ TEST(JunitXmlReporter, reportTimeout)
   reporter.init("/tmp");
   JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
 
-  testcase.reportTimeout();
+  EXPECT_NO_THROW(testcase.reportTimeout());
 
-  reporter.write();
+  EXPECT_NO_THROW(reporter.write());
 
-  std::string report = readFile();
-  std::string ans = R"(<?xml version="1.0"?>
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
 <testsuites failures="0" errors="1" tests="1">
   <testsuite name="testsuite" failures="0" errors="1" tests="1">
     <testcase name="testcase">
+      <error type="timeout" message="Ego failed to reach goal within timeout" />
+    </testcase>
+  </testsuite>
+</testsuites>
+)";
+  EXPECT_STREQ(report.c_str(), ans.c_str());
+}
+
+TEST(JunitXmlReporter, reportTimeout_double)
+{
+  JunitXmlReporter reporter(rclcpp::get_logger("test_junit_xml_reporter"));
+  reporter.init("/tmp");
+  JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
+
+  EXPECT_NO_THROW(testcase.reportTimeout());
+  EXPECT_NO_THROW(testcase.reportTimeout());
+
+  EXPECT_NO_THROW(reporter.write());
+
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
+<testsuites failures="0" errors="2" tests="2">
+  <testsuite name="testsuite" failures="0" errors="2" tests="2">
+    <testcase name="testcase">
+      <error type="timeout" message="Ego failed to reach goal within timeout" />
       <error type="timeout" message="Ego failed to reach goal within timeout" />
     </testcase>
   </testsuite>
@@ -103,16 +189,96 @@ TEST(JunitXmlReporter, reportException)
   reporter.init("/tmp");
   JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
 
-  testcase.reportException("exception_type", "Exception message");
+  EXPECT_NO_THROW(testcase.reportException("exception_type", "Exception message"));
 
-  reporter.write();
+  EXPECT_NO_THROW(reporter.write());
 
-  std::string report = readFile();
-  std::string ans = R"(<?xml version="1.0"?>
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
 <testsuites failures="0" errors="1" tests="1">
   <testsuite name="testsuite" failures="0" errors="1" tests="1">
     <testcase name="testcase">
       <error type="exception_type" message="Exception message" />
+    </testcase>
+  </testsuite>
+</testsuites>
+)";
+  EXPECT_STREQ(report.c_str(), ans.c_str());
+}
+
+TEST(JunitXmlReporter, reportException_double)
+{
+  JunitXmlReporter reporter(rclcpp::get_logger("test_junit_xml_reporter"));
+  reporter.init("/tmp");
+  JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
+
+  EXPECT_NO_THROW(testcase.reportException("exception_type1", "Exception message 1"));
+  EXPECT_NO_THROW(testcase.reportException("exception_type2", "Exception message 2"));
+
+  EXPECT_NO_THROW(reporter.write());
+
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
+<testsuites failures="0" errors="2" tests="2">
+  <testsuite name="testsuite" failures="0" errors="2" tests="2">
+    <testcase name="testcase">
+      <error type="exception_type1" message="Exception message 1" />
+      <error type="exception_type2" message="Exception message 2" />
+    </testcase>
+  </testsuite>
+</testsuites>
+)";
+  EXPECT_STREQ(report.c_str(), ans.c_str());
+}
+
+TEST(JunitXmlReporter, reportStandStillAndTimeout)
+{
+  JunitXmlReporter reporter(rclcpp::get_logger("test_junit_xml_reporter"));
+  reporter.init("/tmp");
+  JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
+
+  EXPECT_NO_THROW(testcase.reportStandStill());
+  EXPECT_NO_THROW(testcase.reportTimeout());
+
+  EXPECT_NO_THROW(reporter.write());
+
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
+<testsuites failures="0" errors="2" tests="2">
+  <testsuite name="testsuite" failures="0" errors="2" tests="2">
+    <testcase name="testcase">
+      <error type="stand still" message="Ego seems to be stuck" />
+      <error type="timeout" message="Ego failed to reach goal within timeout" />
+    </testcase>
+  </testsuite>
+</testsuites>
+)";
+  EXPECT_STREQ(report.c_str(), ans.c_str());
+}
+
+TEST(JunitXmlReporter, reportAll)
+{
+  JunitXmlReporter reporter(rclcpp::get_logger("test_junit_xml_reporter"));
+  reporter.init("/tmp");
+  JunitXmlReporterTestCase testcase = reporter.spawnTestCase("testsuite", "testcase");
+
+  EXPECT_NO_THROW(
+    testcase.reportCollision(makeNPCDescription("npc", 1.0, makeLaneletPose(123, 5.0)), 10.0));
+  EXPECT_NO_THROW(testcase.reportStandStill());
+  EXPECT_NO_THROW(testcase.reportTimeout());
+  EXPECT_NO_THROW(testcase.reportException("test_exception", "The vehicle has collided with npc"));
+
+  EXPECT_NO_THROW(reporter.write());
+
+  const std::string report = readFile();
+  const std::string ans = R"(<?xml version="1.0"?>
+<testsuites failures="0" errors="4" tests="4">
+  <testsuite name="testsuite" failures="0" errors="4" tests="4">
+    <testcase name="testcase">
+      <error type="collision" message="npc and ego collided at 10s" />
+      <error type="stand still" message="Ego seems to be stuck" />
+      <error type="timeout" message="Ego failed to reach goal within timeout" />
+      <error type="test_exception" message="The vehicle has collided with npc" />
     </testcase>
   </testsuite>
 </testsuites>
