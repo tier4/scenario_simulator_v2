f3bbb5df9c3bdbda05dac7203e468874c7bbd333
Dawid Moszyński <dawid.moszynski@robotec.ai>
2024-02-08 12:41:59 +0100

    ref(zmq): apply clang reformat

--- simulation/simulation_interface/include/simulation_interface/zmq_multi_server.hpp
+++ simulation/simulation_interface/include/simulation_interface/zmq_multi_server.hpp
@@ -15,17 +15,16 @@
 #ifndef SIMULATION_INTERFACE__ZMQ_MULTI_SERVER_HPP_
 #define SIMULATION_INTERFACE__ZMQ_MULTI_SERVER_HPP_
 
-#include <rclcpp/rclcpp.hpp>
-#include <scenario_simulator_exception/exception.hpp>
-#include <simulation_interface/constants.hpp>
-#include <zmqpp/zmqpp.hpp>
-
 #include <simulation_api_schema.pb.h>
 
 #include <functional>
+#include <rclcpp/rclcpp.hpp>
+#include <scenario_simulator_exception/exception.hpp>
+#include <simulation_interface/constants.hpp>
 #include <string>
 #include <thread>
 #include <tuple>
+#include <zmqpp/zmqpp.hpp>
 
 namespace zeromq
 {
