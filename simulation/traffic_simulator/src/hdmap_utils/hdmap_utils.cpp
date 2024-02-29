c43382122b218927e8e59026c94edf90d4e23a8d
Mateusz Palczuk <mateusz.palczuk@robotec.ai>
2024-02-29 13:48:07 +0100

    Simplify code
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk@robotec.ai>

--- simulation/traffic_simulator/src/hdmap_utils/hdmap_utils.cpp
+++ simulation/traffic_simulator/src/hdmap_utils/hdmap_utils.cpp
@@ -510,7 +510,8 @@ auto HdMapUtils::matchToLane(
         bbox.center.x - bbox.dimensions.x * 0.5 * reduction_ratio,
         bbox.center.y - bbox.dimensions.y * 0.5 * reduction_ratio}},
     obj.pose);
-  auto matches = lanelet::matching::getDeterministicMatches(*lanelet_map_ptr_, obj, 3.0);
+  auto matches =
+    lanelet::matching::getDeterministicMatches(*lanelet_map_ptr_, obj, matching_distance);
   if (!include_crosswalk) {
     matches = lanelet::matching::removeNonRuleCompliantMatches(matches, traffic_rules_vehicle_ptr_);
   }
@@ -519,23 +520,17 @@ auto HdMapUtils::matchToLane(
   }
   std::vector<std::pair<lanelet::Id, double>> id_and_distance;
   for (const auto & match : matches) {
-    /**
-     * @note Hard coded parameter. Matching threshold for lanelet.
-     */
-    if (match.distance <= matching_distance) {
-      auto lanelet_pose = toLaneletPose(pose, match.lanelet.id(), matching_distance);
-      if (lanelet_pose) {
-        id_and_distance.emplace_back(lanelet_pose->lanelet_id, lanelet_pose->offset);
-      }
+    if (const auto lanelet_pose = toLaneletPose(pose, match.lanelet.id(), matching_distance)) {
+      id_and_distance.emplace_back(lanelet_pose->lanelet_id, lanelet_pose->offset);
     }
   }
   if (id_and_distance.empty()) {
     return std::nullopt;
   }
-  std::sort(id_and_distance.begin(), id_and_distance.end(), [](auto const & lhs, auto const & rhs) {
-    return lhs.second < rhs.second;
-  });
-  return id_and_distance[0].first;
+  const auto min_id_and_distance = std::min_element(
+    id_and_distance.begin(), id_and_distance.end(),
+    [](auto const & lhs, auto const & rhs) { return lhs.second < rhs.second; });
+  return min_id_and_distance->first;
 }
 
 auto HdMapUtils::toLaneletPose(
