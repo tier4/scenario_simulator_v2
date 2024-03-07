0450cbd08efd1a72589bd5cdbc0cf878a0ae5be9
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-03-07 13:42:50 +0900

    Update simulation/traffic_simulator/include/traffic_simulator/entity/entity_manager.hpp
    Co-authored-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation/traffic_simulator/include/traffic_simulator/entity/entity_manager.hpp
+++ simulation/traffic_simulator/include/traffic_simulator/entity/entity_manager.hpp
@@ -540,7 +540,7 @@ public:
             lanelet_pose) {
           entity_status.lanelet_pose = *lanelet_pose;
           entity_status.lanelet_pose_valid = true;
-          // @note fix z, roll and pitch to fitting to the lanelet
+          /// @note fix z, roll and pitch to fitting to the lanelet
           static bool consider_pose_by_road_slope =
             getParameter<bool>("consider_pose_by_road_slope", false);
           if (consider_pose_by_road_slope) {
