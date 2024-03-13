76bc24631c68acf93285a8d820229539e2d0a679
Masaya Kataoka <ms.kataoka@gmail.com>
2024-03-13 23:00:45 +0900

    make API::getDefaultMatchingDistanceForLaneletPoseCalculation to the private functions
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- simulation/traffic_simulator/include/traffic_simulator/api/api.hpp
+++ simulation/traffic_simulator/include/traffic_simulator/api/api.hpp
@@ -331,7 +331,6 @@ public:
   FORWARD_TO_ENTITY_MANAGER(getRelativePose);
   FORWARD_TO_ENTITY_MANAGER(getStandStillDuration);
   FORWARD_TO_ENTITY_MANAGER(getTraveledDistance);
-  FORWARD_TO_ENTITY_MANAGER(getDefaultMatchingDistanceForLaneletPoseCalculation);
   FORWARD_TO_ENTITY_MANAGER(getV2ITrafficLight);
   FORWARD_TO_ENTITY_MANAGER(getV2ITrafficLights);
   FORWARD_TO_ENTITY_MANAGER(isEgoSpawned);
@@ -359,6 +358,10 @@ public:
   FORWARD_TO_ENTITY_MANAGER(setVelocityLimit);
   FORWARD_TO_ENTITY_MANAGER(toMapPose);
 
+private:
+  FORWARD_TO_ENTITY_MANAGER(getDefaultMatchingDistanceForLaneletPoseCalculation);
+
+public:
 #undef FORWARD_TO_ENTITY_MANAGER
 
   auto canonicalize(const LaneletPose & maybe_non_canonicalized_lanelet_pose) const
