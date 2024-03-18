b31aa8f3b080caec62e4078ad28dcdd2094e4a28
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-03-18 14:03:53 +0900

    feat: support UNKNOWN for traffic light shape in scenario

--- simulation/traffic_simulator/include/traffic_simulator/traffic_lights/traffic_light.hpp
+++ simulation/traffic_simulator/include/traffic_simulator/traffic_lights/traffic_light.hpp
@@ -135,26 +135,29 @@ struct TrafficLight
       circle,
       cross,
       arrow,
+      unknown,
     };
 
     // clang-format off
-    static_assert(static_cast<std::uint8_t>(Category::circle) == 0b0000'0000);
-    static_assert(static_cast<std::uint8_t>(Category::cross ) == 0b0000'0001);
-    static_assert(static_cast<std::uint8_t>(Category::arrow ) == 0b0000'0010);
+    static_assert(static_cast<std::uint8_t>(Category::circle ) == 0b0000'0000);
+    static_assert(static_cast<std::uint8_t>(Category::cross  ) == 0b0000'0001);
+    static_assert(static_cast<std::uint8_t>(Category::arrow  ) == 0b0000'0010);
+    static_assert(static_cast<std::uint8_t>(Category::unknown) == 0b0000'0011);
     // clang-format on
 
     enum Value : std::uint16_t {
       // clang-format off
-      circle      =                      static_cast<std::uint8_t>(Category::circle),
-      cross       =                      static_cast<std::uint8_t>(Category::cross ),
-      left        = (0b0000'1000 << 8) | static_cast<std::uint8_t>(Category::arrow ),
-      down        = (0b0000'0100 << 8) | static_cast<std::uint8_t>(Category::arrow ),
-      up          = (0b0000'0010 << 8) | static_cast<std::uint8_t>(Category::arrow ),
-      right       = (0b0000'0001 << 8) | static_cast<std::uint8_t>(Category::arrow ),
-      lower_left  = (0b0000'1100 << 8) | static_cast<std::uint8_t>(Category::arrow ),
-      upper_left  = (0b0000'1010 << 8) | static_cast<std::uint8_t>(Category::arrow ),
-      lower_right = (0b0000'0101 << 8) | static_cast<std::uint8_t>(Category::arrow ),
-      upper_right = (0b0000'0011 << 8) | static_cast<std::uint8_t>(Category::arrow ),
+      circle      =                      static_cast<std::uint8_t>(Category::circle ),
+      cross       =                      static_cast<std::uint8_t>(Category::cross  ),
+      left        = (0b0000'1000 << 8) | static_cast<std::uint8_t>(Category::arrow  ),
+      down        = (0b0000'0100 << 8) | static_cast<std::uint8_t>(Category::arrow  ),
+      up          = (0b0000'0010 << 8) | static_cast<std::uint8_t>(Category::arrow  ),
+      right       = (0b0000'0001 << 8) | static_cast<std::uint8_t>(Category::arrow  ),
+      lower_left  = (0b0000'1100 << 8) | static_cast<std::uint8_t>(Category::arrow  ),
+      upper_left  = (0b0000'1010 << 8) | static_cast<std::uint8_t>(Category::arrow  ),
+      lower_right = (0b0000'0101 << 8) | static_cast<std::uint8_t>(Category::arrow  ),
+      upper_right = (0b0000'0011 << 8) | static_cast<std::uint8_t>(Category::arrow  ),
+      unknown     =                      static_cast<std::uint8_t>(Category::unknown),
       // clang-format on
     } value;
 
@@ -169,6 +172,7 @@ struct TrafficLight
     static_assert(static_cast<std::uint16_t>(upper_left ) == 0b0000'1010'0000'0010);
     static_assert(static_cast<std::uint16_t>(lower_right) == 0b0000'0101'0000'0010);
     static_assert(static_cast<std::uint16_t>(upper_right) == 0b0000'0011'0000'0010);
+    static_assert(static_cast<std::uint16_t>(unknown    ) == 0b0000'0000'0000'0011);
     // clang-format on
 
     constexpr Shape(const Value value = circle) : value(value) {}
@@ -186,6 +190,7 @@ struct TrafficLight
       std::make_pair("upperLeft", Shape::upper_left),
       std::make_pair("lowerRight", Shape::lower_right),
       std::make_pair("upperRight", Shape::upper_right),
+      std::make_pair("unknown", Shape::unknown),
 
       // BACKWARD COMPATIBILITY
       std::make_pair("straight", Shape::up),
@@ -309,6 +314,8 @@ struct TrafficLight
             return simulation_api_schema::TrafficLight_Shape_UP_LEFT_ARROW;
           case Shape::upper_right:
             return simulation_api_schema::TrafficLight_Shape_UP_RIGHT_ARROW;
+          case Shape::unknown:
+            return simulation_api_schema::TrafficLight_Shape_UNKNOWN_SHAPE;
           default:
             throw common::SyntaxError(std::get<Shape>(value), " is not supported as a shape.");
         }
