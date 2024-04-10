29fdcbfd84f3f42f667d76a76eed1a3c1862b80d
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-04-10 14:37:33 +0900

    refactor: fix shellcheck errors

--- .github/workflows/workflow.sh
+++ .github/workflows/workflow.sh
@@ -2,8 +2,8 @@
 
 FILE_PATH="$1"
 
+# Arguments other than file path are passed to the command's arguments
 shift 1
-OPTIONS = "$@"
 
 if [ ! -f "$FILE_PATH" ]; then
     echo "No such file: $FILE_PATH"
@@ -14,7 +14,7 @@ exit_status=0
 
 while IFS= read -r line
 do
-    ros2 launch scenario_test_runner scenario_test_runner.launch.py scenario:="$line" $OPTIONS
+    ros2 launch scenario_test_runner scenario_test_runner.launch.py scenario:="$line" "$@"
     ros2 run scenario_test_runner result_checker.py /tmp/scenario_test_runner/result.junit.xml
     cmd_exit_status=$?
     if [ $cmd_exit_status -ne 0 ]; then
