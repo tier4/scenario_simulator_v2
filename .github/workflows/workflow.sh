de2d26ed92f7df11a520b35a01949fcc7de31426
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-04-16 18:06:25 +0900

    refactor: replace cmd with command

--- .github/workflows/workflow.sh
+++ .github/workflows/workflow.sh
@@ -16,9 +16,9 @@ while IFS= read -r line
 do
   ros2 launch scenario_test_runner scenario_test_runner.launch.py scenario:="$line" "$@"
   ros2 run scenario_test_runner result_checker.py /tmp/scenario_test_runner/result.junit.xml
-  cmd_exit_status=$?
-  if [ $cmd_exit_status -ne 0 ]; then
-    echo "Error: caught non-zero exit status（code: $cmd_exit_status)"
+  command_exit_status=$?
+  if [ $command_exit_status -ne 0 ]; then
+    echo "Error: caught non-zero exit status（code: $command_exit_status)"
     exit_status=1
   fi
 done < "$file_path"
