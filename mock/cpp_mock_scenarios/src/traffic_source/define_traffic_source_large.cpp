fb7e3f0b8874c6ec45837ca381a6f8252899b1d7
Mateusz Palczuk <mateusz.palczuk@robotec.ai>
2024-05-08 10:09:43 +0200

    Remove "headers in STL" comments
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk@robotec.ai>

--- mock/cpp_mock_scenarios/src/traffic_source/define_traffic_source_large.cpp
+++ mock/cpp_mock_scenarios/src/traffic_source/define_traffic_source_large.cpp
@@ -17,14 +17,12 @@
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <cpp_mock_scenarios/catalogs.hpp>
 #include <cpp_mock_scenarios/cpp_scenario_node.hpp>
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
+#include <string>
 #include <traffic_simulator/api/api.hpp>
 #include <traffic_simulator/data_type/entity_status.hpp>
 #include <traffic_simulator_msgs/msg/behavior_parameter.hpp>
-
-// headers in STL
-#include <memory>
-#include <string>
 #include <vector>
 
 /**
