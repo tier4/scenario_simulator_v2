18dc190c4cf04c0667153ec873d84e5d1f71994a
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-05-14 18:23:44 +0900

    feat: integrate TrafficLightPublisher for evaluator to EntityManager

--- simulation/traffic_simulator/include/traffic_simulator/entity/entity_manager.hpp
+++ simulation/traffic_simulator/include/traffic_simulator/entity/entity_manager.hpp
@@ -29,6 +29,7 @@
 #include <stdexcept>
 #include <string>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tier4_simulation_msgs/msg/traffic_light_array_v1.hpp>
 #include <traffic_simulator/api/configuration.hpp>
 #include <traffic_simulator/data_type/lane_change.hpp>
 #include <traffic_simulator/data_type/speed_change.hpp>
@@ -113,6 +114,8 @@ class EntityManager
   const std::shared_ptr<TrafficLightManager> conventional_traffic_light_manager_ptr_;
   const std::shared_ptr<TrafficLightMarkerPublisher>
     conventional_traffic_light_marker_publisher_ptr_;
+  const std::shared_ptr<TrafficLightPublisherBase>
+    conventional_backward_compatible_traffic_light_publisher_ptr_;
 
   const std::shared_ptr<TrafficLightManager> v2i_traffic_light_manager_ptr_;
   const std::shared_ptr<TrafficLightMarkerPublisher> v2i_traffic_light_marker_publisher_ptr_;
@@ -177,6 +180,9 @@ public:
       std::make_shared<TrafficLightManager>(hdmap_utils_ptr_)),
     conventional_traffic_light_marker_publisher_ptr_(
       std::make_shared<TrafficLightMarkerPublisher>(conventional_traffic_light_manager_ptr_, node)),
+    conventional_backward_compatible_traffic_light_publisher_ptr_(
+      std::make_shared<TrafficLightPublisher<tier4_simulation_msgs::msg::TrafficLightArrayV1>>(
+        "/simulation/traffic_lights", node, hdmap_utils_ptr_)),
     v2i_traffic_light_manager_ptr_(std::make_shared<TrafficLightManager>(hdmap_utils_ptr_)),
     v2i_traffic_light_marker_publisher_ptr_(
       std::make_shared<TrafficLightMarkerPublisher>(v2i_traffic_light_manager_ptr_, node)),
@@ -193,8 +199,12 @@ public:
         v2i_traffic_light_legacy_topic_publisher_ptr_->publish(
           clock_ptr_->now(), v2i_traffic_light_manager_ptr_->generateUpdateTrafficLightsRequest());
       }),
-    conventional_traffic_light_updater_(
-      node, [this]() { conventional_traffic_light_marker_publisher_ptr_->publish(); })
+    conventional_traffic_light_updater_(node, [this]() {
+      conventional_traffic_light_marker_publisher_ptr_->publish();
+      conventional_backward_compatible_traffic_light_publisher_ptr_->publish(
+        clock_ptr_->now(),
+        conventional_traffic_light_manager_ptr_->generateUpdateTrafficLightsRequest());
+    })
   {
     updateHdmapMarker();
   }
