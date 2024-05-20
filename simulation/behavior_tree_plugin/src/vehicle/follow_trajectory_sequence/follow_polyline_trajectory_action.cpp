a082f057bf4c9d913b2ea9c836a6fb4b018f0f0a
Dawid Moszynski <dawid.moszynski@robotec.ai>
2024-05-20 10:44:06 +0200

    ref(behavior_tree_plugin): rename updated_status variable

--- simulation/behavior_tree_plugin/src/vehicle/follow_trajectory_sequence/follow_polyline_trajectory_action.cpp
+++ simulation/behavior_tree_plugin/src/vehicle/follow_trajectory_sequence/follow_polyline_trajectory_action.cpp
@@ -74,13 +74,12 @@ auto FollowPolylineTrajectoryAction::tick() -> BT::NodeStatus
     THROW_SIMULATION_ERROR(
       "Time in entity_status is NaN - FollowTrajectoryAction does not support such case.");
   } else if (
-    const auto updated_canonicalized_status =
-      traffic_simulator::follow_trajectory::makeUpdatedStatus(
-        static_cast<traffic_simulator::EntityStatus>(*entity_status), *polyline_trajectory,
-        behavior_parameter, hdmap_utils, step_time, getTargetSpeed())) {
+    const auto updated_status = traffic_simulator::follow_trajectory::makeUpdatedStatus(
+      static_cast<traffic_simulator::EntityStatus>(*entity_status), *polyline_trajectory,
+      behavior_parameter, hdmap_utils, step_time, getTargetSpeed())) {
     setOutput(
       "non_canonicalized_updated_status",
-      std::make_shared<traffic_simulator::EntityStatus>(updated_canonicalized_status.value()));
+      std::make_shared<traffic_simulator::EntityStatus>(updated_status.value()));
     setOutput("waypoints", calculateWaypoints());
     setOutput("obstacle", calculateObstacle(calculateWaypoints()));
     return BT::NodeStatus::RUNNING;
