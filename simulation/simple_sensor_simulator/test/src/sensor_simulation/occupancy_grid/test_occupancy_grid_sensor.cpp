07b8e6be3570f21955fcaaf5155a8d70ce4eb6f7
robomic <michal.ciasnocha@robotec.ai>
2024-06-06 09:35:58 +0200

    remove simple sensor simulator tets

--- simulation/simple_sensor_simulator/test/src/sensor_simulation/occupancy_grid/test_occupancy_grid_sensor.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-#include <simple_sensor_simulator/sensor_simulation/occupancy_grid/occupancy_grid_sensor.hpp>
-
-int main(int argc, char ** argv)
-{
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
--- simulation/simple_sensor_simulator/test/src/vehicle_simulation/vehicle_model/test_sim_model_interface.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-#include <simple_sensor_simulator/vehicle_simulation/vehicle_model/sim_model_interface.hpp>
-
-int main(int argc, char ** argv)
-{
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
