9255d231659845d25aa9054d610603a63b63cc81
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-06-18 14:17:52 +0900

    feat(openscenario_interpreter): add RelativeLaneRange

---
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/syntax/relative_lane_range.hpp
@@ -0,0 +1,45 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef OPENSCENARIO_INTERPRETER__SYNTAX__RELATIVE_LANE_RANGE_HPP_
+#define OPENSCENARIO_INTERPRETER__SYNTAX__RELATIVE_LANE_RANGE_HPP_
+
+#include <openscenario_interpreter/scope.hpp>
+#include <pugixml.hpp>
+#include <traffic_simulator/data_type/lane_change.hpp>
+
+namespace openscenario_interpreter
+{
+inline namespace syntax
+{
+/*
+   RelativeLaneRange (OpenSCENARIO XML 1.3)
+
+   <xsd:complexType name="RelativeLaneRange">
+     <xsd:attribute name="from" type="Int"/>
+     <xsd:attribute name="to" type="Int"/>
+   </xsd:complexType>
+*/
+struct RelativeLaneRange
+{
+  const Integer from;
+
+  const Integer to;
+
+  explicit RelativeLaneRange(const pugi::xml_node &, Scope &);
+};
+}  // namespace syntax
+}  // namespace openscenario_interpreter
+
+#endif  // OPENSCENARIO_INTERPRETER__SYNTAX__RELATIVE_LANE_RANGE_HPP_
