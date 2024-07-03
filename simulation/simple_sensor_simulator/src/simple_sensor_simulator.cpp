9baf4b93a63f6ea46628fb37a6ff453930e57f24
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-07-03 09:32:58 +0900

    fix(concealer): simulate hardware override

--- simulation/simple_sensor_simulator/src/simple_sensor_simulator.cpp
+++ simulation/simple_sensor_simulator/src/simple_sensor_simulator.cpp
@@ -156,20 +156,15 @@ auto ScenarioSimulator::updateEntityStatus(
     try {
       if (isEgo(status.name())) {
         assert(ego_entity_simulation_ && "Ego is spawned but ego_entity_simulation_ is nullptr!");
-        if (ego_entity_simulation_->autoware->getControlModeReport().mode == autoware_auto_vehicle_msgs::msg::ControlModeReport::MANUAL){
-            traffic_simulator_msgs::msg::EntityStatus ego_status_msg;
-            simulation_interface::toMsg(status, ego_status_msg);
-            ego_entity_simulation_->overwrite(
-                ego_status_msg, current_scenario_time_ + step_time_, step_time_,
-                req.npc_logic_started());
-        } else if(ego_entity_simulation_->autoware->getControlModeReport().mode == autoware_auto_vehicle_msgs::msg::ControlModeReport::AUTONOMOUS){
+        if (req.overwrite_ego_status()) {
+          traffic_simulator_msgs::msg::EntityStatus ego_status_msg;
+          simulation_interface::toMsg(status, ego_status_msg);
+          ego_entity_simulation_->overwrite(
+            ego_status_msg, current_scenario_time_ + step_time_, step_time_,
+            req.npc_logic_started());
+        } else {
           ego_entity_simulation_->update(
             current_scenario_time_ + step_time_, step_time_, req.npc_logic_started());
-        }else{
-          std::stringstream what;
-          what << "Unexpected autoware control mode for simple_sensor_simulator is given: " << ego_entity_simulation_->autoware->getControlModeReport().mode;
-          what << ". Expected: " << autoware_auto_vehicle_msgs::msg::ControlModeReport::MANUAL << " or " << autoware_auto_vehicle_msgs::msg::ControlModeReport::AUTONOMOUS << ".";
-          throw common::Error(what.str());
         }
         simulation_api_schema::EntityStatus ego_status;
         simulation_interface::toProto(ego_entity_simulation_->getStatus(), ego_status);
