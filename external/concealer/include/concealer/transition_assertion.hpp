5dd0c2d83e65e958660239a49f3f924dab9993a5
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-11-28 19:49:23 +0900

    refactor: delete unused include

--- external/concealer/include/concealer/transition_assertion.hpp
+++ external/concealer/include/concealer/transition_assertion.hpp
@@ -15,7 +15,6 @@
 #ifndef CONCEALER__TRANSITION_ASSERTION_HPP_
 #define CONCEALER__TRANSITION_ASSERTION_HPP_
 
-#include <autoware_system_msgs/msg/autoware_state.hpp>
 #include <chrono>
 #include <rclcpp/node.hpp>
 #include <rclcpp/rate.hpp>
