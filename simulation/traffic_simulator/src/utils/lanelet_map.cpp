ebe7b5f056922005d7cf132b6cc324c3ed11d1e0
Dawid Moszynski <dawid.moszynski@robotec.ai>
2024-12-18 22:05:05 +0100

    feat(traffic_simulator, geometry): add CatmullRomSpline::getAltitudeRange and use it in splineDistanceToBoundingBox - as on master

--- simulation/traffic_simulator/src/utils/lanelet_map.cpp
+++ simulation/traffic_simulator/src/utils/lanelet_map.cpp
@@ -22,6 +22,12 @@ auto laneletLength(const lanelet::Id lanelet_id) -> double
 {
   return lanelet_wrapper::lanelet_map::laneletLength(lanelet_id);
 }
+auto laneletAltitude(
+  const lanelet::Id & lanelet_id, const geometry_msgs::msg::Pose & pose,
+  const double matching_distance) -> std::optional<double>
+{
+  return lanelet_wrapper::lanelet_map::laneletAltitude(lanelet_id, pose, matching_distance);
+}
 
 auto nearbyLaneletIds(
   const Pose & pose, const double distance_thresh, const bool include_crosswalk,
