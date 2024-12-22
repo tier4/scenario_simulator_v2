bac3ffd9d4b25f2fcb9cf9b392b89875d3bc0668
satoshi-ota <satoshi.ota928@gmail.com>
2024-12-22 18:59:27 +0900

    fix: respawn ego mock test
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- mock/cpp_mock_scenarios/src/respawn_ego/respawn_ego.cpp
+++ mock/cpp_mock_scenarios/src/respawn_ego/respawn_ego.cpp
@@ -39,7 +39,8 @@ public:
         goal_msg.header.frame_id = "map";
         goal_msg.pose = static_cast<geometry_msgs::msg::Pose>(goal_pose);
         api_.respawn("ego", message, goal_msg);
-      })}
+      })},
+    has_respawned{false}
   {
     start();
   }
@@ -47,8 +48,29 @@ public:
 private:
   void onUpdate() override
   {
-    if (api_.getCurrentTime() >= 30) {
+    if (api_.getCurrentTime() >= 60) {
+      stop(cpp_mock_scenarios::Result::FAILURE);
+    }
+
+    if (api_.isInLanelet("ego", 34564, 0.1)) {
       stop(cpp_mock_scenarios::Result::SUCCESS);
+    }
+
+    if (api_.getCurrentTime() >= 10 && !has_respawned) {
+      geometry_msgs::msg::PoseWithCovarianceStamped ego_pose;
+      ego_pose.header.frame_id = "map";
+      ego_pose.pose.pose = static_cast<geometry_msgs::msg::Pose>(
+        traffic_simulator::helper::constructCanonicalizedLaneletPose(
+          34576, 10.0, 0.0, api_.getHdmapUtils()));
+
+      geometry_msgs::msg::PoseStamped goal_pose;
+      goal_pose.header.frame_id = "map";
+      goal_pose.pose = static_cast<geometry_msgs::msg::Pose>(
+        traffic_simulator::helper::constructCanonicalizedLaneletPose(
+          34564, 10.0, 0.0, api_.getHdmapUtils()));
+      api_.respawn("ego", ego_pose, goal_pose);
+
+      has_respawned = true;
     }
   }
 
@@ -65,6 +87,8 @@ private:
 
   const rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr
     new_position_subscriber;
+
+  bool has_respawned;
 };
 }  // namespace cpp_mock_scenarios
 
