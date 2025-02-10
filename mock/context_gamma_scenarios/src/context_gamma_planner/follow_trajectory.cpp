215ccba213200cac162163ced8688d574b08bc5b
Masaya Kataoka <ms.kataoka@gmail.com>
2025-02-10 16:04:17 +0900

    port follow_trajectory scenario
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- mock/context_gamma_scenarios/src/context_gamma_planner/follow_trajectory.cpp
+++ mock/context_gamma_scenarios/src/context_gamma_planner/follow_trajectory.cpp
@@ -47,7 +47,7 @@ private:
   {
     const auto t = api_.getCurrentTime();
     const double distance_threshold = 4.0;
-    const auto ego_pos = toEigenVector3d(api_.getEntityStatus("ego").getMapPose().position);
+    const auto ego_pos = toEigenVector3d(api_.getEntity("ego").getMapPose().position);
     const auto reference_pos =
       toEigenVector3d(follow_trajectory.shape.vertices.at(vertex_num).position.position);
 
@@ -71,10 +71,11 @@ private:
     api_.spawn(
       "ego", traffic_simulator::helper::constructCanonicalizedLaneletPose(34579, 0, 0),
       getVehicleParameters(), traffic_simulator::VehicleBehavior::contextGamma());
-    api_.setEntityStatus(
-      "ego", traffic_simulator::helper::constructCanonicalizedLaneletPose(34579, 0, 0),
+    auto & ego = api_.getEntity("ego");
+    ego.setStatus(
+      traffic_simulator::helper::constructCanonicalizedLaneletPose(34579, 0, 0),
       traffic_simulator::helper::constructActionStatus(10));
-    api_.requestSpeedChange("ego", 7, true);
+    ego.requestSpeedChange(7, true);
 
     auto toVertex = [](double time, const geometry_msgs::msg::Pose & pose) {
       traffic_simulator_msgs::msg::Vertex vertex;
@@ -113,7 +114,7 @@ private:
     follow_trajectory.closed = false;
     std::shared_ptr<traffic_simulator_msgs::msg::PolylineTrajectory> follow_trajectory_ptr =
       std::make_shared<traffic_simulator_msgs::msg::PolylineTrajectory>(follow_trajectory);
-    api_.requestFollowTrajectory("ego", follow_trajectory_ptr);
+    ego.requestFollowTrajectory(follow_trajectory_ptr);
   }
 };
 
