d3436b9e838b129df21acf81c32349257274e5ac
koki suzuki <curious.ks.jp@gmail.com>
2025-02-12 18:04:14 +0900

    Refactor scenario class name and method name for getting distance to lane bound

--- mock/cpp_mock_scenarios/src/measurement/get_distance_in_lane_coordinate_distance.cpp
+++ mock/cpp_mock_scenarios/src/measurement/get_distance_in_lane_coordinate_distance.cpp
@@ -25,12 +25,12 @@
 
 namespace cpp_mock_scenarios
 {
-class GetDistanceInLaneCoordinateScenario : public cpp_mock_scenarios::CppScenarioNode
+class GetDistanceToLaneBoundScenario : public cpp_mock_scenarios::CppScenarioNode
 {
 public:
-  explicit GetDistanceInLaneCoordinateScenario(const rclcpp::NodeOptions & option)
+  explicit GetDistanceToLaneBoundScenario(const rclcpp::NodeOptions & option)
   : cpp_mock_scenarios::CppScenarioNode(
-      "get_longitudinal_distance",
+      "get_distance_to_lane_bound",
       ament_index_cpp::get_package_share_directory("kashiwanoha_map") + "/map", "lanelet2_map.osm",
       __FILE__, false, option)
   {
@@ -39,99 +39,18 @@ public:
 
 private:
   bool requested = false;
-
-  /// @todo it should be separated into the API helper collection for the cpp_mock_scenario
-  auto lateralDistance(const std::string & from_entity_name, const std::string & to_entity_name)
-    -> std::optional<double>
-  {
-    const auto & from_entity = api_.getEntity(from_entity_name);
-    const auto & to_entity = api_.getEntity(to_entity_name);
-    if (from_entity.isInLanelet() && to_entity.isInLanelet()) {
-      return traffic_simulator::distance::lateralDistance(
-        from_entity.getCanonicalizedLaneletPose().value(),
-        to_entity.getCanonicalizedLaneletPose().value(), traffic_simulator::RoutingConfiguration(),
-        api_.getHdmapUtils());
-    }
-    return std::nullopt;
-  };
-
-  auto lateralDistance(
-    const std::string & from_entity_name, const std::string & to_entity_name,
-    const double matching_distance) -> std::optional<double>
-  {
-    const auto from_entity_lanelet_pose =
-      api_.getEntity(from_entity_name).getCanonicalizedLaneletPose(matching_distance);
-    const auto to_entity_lanelet_pose =
-      api_.getEntity(to_entity_name).getCanonicalizedLaneletPose(matching_distance);
-    if (from_entity_lanelet_pose && to_entity_lanelet_pose) {
-      return traffic_simulator::distance::lateralDistance(
-        from_entity_lanelet_pose.value(), to_entity_lanelet_pose.value(),
-        traffic_simulator::RoutingConfiguration());
-    }
-    return std::nullopt;
-  };
-
-  auto longitudinalDistance(
-    const std::string & from_entity_name, const std::string & to_entity_name)
-    -> std::optional<double>
-  {
-    const auto & from_entity = api_.getEntity(from_entity_name);
-    const auto & to_entity = api_.getEntity(to_entity_name);
-    if (from_entity.isInLanelet() && to_entity.isInLanelet()) {
-      return traffic_simulator::distance::longitudinalDistance(
-        from_entity.getCanonicalizedLaneletPose().value(),
-        to_entity.getCanonicalizedLaneletPose().value(), false, false,
-        traffic_simulator::RoutingConfiguration());
-    }
-    return std::nullopt;
-  }
-
   void onUpdate() override
   {
     if (api_.getCurrentTime() >= 10.0) {
       stop(cpp_mock_scenarios::Result::SUCCESS);
     }
-    const auto distance_to_front = longitudinalDistance("ego", "front");
-    const auto distance_to_behind = longitudinalDistance("ego", "behind");
-    const auto lateral_none = lateralDistance("ego", "front", 0.1);
-    const auto lateral_about_one = lateralDistance("ego", "front", 1.5);
-    const auto lateral_to_front = lateralDistance("ego", "front");
-    const auto lateral_to_behind = lateralDistance("ego", "behind");
+    auto & ego_entity = api_.getEntity("ego");
+    const auto distance = traffic_simulator::distance::distanceToLaneBound(
+      ego_entity.getMapPose(), ego_entity.getBoundingBox(), ego_entity.getRouteLanelets(),
+      api_.getHdmapUtils());
     // LCOV_EXCL_START
-    if (lateral_none) {
-      return stop(cpp_mock_scenarios::Result::FAILURE);
-    }
-    if (!lateral_about_one) {
-      return stop(cpp_mock_scenarios::Result::FAILURE);
-    }
-    if (lateral_about_one && !equals(lateral_about_one.value(), 1.0, 0.001)) {
-      return stop(cpp_mock_scenarios::Result::FAILURE);
-    }
-    if (lateral_to_front && !equals(lateral_to_front.value(), 1.0)) {
-      return stop(cpp_mock_scenarios::Result::FAILURE);
-    }
-    if (lateral_to_behind && !equals(lateral_to_behind.value(), -1.0)) {
-      return stop(cpp_mock_scenarios::Result::FAILURE);
-    }
-    if (!distance_to_front) {
+    if (distance <= 0.4 && distance >= 0.52) {
       stop(cpp_mock_scenarios::Result::FAILURE);
-      return;
-    }
-    if (!lateral_to_behind) {
-      stop(cpp_mock_scenarios::Result::FAILURE);
-      return;
-    }
-    if (distance_to_front.value() >= 5.1 || 4.9 >= distance_to_front.value()) {
-      stop(cpp_mock_scenarios::Result::FAILURE);
-      return;
-    }
-    if (!distance_to_behind) {
-      stop(cpp_mock_scenarios::Result::FAILURE);
-      return;
-    }
-    if (distance_to_behind.value() >= -4.9 || -5.1 >= distance_to_behind.value()) {
-      stop(cpp_mock_scenarios::Result::FAILURE);
-      return;
     }
     // LCOV_EXCL_STOP
   }
@@ -143,20 +62,6 @@ private:
     auto & ego_entity = api_.getEntity("ego");
     ego_entity.setLinearVelocity(10);
     ego_entity.requestSpeedChange(3, true);
-
-    api_.spawn(
-      "front", traffic_simulator::helper::constructCanonicalizedLaneletPose(34513, 10.0, 1.0),
-      getVehicleParameters());
-    auto & front_entity = api_.getEntity("front");
-    front_entity.setLinearVelocity(10);
-    front_entity.requestSpeedChange(3, true);
-
-    api_.spawn(
-      "behind", traffic_simulator::helper::constructCanonicalizedLaneletPose(34513, 0.0, -1.0),
-      getVehicleParameters());
-    auto & behind_entity = api_.getEntity("behind");
-    behind_entity.setLinearVelocity(10);
-    behind_entity.requestSpeedChange(3, true);
   }
 };
 }  // namespace cpp_mock_scenarios
@@ -165,8 +70,7 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions options;
-  auto component =
-    std::make_shared<cpp_mock_scenarios::GetDistanceInLaneCoordinateScenario>(options);
+  auto component = std::make_shared<cpp_mock_scenarios::GetDistanceToLaneBoundScenario>(options);
   rclcpp::spin(component);
   rclcpp::shutdown();
   return 0;
