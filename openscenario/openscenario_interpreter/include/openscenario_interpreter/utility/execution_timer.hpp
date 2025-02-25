f6215738cbd55475e7968f4bdb492b9c26431939
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-02-25 16:36:21 +0900

    refactor: use Boost.Accumulators for statistics

--- openscenario/openscenario_interpreter/include/openscenario_interpreter/utility/execution_timer.hpp
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/utility/execution_timer.hpp
@@ -15,6 +15,8 @@
 #ifndef OPENSCENARIO_INTERPRETER__UTILITY__EXECUTION_TIMER_HPP_
 #define OPENSCENARIO_INTERPRETER__UTILITY__EXECUTION_TIMER_HPP_
 
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics.hpp>
 #include <chrono>
 #include <cmath>
 #include <functional>
@@ -27,69 +29,11 @@ inline namespace utility
 template <typename Clock = std::chrono::system_clock>
 class ExecutionTimer
 {
-  class Statistics
-  {
-    std::int64_t ns_max = 0;
-
-    std::int64_t ns_min = std::numeric_limits<std::int64_t>::max();
-
-    std::int64_t ns_sum = 0;
-
-    std::int64_t ns_square_sum = 0;
-
-    int count = 0;
-
-  public:
-    template <typename Duration>
-    auto add(Duration diff) -> void
-    {
-      std::int64_t diff_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count();
-      count++;
-      ns_max = std::max(ns_max, diff_ns);
-      ns_min = std::min(ns_max, diff_ns);
-      ns_sum += diff_ns;
-      ns_square_sum += std::pow(diff_ns, 2);
-    }
-
-    template <typename T>
-    auto max() const
-    {
-      return std::chrono::duration_cast<T>(std::chrono::nanoseconds(ns_max));
-    }
-
-    template <typename T>
-    auto min() const
-    {
-      return std::chrono::duration_cast<T>(std::chrono::nanoseconds(ns_min));
-    }
-
-    template <typename T>
-    auto mean() const
-    {
-      return std::chrono::duration_cast<T>(std::chrono::nanoseconds(ns_sum / count));
-    }
-
-    template <typename T>
-    auto standardDeviation() const
-    {
-      std::int64_t mean_of_square = ns_square_sum / count;
-      std::int64_t square_of_mean = std::pow(ns_sum / count, 2);
-      std::int64_t var = mean_of_square - square_of_mean;
-      double standard_deviation = std::sqrt(var);
-      return std::chrono::duration_cast<T>(
-        std::chrono::nanoseconds(static_cast<std::int64_t>(standard_deviation)));
-    }
-
-    friend auto operator<<(std::ostream & os, const Statistics & statistics) -> std::ostream &
-    {
-      using namespace std::chrono;
-
-      return os << "mean = " << statistics.template mean<milliseconds>().count() << " ms, "
-                << "max = " << statistics.template max<milliseconds>().count() << " ms, "
-                << "standard deviation = "
-                << statistics.template standardDeviation<milliseconds>().count() / 1000.0 << " ms";
-    }
-  };
+  using Statistics = boost::accumulators::accumulator_set<
+    std::int64_t,
+    boost::accumulators::stats<
+      boost::accumulators::tag::min, boost::accumulators::tag::max, boost::accumulators::tag::mean,
+      boost::accumulators::tag::sum, boost::accumulators::tag::variance>>;
 
   std::unordered_map<std::string, Statistics> statistics_map;
 
@@ -103,7 +47,7 @@ public:
 
     const auto end = Clock::now();
 
-    statistics_map[tag].add(end - begin);
+    statistics_map[tag](std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count());
 
     return end - begin;
   }
