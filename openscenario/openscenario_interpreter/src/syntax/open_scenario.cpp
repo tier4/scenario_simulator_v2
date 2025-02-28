70cc92f5234eb2c70cef39798a5ce991b83cb922
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-02-28 13:20:49 +0900

    fix: add include boost/json/src.hpp to fix build error

--- openscenario/openscenario_interpreter/src/syntax/open_scenario.cpp
+++ openscenario/openscenario_interpreter/src/syntax/open_scenario.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <boost/json/src.hpp>
 #include <openscenario_interpreter/reader/element.hpp>
 #include <openscenario_interpreter/syntax/open_scenario.hpp>
 #include <openscenario_interpreter/syntax/open_scenario_category.hpp>
