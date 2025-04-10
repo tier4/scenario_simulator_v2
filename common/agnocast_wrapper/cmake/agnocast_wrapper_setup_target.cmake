7001a32d7af1e5ed0e0e4766d2e8814be538a015
Mateusz Palczuk <mateusz.palczuk@robotec.ai>
2025-04-10 18:28:07 +0200

    Add copyright to cmake function
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk@robotec.ai>

--- common/agnocast_wrapper/cmake/agnocast_wrapper_setup_target.cmake
+++ common/agnocast_wrapper/cmake/agnocast_wrapper_setup_target.cmake
@@ -1,3 +1,17 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 # based on https://github.com/veqcc/autoware.universe/blob/252ae60788a8388585780a6b1935a5682c688464/common/autoware_agnocast_wrapper/cmake/agnocast_wrapper_config_extras.cmake#L1-L8
 function(agnocast_wrapper_setup_target target)
   if(DEFINED ENV{ENABLE_AGNOCAST_SIMULATOR} AND "$ENV{ENABLE_AGNOCAST_SIMULATOR}" STREQUAL "1")
