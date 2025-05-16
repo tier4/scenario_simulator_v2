c5990ab29d442f9f0d3f20e0ce1884431b2da968
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-05-16 17:54:02 +0900

    Resolve allow_goal_modification from parameter and property in AcquirePositionAction::start

--- openscenario/openscenario_interpreter/src/syntax/acquire_position_action.cpp
+++ openscenario/openscenario_interpreter/src/syntax/acquire_position_action.cpp
@@ -15,6 +15,9 @@
 #include <openscenario_interpreter/reader/element.hpp>
 #include <openscenario_interpreter/simulator_core.hpp>
 #include <openscenario_interpreter/syntax/acquire_position_action.hpp>
+#include <openscenario_interpreter/syntax/controller.hpp>
+#include <openscenario_interpreter/syntax/entities.hpp>
+#include <openscenario_interpreter/syntax/scenario_object.hpp>
 #include <openscenario_interpreter/utility/overload.hpp>
 
 namespace openscenario_interpreter
@@ -50,9 +53,44 @@ auto AcquirePositionAction::start() -> void
       return applyAcquirePositionAction(actor, static_cast<NativeLanePosition>(position), options);
     });
 
+  auto allow_goal_modification_from_parameter = [&]() -> bool {
+    try {
+      return ref<Boolean>(std::string("AcquirePositionAction.allow_goal_modification"));
+    } catch (const SyntaxError &) {
+      // default value of allow_goal_modification
+      return false;
+    }
+  }();
+
+  auto get_allow_goal_modification_from_property =
+    [this](const EntityRef & entity_ref) -> std::optional<bool> {
+    if (const auto & entity = global().entities->ref(entity_ref);
+        entity.template is<ScenarioObject>()) {
+      const auto & object_controller = entity.as<ScenarioObject>().object_controller;
+      if (object_controller.isAutoware() && object_controller.is<Controller>()) {
+        auto controller = object_controller.as<Controller>();
+        if (controller.properties.contains("allowGoalModification")) {
+          return controller.properties.template get<Boolean>("allowGoalModification");
+        }
+      }
+    }
+    return std::nullopt;
+  };
+
   for (const auto & actor : actors) {
     actor.apply([&](const auto & object) {
       traffic_simulator::RouteOptions options;
+      options.allow_goal_modification = [&]() {
+        if (auto allow_goal_modification_from_property =
+              get_allow_goal_modification_from_property(object);
+            allow_goal_modification_from_property.has_value()) {
+          // property specification takes precedence
+          return allow_goal_modification_from_property.value();
+        } else {
+          return allow_goal_modification_from_parameter;
+        }
+      }();
+
       apply<void>(acquire_position, position, object, options);
     });
   }
