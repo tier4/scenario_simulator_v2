8b6f21cdc27341003f1dc65f0f0fec02b80d8062
SzymonParapura <szymon.parapura@robotec.ai>
2025-07-09 11:39:46 +0200

    revert: Remove rviz node

--- test_runner/random_test_runner/launch/random_test.launch.py
+++ test_runner/random_test_runner/launch/random_test.launch.py
@@ -230,18 +230,6 @@ class RandomTestRunnerLaunch(object):
                         ' == "simple_sensor_simulator"'
                     ])
                 ),
-            ),
-            Node(
-                package="rviz2",
-                executable="rviz2",
-                name="rviz2",
-                output={"stderr": "log", "stdout": "log"},
-                arguments=["-d", self.autoware_launch_configuration["rviz_config"]],
-                condition=IfCondition(
-                    PythonExpression([
-                        "'", self.random_test_runner_launch_configuration["launch_rviz"], "' == 'true'"
-                    ])
-                ),
             )
         ]
 
