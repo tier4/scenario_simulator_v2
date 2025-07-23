41af1b9b4ee8b64a421c5670056b67785df696b8
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-07-24 02:23:17 +0900

    Add tests for TrafficSignalState::ParsedTrafficSignalID

---
+++ openscenario/openscenario_interpreter/test/test_parsed_traffic_signal_id.cpp
@@ -0,0 +1,196 @@
+// Copyright 2015 TIER IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+#include <openscenario_interpreter/syntax/traffic_signal_state.hpp>
+
+TEST(ParsedTrafficSignalID, ValidId)
+{
+  const std::string traffic_signal_id = "34802";
+  openscenario_interpreter::syntax::TrafficSignalState::ParsedTrafficSignalID parsed(
+    traffic_signal_id);
+
+  EXPECT_EQ(parsed.lanelet_id, 34802);
+  EXPECT_EQ(
+    parsed.traffic_signal_type,
+    openscenario_interpreter::syntax::TrafficSignalState::TrafficSignalType::CONVENTIONAL);
+}
+
+TEST(ParsedTrafficSignalID, ValidIdWithV2IType)
+{
+  const std::string traffic_signal_id = "34802 v2i";
+  openscenario_interpreter::syntax::TrafficSignalState::ParsedTrafficSignalID parsed(
+    traffic_signal_id);
+
+  EXPECT_EQ(parsed.lanelet_id, 34802);
+  EXPECT_EQ(
+    parsed.traffic_signal_type,
+    openscenario_interpreter::syntax::TrafficSignalState::TrafficSignalType::V2I);
+}
+
+TEST(ParsedTrafficSignalID, ValidIdWithMultipleSpaces)
+{
+  const std::string traffic_signal_id = "34802    v2i";
+  openscenario_interpreter::syntax::TrafficSignalState::ParsedTrafficSignalID parsed(
+    traffic_signal_id);
+
+  EXPECT_EQ(parsed.lanelet_id, 34802);
+  EXPECT_EQ(
+    parsed.traffic_signal_type,
+    openscenario_interpreter::syntax::TrafficSignalState::TrafficSignalType::V2I);
+}
+
+TEST(ParsedTrafficSignalID, EmptyString)
+{
+  const std::string traffic_signal_id = "";
+  EXPECT_THROW(
+    {
+      openscenario_interpreter::syntax::TrafficSignalState::ParsedTrafficSignalID parsed(
+        traffic_signal_id);
+    },
+    openscenario_interpreter::Error);
+}
+
+TEST(ParsedTrafficSignalID, InvalidId)
+{
+  const std::string traffic_signal_id = "invalid";
+  EXPECT_THROW(
+    {
+      openscenario_interpreter::syntax::TrafficSignalState::ParsedTrafficSignalID parsed(
+        traffic_signal_id);
+    },
+    openscenario_interpreter::Error);
+}
+
+TEST(ParsedTrafficSignalID, InvalidType)
+{
+  const std::string traffic_signal_id = "34802 invalid";
+  EXPECT_THROW(
+    {
+      openscenario_interpreter::syntax::TrafficSignalState::ParsedTrafficSignalID parsed(
+        traffic_signal_id);
+    },
+    openscenario_interpreter::Error);
+}
+
+TEST(ParsedTrafficSignalID, TooManyParts)
+{
+  const std::string traffic_signal_id = "34802 v2i extra";
+  EXPECT_THROW(
+    {
+      openscenario_interpreter::syntax::TrafficSignalState::ParsedTrafficSignalID parsed(
+        traffic_signal_id);
+    },
+    openscenario_interpreter::Error);
+}
+
+// TEST(Syntax, LexicalScope)
+// {
+//   using ament_index_cpp::get_package_share_directory;
+//
+//   auto node {
+//     std::make_shared<rclcpp::Node>("", rclcpp::NodeOptions())
+//   };
+//
+//   openscenario_interpreter::OpenScenario interpreter {
+//     get_package_share_directory("openscenario_interpreter") + "/test/lexical-scope.xosc",
+//     node
+//   };
+// }
+//
+// TEST(Error, Success)
+// {
+//   using ament_index_cpp::get_package_share_directory;
+//
+//   auto node {
+//     std::make_shared<rclcpp::Node>("", rclcpp::NodeOptions())
+//   };
+//
+//   openscenario_interpreter::OpenScenario evaluate {
+//     get_package_share_directory("openscenario_interpreter") + "/test/success.xosc",
+//     node
+//   };
+//
+//   ASSERT_FALSE(evaluate.complete());
+//
+//   const auto begin {std::chrono::high_resolution_clock::now()};
+//
+//   using std::chrono_literals::operator""ms;
+//
+//   rclcpp::WallRate rate {50ms};
+//
+//   using openscenario_interpreter::complete_state;
+//
+//   for (evaluate.init(); evaluate() != complete_state; rate.sleep()) {
+//     ASSERT_LT(
+//       std::chrono::duration_cast<std::chrono::seconds>(
+//         std::chrono::high_resolution_clock::now() - begin).count(),
+//       20);
+//   }
+// }
+
+// TEST(Syntax, invalid)
+// {
+//   auto f = []()
+//   {
+//     openscenario_interpreter::OpenSCENARIO osc { XOSC("invalid-1.xosc") };
+//   };
+//
+//   EXPECT_THROW({ f(); }, openscenario_interpreter::SyntaxError);
+// }
+
+// TEST(Syntax, scenarioDefinition)
+// {
+//   using namespace openscenario_interpreter;
+//
+//   OpenSCENARIO osc { XOSC("example.xosc"), "127.0.0.1", 5555 };
+//
+//   // EXPECT_TRUE(osc.element("FileHeader"));
+//   // EXPECT_TRUE(osc.element("ParameterDeclarations"));
+//
+//   // EXPECT_EQ(osc["FileHeader"].revMajor(), 1);
+//   // EXPECT_TRUE(osc.catalog_locations);
+//   // EXPECT_TRUE(osc.entities);
+//   // EXPECT_TRUE(osc.parameter_declarations);
+//   // EXPECT_TRUE(osc.road_network);
+//   // EXPECT_TRUE(osc.storyboard);
+//
+//   EXPECT_TRUE(osc.evaluate().is<Boolean>());
+// }
+
+// TEST(Core, objectBinder)
+// {
+//   using openscenario_interpreter::make;
+//
+//   const auto foo {make<Double>(3.14)};
+//
+//   const auto result {foo.evaluate()};
+//
+//   EXPECT_TRUE(result.is<Double>());
+//   EXPECT_TRUE(result.as<Double>().data = 3.14);
+// }
+
+// TEST(Scenario, LaneChange)
+// {
+//   using namespace openscenario_interpreter;
+//
+//   OpenSCENARIO osc { XOSC("lane_change.xosc"), "127.0.0.1", 5555 };
+// }
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
