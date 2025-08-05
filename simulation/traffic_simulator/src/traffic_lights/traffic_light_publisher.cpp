173c0ac636fbb93260d82e9e11463e45fa28d1d8
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-08-06 02:09:30 +0900

    Fix TrafficLightPublisher::generateMessage for TrafficLightGroupArray to output only a single element per a relation ID

--- simulation/traffic_simulator/src/traffic_lights/traffic_light_publisher.cpp
+++ simulation/traffic_simulator/src/traffic_lights/traffic_light_publisher.cpp
@@ -93,27 +93,28 @@ auto TrafficLightPublisher<autoware_perception_msgs::msg::TrafficLightGroupArray
   const TrafficLightStatePredictions * predictions)
   -> std::unique_ptr<autoware_perception_msgs::msg::TrafficLightGroupArray>
 {
-  auto message = std::make_unique<autoware_perception_msgs::msg::TrafficLightGroupArray>();
+  using autoware_perception_msgs::msg::PredictedTrafficLightState;
+  using autoware_perception_msgs::msg::TrafficLightElement;
+  using autoware_perception_msgs::msg::TrafficLightGroup;
 
-  message->stamp = current_ros_time;
-
-  using TrafficLightGroupType = autoware_perception_msgs::msg::TrafficLightGroup;
-  using TrafficLightBulbType = autoware_perception_msgs::msg::TrafficLightElement;
-
-  for (const auto & traffic_light : request.states()) {
-    for (const auto & relation_id : traffic_light.relation_ids()) {
-      // skip if the traffic light has no bulbs
-      if (not traffic_light.traffic_light_status().empty()) {
-        TrafficLightGroupType traffic_light_group_message;
-        traffic_light_group_message.traffic_light_group_id = relation_id;
-        for (auto bulb_status : traffic_light.traffic_light_status()) {
-          TrafficLightBulbType light_bulb_message;
-          simulation_interface::toMsg<TrafficLightBulbType>(bulb_status, light_bulb_message);
-          traffic_light_group_message.elements.push_back(light_bulb_message);
-        }
-        message->traffic_light_groups.push_back(traffic_light_group_message);
+  // NOTE: key is relation ID
+  std::unordered_map<lanelet::Id, TrafficLightGroup> traffic_light_group_map;
+  for (const auto & way_id_level_traffic_light : request.states()) {
+    for (const auto & bulb_proto : way_id_level_traffic_light.traffic_light_status()) {
+      TrafficLightElement bulb_message;
+      simulation_interface::toMsg<TrafficLightElement>(bulb_proto, bulb_message);
+      for (const auto & relation_id : way_id_level_traffic_light.relation_ids()) {
+        traffic_light_group_map[relation_id].elements.push_back(bulb_message);
       }
     }
+  }
+
+  auto message = std::make_unique<autoware_perception_msgs::msg::TrafficLightGroupArray>();
+  message->stamp = current_ros_time;
+
+  for (const auto & [relation_id, traffic_light_group] : traffic_light_group_map) {
+    message->traffic_light_groups.push_back(traffic_light_group);
+    message->traffic_light_groups.back().traffic_light_group_id = relation_id;
   }
   return message;
 }
