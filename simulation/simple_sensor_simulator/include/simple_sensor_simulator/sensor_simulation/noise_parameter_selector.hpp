a98bb1fe1a2a7b37bb4c7b62aa6788a008226ddb
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-10-31 05:54:02 +0800

    extract get_first_matched_config_name lambda function to noise_parameter_selector::findMatchingNoiseConfigForEntity

--- simulation/simple_sensor_simulator/include/simple_sensor_simulator/sensor_simulation/noise_parameter_selector.hpp
+++ simulation/simple_sensor_simulator/include/simple_sensor_simulator/sensor_simulation/noise_parameter_selector.hpp
@@ -18,6 +18,7 @@
 #include <boost/math/constants/constants.hpp>
 #include <execution>
 #include <get_parameter/get_parameter.hpp>
+#include <regex>
 #include <scenario_simulator_exception/exception.hpp>
 #include <simulation_interface/operators.hpp>
 #include <string>
@@ -63,6 +64,80 @@ inline auto createEllipticalParameterSelector(
       }
     };
   };
+}
+
+inline auto findMatchingNoiseConfigForEntity(
+  const traffic_simulator_msgs::EntityStatus & entity, const std::string & version,
+  const std::string & topic_name) -> std::string
+{
+  const std::string version_base_path = topic_name + ".noise." + version + ".";
+  auto matches_noise_application_entities = [&](
+                                              const traffic_simulator_msgs::EntityStatus & entity,
+                                              const std::string & noise_config_name) -> bool {
+    const auto base_path = version_base_path + noise_config_name + ".noise_application_entities.";
+
+    const auto types = common::getParameter<std::vector<std::string>>(base_path + "types");
+    const auto subtypes = common::getParameter<std::vector<std::string>>(base_path + "subtypes");
+    const auto names = common::getParameter<std::vector<std::string>>(base_path + "names");
+
+    auto string_with_wildcards_to_regex =
+      [](const std::string & string_with_wildcards) -> std::regex {
+      std::string regex_pattern;
+      for (char c : string_with_wildcards) {
+        regex_pattern += (c == '*') ? ".*" : (c == '?') ? "." : std::string(1, c);
+      }
+      return std::regex(regex_pattern);
+    };
+
+    return std::any_of(
+             types.begin(), types.end(),
+             [entity_type = boost::lexical_cast<std::string>(entity.type()),
+              string_with_wildcards_to_regex](const auto & target) {
+               return std::regex_match(entity_type, string_with_wildcards_to_regex(target));
+             }) &&
+           std::any_of(
+             subtypes.begin(), subtypes.end(),
+             [entity_subtype = boost::lexical_cast<std::string>(entity.subtype()),
+              string_with_wildcards_to_regex](const auto & target) {
+               return std::regex_match(entity_subtype, string_with_wildcards_to_regex(target));
+             }) &&
+           std::any_of(
+             names.begin(), names.end(),
+             [entity_name = entity.name(), string_with_wildcards_to_regex](const auto & target) {
+               return std::regex_match(entity_name, string_with_wildcards_to_regex(target));
+             });
+  };
+
+  const auto parameter_names =
+    common::getParameterNode()
+      .list_parameters({}, rcl_interfaces::srv::ListParameters::Request::DEPTH_RECURSIVE)
+      .names;
+
+  auto extract_child_namespace = [&](const std::string & parameter_name) -> std::string {
+    if (const auto next_dot_pos = parameter_name.find('.', version_base_path.length());
+        next_dot_pos != std::string::npos) {
+      return parameter_name.substr(
+        version_base_path.length(), next_dot_pos - version_base_path.length());
+    }
+    return "";
+  };
+
+  if (auto matched_parameter = std::find_if(
+        parameter_names.begin(), parameter_names.end(),
+        [&](const auto & parameter_name) {
+          if (parameter_name.rfind(version_base_path, 0) == 0) {
+            if (auto child_namespace = extract_child_namespace(parameter_name);
+                child_namespace != "") {
+              return matches_noise_application_entities(entity, child_namespace);
+            }
+          }
+          return false;
+        });
+      matched_parameter != parameter_names.end()) {
+    return extract_child_namespace(*matched_parameter);
+  } else {
+    return "";
+  }
 }
 }  // namespace noise_parameter_selector
 }  // namespace simple_sensor_simulator
--- simulation/simple_sensor_simulator/src/sensor_simulation/detection_sensor/detection_sensor.cpp
+++ simulation/simple_sensor_simulator/src/sensor_simulation/detection_sensor/detection_sensor.cpp
@@ -528,92 +528,14 @@ auto DetectionSensor<autoware_perception_msgs::msg::DetectedObjects>::update(
       return noised_detected_entities;
     };
 
-    auto get_first_matched_config_name = [this](
-                                           const traffic_simulator_msgs::EntityStatus & entity,
-                                           const std::string & version) -> std::string {
-      const std::string version_base_path =
-        std::string(detected_objects_publisher->get_topic_name()) + ".noise." + version + ".";
-      auto matches_noise_application_entities =
-        [&](
-          const traffic_simulator_msgs::EntityStatus & entity,
-          const std::string & noise_config_name) -> bool {
-        const auto base_path =
-          version_base_path + noise_config_name + ".noise_application_entities.";
-
-        const auto types = common::getParameter<std::vector<std::string>>(base_path + "types");
-        const auto subtypes =
-          common::getParameter<std::vector<std::string>>(base_path + "subtypes");
-        const auto names = common::getParameter<std::vector<std::string>>(base_path + "names");
-
-        auto string_with_wildcards_to_regex =
-          [](const std::string & string_with_wildcards) -> std::regex {
-          std::string regex_pattern;
-          for (char c : string_with_wildcards) {
-            regex_pattern += (c == '*') ? ".*" : (c == '?') ? "." : std::string(1, c);
-          }
-          return std::regex(regex_pattern);
-        };
-
-        // clang-format off
-          return std::any_of(
-                   types.begin(), types.end(),
-                   [entity_type = boost::lexical_cast<std::string>(entity.type()), string_with_wildcards_to_regex]
-                   (const auto & target) {
-                     return std::regex_match(entity_type, string_with_wildcards_to_regex(target));
-                   }) &&
-                 std::any_of(
-                   subtypes.begin(), subtypes.end(),
-                   [entity_subtype = boost::lexical_cast<std::string>(entity.subtype()), string_with_wildcards_to_regex]
-                   (const auto & target) {
-                     return std::regex_match(entity_subtype, string_with_wildcards_to_regex(target));
-                   }) &&
-                 std::any_of(
-                   names.begin(), names.end(),
-                   [entity_name = entity.name(), string_with_wildcards_to_regex]
-                   (const auto & target) {
-                     return std::regex_match(entity_name, string_with_wildcards_to_regex(target));
-                   });
-        // clang-format on
-      };
-
-      const auto parameter_names =
-        common::getParameterNode()
-          .list_parameters({}, rcl_interfaces::srv::ListParameters::Request::DEPTH_RECURSIVE)
-          .names;
-
-      auto extract_child_namespace = [&](const std::string & parameter_name) -> std::string {
-        if (const auto next_dot_pos = parameter_name.find('.', version_base_path.length());
-            next_dot_pos != std::string::npos) {
-          return parameter_name.substr(
-            version_base_path.length(), next_dot_pos - version_base_path.length());
-        }
-        return "";
-      };
-
-      if (auto matched_parameter = std::find_if(
-            parameter_names.begin(), parameter_names.end(),
-            [&](const auto & parameter_name) {
-              if (parameter_name.rfind(version_base_path, 0) == 0) {
-                if (auto child_namespace = extract_child_namespace(parameter_name);
-                    child_namespace != "") {
-                  return matches_noise_application_entities(entity, child_namespace);
-                }
-              }
-              return false;
-            });
-          matched_parameter != parameter_names.end()) {
-        return extract_child_namespace(*matched_parameter);
-      } else {
-        return "";
-      }
-    };
-
     auto noise_v3 = [&](const auto & detected_entities, auto simulation_time) {
       auto noised_detected_entities = std::decay_t<decltype(detected_entities)>();
       for (const auto & entity : detected_entities) {
         if (auto [noise_output, success] = noise_outputs.emplace(entity.name(), simulation_time);
             success) {
-          noise_output->second.config_name = get_first_matched_config_name(entity, "v3");
+          noise_output->second.config_name =
+            noise_parameter_selector::findMatchingNoiseConfigForEntity(
+              entity, "v3", detected_objects_publisher->get_topic_name());
         } else if (not noise_output->second.config_name.empty()) {
           auto vanilla_entity = std::vector<traffic_simulator_msgs::EntityStatus>{entity};
           const std::string config_namespace = "v3." + noise_output->second.config_name;
@@ -751,7 +673,9 @@ auto DetectionSensor<autoware_perception_msgs::msg::DetectedObjects>::update(
       for (const auto & entity : detected_entities) {
         if (auto [noise_output, success] = noise_outputs.emplace(entity.name(), simulation_time);
             success) {
-          noise_output->second.config_name = get_first_matched_config_name(entity, "v4");
+          noise_output->second.config_name =
+            noise_parameter_selector::findMatchingNoiseConfigForEntity(
+              entity, "v4", detected_objects_publisher->get_topic_name());
         } else if (not noise_output->second.config_name.empty()) {
           const std::string parameter_base_path =
             std::string(detected_objects_publisher->get_topic_name()) + ".noise.v4." +
