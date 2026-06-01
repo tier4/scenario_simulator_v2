a9c1fd2577cd89f78f106d9e5a44ba627667d45b
t4-adc <grp-rd-1-adc-admin@tier4.jp>
2026-06-01 18:09:40 +0900

    tmp
    Signed-off-by: t4-adc <grp-rd-1-adc-admin@tier4.jp>

--- openscenario/openscenario_interpreter/include/openscenario_interpreter/simulator_core.hpp
+++ openscenario/openscenario_interpreter/include/openscenario_interpreter/simulator_core.hpp
@@ -335,8 +335,8 @@ public:
       const std::string & entity_ref, Controller && controller) -> void
     {
       auto & entity = core->getEntity(entity_ref);
-      entity.setVelocityLimit(controller.properties.template get<Double>(
-        "maxSpeed", std::numeric_limits<Double::value_type>::max()));
+      // entity.setVelocityLimit(controller.properties.template get<Double>(
+      //   "maxSpeed", std::numeric_limits<Double::value_type>::max()));
 
       if (controller.properties.contains("lateralCollisionThreshold")) {
         entity.setLateralCollisionThreshold(
